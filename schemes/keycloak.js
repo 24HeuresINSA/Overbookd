e586dd1f29573218286fe07face4c188d058a33b
hamza <hamza.badaoui@insa-lyon.fr>
2021-03-11 16:29:06 +0100

    test(auth): 🥳 fix CORS issue #2

--- schemes/keycloak.js
+++ schemes/keycloak.js
@@ -1,12 +1,13 @@
 import { LocalScheme } from "~auth/runtime";
 import axios from "axios";
 import { KEYCLOAK } from "../config/url.json";
+import qs from "qs";
 
 export default class KeycloakScheme extends LocalScheme {
   keycloakServer = axios.create({
     baseURL: KEYCLOAK.BASE_URL,
     headers: {
-      "Access-Control-Allow-Credentials": true,
+      // "Access-Control-Allow-Credentials": true,
       // "Access-Control-Allow-Origin:": "*",
     },
   });
@@ -50,12 +51,13 @@ export default class KeycloakScheme extends LocalScheme {
   }
 
   async login(scheme, { username, password }) {
-    const response = await this.keycloakServer.post(KEYCLOAK.TOKEN, {
+    const data = qs.stringify({
       username,
       password,
       client_id: "project_a_web",
       grant_type: "password",
     });
+    const response = await this.keycloakServer.post(KEYCLOAK.TOKEN, data);
     console.log(response);
   }
 }
