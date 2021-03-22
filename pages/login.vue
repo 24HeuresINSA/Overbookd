cb927b75bd527965245e1de4e4bb1b794c72809f
hamza <hamza.badaoui@insa-lyon.fr>
2021-03-22 10:28:49 +0100

    feat(auth): 📣 cleaned code #2 removed useless button from login screen

--- pages/login.vue
+++ pages/login.vue
@@ -40,12 +40,6 @@
     </v-form>
     <v-snackbar v-model="snackbar" :timeout="timeout">
       {{ feedbackMessage }}
-
-      <template v-slot:action="{ attrs }">
-        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
-          Close
-        </v-btn>
-      </template>
     </v-snackbar>
   </div>
 </template>
