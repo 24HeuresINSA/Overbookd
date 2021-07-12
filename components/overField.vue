4c765b8d0bb49ce66d7f1f2523e8c9824366d4dc
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2021-07-12 23:19:14 +0200

    fix(form): 🔨 fixed field labels

--- components/overField.vue
+++ components/overField.vue
@@ -5,33 +5,33 @@
         v-if="field.type=== 'string' || field.type === undefined"
         :rules="field.rule"
         :counter="field.counter"
-        :label="field.name ? field.name : field.key"
+        :label="field.label ? field.label : field.key"
         required
     ></v-text-field>
     <v-switch
         v-model="field.value"
-        :label="field.name ? field.name : field.key"
+        :label="field.label ? field.label : field.key"
         v-else-if="field.type === 'switch'"
     ></v-switch>
     <v-select
         v-else-if="field.type === 'select'"
-        :label="field.name ? field.name : field.key"
+        :label="field.label ? field.label : field.key"
         v-model="field.value"
         :items="field.options"
     ></v-select>
     <v-datetime-picker
         v-if="field.type === 'datetime'"
-        :label="field.name ? field.name : field.key"
+        :label="field.label ? field.label : field.key"
         v-model="field.value"
     ></v-datetime-picker>
     <v-date-picker
-        v-if="field.type == 'date'"
-        :label="field.name ? field.name : field.key"
+        v-if="field.type === 'date'"
+        :label="field.label ? field.label : field.key"
         v-model="field.date"
     ></v-date-picker>
     <v-time-picker
         v-if="field.type == 'time'"
-        :label="field.name ? field.name : field.key"
+        :label="field.label ? field.label : field.key"
         v-model="field.time"
     ></v-time-picker>
     <p v-if="field.description">{{ field.description }}</p>
