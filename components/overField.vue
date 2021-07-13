3bc293e739ffa560d85b502569ef4d36bdeffcec
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2021-07-13 10:19:11 +0200

    fix(form): 🔨 fixed values

--- components/overField.vue
+++ components/overField.vue
@@ -1,38 +1,42 @@
 <template>
   <div>
     <v-text-field
-        v-model="field.value"
+        v-model="value"
         v-if="field.type=== 'string' || field.type === undefined"
         :rules="field.rule"
         :counter="field.counter"
         :label="field.label ? field.label : field.key"
+        @change="onChange"
         required
     ></v-text-field>
     <v-switch
-        v-model="field.value"
+        v-model="value"
         :label="field.label ? field.label : field.key"
         v-else-if="field.type === 'switch'"
     ></v-switch>
     <v-select
         v-else-if="field.type === 'select'"
         :label="field.label ? field.label : field.key"
-        v-model="field.value"
+        v-model="value"
         :items="field.options"
     ></v-select>
     <v-datetime-picker
         v-if="field.type === 'datetime'"
         :label="field.label ? field.label : field.key"
-        v-model="field.value"
+        v-model="value"
     ></v-datetime-picker>
-    <v-date-picker
-        v-if="field.type === 'date'"
-        :label="field.label ? field.label : field.key"
-        v-model="field.date"
-    ></v-date-picker>
+    <div v-if="field.type === 'date'">
+      <p>{{field.label ? field.label : field.key}}</p>
+      <v-date-picker
+          :label="field.label ? field.label : field.key"
+          v-model="value"
+      ></v-date-picker>
+    </div>
+
     <v-time-picker
         v-if="field.type == 'time'"
         :label="field.label ? field.label : field.key"
-        v-model="field.time"
+        v-model="value"
     ></v-time-picker>
     <p v-if="field.description">{{ field.description }}</p>
   </div>
@@ -42,13 +46,24 @@
 export default {
   name: "over-field",
   props: ["field"],
+  data(){
+    return {
+      value: undefined,
+    }
+  },
+
+  methods: {
+    onChange(){
+      this.$emit('value', this.value)
+    }
+  },
+
   mounted() {
-    console.log('dads')
   },
 
   watch: {
     field: function (){
-      console.log('field emitted ...')
+      console.log('field emitted ...' + this.field.value)
       this.$emit('value', this.field)
     }
   }
