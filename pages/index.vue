1911783cb8651a21d0b9df55cc97737465693173
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2021-07-13 16:31:02 +0200

    feat(index): 🥳 started adding index

--- pages/index.vue
+++ pages/index.vue
@@ -1,100 +1,55 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
+    <v-col cols="6" sm="4" md="3">
+      <v-card v-if="user">
+        <v-card-title>Bonsoir {{user.nickname ? user.nickname : user.firstname}}</v-card-title>
+        <v-card-subtitle> {{user.email}}</v-card-subtitle>
         <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+          <p>Charisme: {{user.charisma}}</p>
+          <v-chip-group>
+            <v-chip v-for="team in user.team">
+              {{team}}
+            </v-chip>
+          </v-chip-group>
+          <v-progress-linear value="15"></v-progress-linear>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" @click="test()"> Continue </v-btn>
-        </v-card-actions>
       </v-card>
+    </v-col>
+
+    <v-col cols="6" sm="4" md="3">
+
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
+import jwt_decode from "jwt-decode";
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  components: {},
+
+  data(){
+    return {
+      user: undefined,
+    }
+  },
+
+  async mounted() {
+    const keycloakID = this.getUsername();
+    this.user = await this.getUser(keycloakID)
+    console.log(this.user)
   },
 
   methods: {
-    test: async function () {
-      await this.$axios.$get("");
-      console.log("request sent");
+    getUsername(){
+      const token = this.$auth.$storage._state["_token.keycloak"].split(' ')[1]
+      const decoded = jwt_decode(token)
+      return decoded.sub
     },
+
+    async getUser(keycloakID){
+      return this.$axios.$get(`/user/${keycloakID}`)
+    }
   },
 };
 </script>
