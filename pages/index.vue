5a336575c4f92ec5cd4d748ab38de8f9b8004538
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2021-07-15 10:13:37 +0200

    feat(index): 💰 display compte perso

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="6" sm="4" md="3">
+    <v-col cols="6" sm="6" md="4">
       <v-card v-if="user">
         <v-card-title>Bonsoir {{user.nickname ? user.nickname : user.firstname}}</v-card-title>
         <v-card-subtitle> {{user.email}}</v-card-subtitle>
@@ -16,14 +16,99 @@
       </v-card>
     </v-col>
 
-    <v-col cols="6" sm="4" md="3">
+    <v-col cols="6" sm="4" md="4">
+      <v-card v-if="user">
+        <v-card-title>Compte perso 💰</v-card-title>
+        <v-card-subtitle>Balance: {{user.balance || 0}} €</v-card-subtitle>
+        <v-card-text v-if="user.transactionHistory">
+          <v-simple-table>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  Operation
+                </th>
+                <th class="text-right">
+                  €
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                  v-for="item in user.transactionHistory"
+              >
+                <td>{{ item.reason }}</td>
+                <td class="text-right">{{ item.amount }} €</td>
+              </tr>
+            </tbody>
+            </v-simple-table>
+        </v-card-text>
+      </v-card>
+    </v-col>
 
+    <v-col cols="6" sm="4" md="4">
+      <v-card v-if="user">
+        <v-card-title>Amis ❤️</v-card-title>
+        <v-card-subtitle>mes reuf</v-card-subtitle>
+        <v-card-text v-if="user.friends.length !== 0">
+          <p>{{user.friends}}</p>
+          <v-simple-table>
+            <thead>
+            <tr>
+              <th class="text-left">
+                Amis
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                v-for="item in user.friends"
+            >
+              <td>{{ item }}</td>
+            </tr>
+            </tbody>
+          </v-simple-table>
+        </v-card-text>
+        <v-card-text v-else>Lonely 🥲</v-card-text>
+        <v-card-actions>
+          <v-text-field
+            label="username de ton pote"
+            v-model="newFriend"
+          ></v-text-field>
+          <v-btn @click="sendFriendRequest">ajouter un amis</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+    <v-col cols="6" sm="4" md="4">
+      <v-card v-if="user">
+        <v-card-title>Notification 📣️</v-card-title>
+        <v-card-text v-if="user.notifications">
+          <p>{{user.friends}}</p>
+          <v-simple-table>
+            <thead>
+            <tr>
+              <th class="text-left">
+                Amis
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                v-for="item in user.friends"
+            >
+              <td>{{ item }}</td>
+            </tr>
+            </tbody>
+          </v-simple-table>
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
 import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 export default {
   components: {},
@@ -31,6 +116,7 @@ export default {
   data(){
     return {
       user: undefined,
+      newFriend: undefined,
     }
   },
 
@@ -49,6 +135,10 @@ export default {
 
     async getUser(keycloakID){
       return this.$axios.$get(`/user/${keycloakID}`)
+    },
+
+    async sendFriendRequest(){
+      this.$axios.post('/user/')
     }
   },
 };
