30b2f7021da400b98fcda220327eb52d45b0e48e
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2021-08-07 17:26:06 +0100

    feat(UI): 🌙 persistant dark mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -65,16 +65,6 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -191,6 +181,10 @@ export default {
     }
   },
 
+  mounted() {
+    this.$vuetify.theme.dark = localStorage['theme'] || false;
+  },
+
   methods: {
     getUser(){
       return this.$store.state.user.data
@@ -209,6 +203,7 @@ export default {
 
     toggleTheme(){
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      localStorage['theme'] = this.$vuetify.theme.dark;
     },
 
     async clickOnTitle(){
