3710532b116b1645328ee9334a5ec1f1ce3179e9
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2021-08-08 09:44:55 +0100

    feat(config): ⚙️ added configs

--- pages/config.vue
+++ pages/config.vue
@@ -7,6 +7,17 @@
     <h2>Inscription</h2>
     <v-switch label="Ouverture des inscription" v-model="config.isSignupOpen"></v-switch>
     <v-text-field label="message de fermeture" v-model="config.fb_signup_closed"></v-text-field>
+
+    <h2>FA</h2>
+    <v-text-field label="message de confirmation" v-model="config.fb_confirm_submit"></v-text-field>
+
+    <h2>Dispo</h2>
+    <v-text-field label="charisme max" type="number" v-model="config.max_charisma"></v-text-field>
+    <v-textarea label="description des dispo" type="number" v-model="config.availabilities_description"></v-textarea>
+
+    <h2>Log</h2>
+    <v-switch v-model="config.isInventoryOpen" label="ouvrir l'edition de l'inventaire au hard"></v-switch>
+
   </v-container>
 </template>
 
@@ -21,6 +32,10 @@ export default {
       config: {
         isSignupOpen: getConfig(this, 'isSignupOpen'),
         fb_signup_closed: getConfig(this, 'fb_signup_closed'),
+        fb_confirm_submit: getConfig(this, 'fb_confirm_submit'),
+        availabilities_description: getConfig(this, 'availabilities_description'),
+        max_charisma: getConfig(this, 'max_charisma'),
+        isInventoryOpen: getConfig(this, 'isInventoryOpen')
       }
     }
   },
