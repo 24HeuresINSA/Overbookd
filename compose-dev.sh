26fd010d71338b1fbd5b82c93fc118c4a8346f8f
titouan-joseph <titouanjoseph@gmail.com>
2021-11-25 14:49:20 +0100

    chore(docker-compose): 🐳 script to simplify docker start

--- compose-dev.sh
+++ compose-dev.sh
@@ -1,1 +1,29 @@
-docker-compose -f docker-compose-dev.yml -p dev --env-file dev.env "$@"
+#/bin/bash
+
+case $1 in
+
+"--dev") echo "starting dev containers"
+         docker-compose -f docker-compose-dev.yml -p dev --env-file docker/dev.env "${@:}"
+         ;;
+
+"--utils") echo "starting utils containers"
+           docker-compose -f docker-compose_utils.yml -p utils up -d
+           ;;
+
+"--prod") echo "starting prod containers"
+          docker-compose -f docker-compose.yml -p prod up -d
+          ;;
+
+"--preprod") echo "starting prod containers"
+             docker-compose -f docker-compose-preprod.yml up -d
+             ;;
+
+"--all") echo "starting utils, prod and prepord containers"
+         docker-compose -f docker-compose_utils.yml -p utils up -d
+         docker-compose -f docker-compose.yml -p prod up -d
+         docker-compose -f docker-compose-preprod.yml up -d
+         ;;
+
+*) echo "no command"
+;;
+esac
