1e95c8e17c85252e7055b5f9189c38c830726ff3
Spycro <lucas.zimmermann@protonmail.com>
2021-12-13 21:53:25 +0000

    Resolve "[Feature_Request][Inventaire] Besoin avant l'inventaire physique de fin janvier"

--- store/equipment.ts
+++ store/equipment.ts
@@ -1,14 +1,25 @@
 import { actionTree, mutationTree } from "typed-vuex";
 import { safeCall } from "~/utils/api/calls";
 import { RepoFactory } from "~/repositories/repoFactory";
+import { Equipment } from "~/utils/models/Equipment";
 
 export const state = () => ({
-  items: [] as any,
+  items: [] as Equipment[],
 });
 
 export const mutations = mutationTree(state, {
-  SET_ALL_EQUIPMENT: function (state, items) {
+  SET_ALL_EQUIPMENT: function (state, items: Equipment[]) {
     state.items = items;
+  },
+  SET_EQUIPMENT: function (state, item: Equipment) {
+    state.items.push(item);
+  },
+  UPDATE_EQUIPMENT: function (state, item: Equipment) {
+    const index = state.items.findIndex((i: any) => i._id === item._id);
+    state.items.splice(index, 1, item);
+  },
+  DELETE_EQUIPMENT: function (state, item: Equipment) {
+    state.items = state.items.filter((i: any) => i._id !== item._id);
   },
 });
 
@@ -26,6 +37,38 @@ export const actions = actionTree(
       if (res) {
         commit("SET_ALL_EQUIPMENT", res.data);
       }
+      return res;
+    },
+    set: async function ({ commit }, equipment: Equipment) {
+      const res = await safeCall(
+        this,
+        RepoFactory.equipmentRepo.setEquipment(this, equipment)
+      );
+      if (res && res.data) {
+        commit("SET_EQUIPMENT", res.data);
+      }
+      return res;
+    },
+    delete: async function ({ commit }, equipment: Equipment) {
+      equipment.isValid = false;
+      const res = await safeCall(
+        this,
+        RepoFactory.equipmentRepo.setEquipment(this, equipment)
+      );
+      if (res && res.data) {
+        commit("DELETE_EQUIPMENT", res.data);
+      }
+      return res;
+    },
+    update: async function ({ commit }, equipment: Equipment) {
+      const res = await safeCall(
+        this,
+        RepoFactory.equipmentRepo.setEquipment(this, equipment)
+      );
+      if (res && res.data) {
+        commit("UPDATE_EQUIPMENT", res.data);
+      }
+      return res;
     },
   }
 );
