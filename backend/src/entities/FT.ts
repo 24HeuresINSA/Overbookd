8fd9c4d2ee0b4af59fe2c679ddb4e5c334dd8665
Tom Sampic <tom.sampic26@gmail.com>
2022-02-20 16:55:39 +0100

    refactor(back): :building_construction: Improve FA and FT types by specifying strict entities
    FTs are now fully typed in backend with structs that match schema definition in mongoose. This improves typescript support.

--- backend/src/entities/FT.ts
+++ backend/src/entities/FT.ts
@@ -1,51 +1,82 @@
-import {model, Schema} from "mongoose";
-import {IForm} from "@entities/FA";
+import { model, Schema, Types } from "mongoose";
+import { IForm } from "@entities/FA";
+import { IComment } from "./FA";
 
+type FTStatus = "draft" | "submitted" | "validated" | "refused" | "ready";
 export interface IFT extends IForm {
-  FA: number;
-  team?: string;
+  FA?: number;
+  status: FTStatus;
+  timeframes: ITimeFrame[];
+  details: Record<string, unknown>;
 }
 
-interface ISchedule {
-  date: string;
-  start: Date;
-  end: Date;
-  needs: INeed[];
-  assigned?: IAssign[];
-}
+type ITFRequiredType = "user" | "team";
 
-interface INeed {
-  role?: string;
-  amount?: number;
+interface ITFRequiredUser {
+  _id: Types.ObjectId;
+  username: string;
 }
-
-interface IAssign {
-  userID: string;
+export interface ITFRequired {
   _id: string;
-  username?: string;
+  type: ITFRequiredType;
+  amount?: number;
+  team?: string;
+  user?: ITFRequiredUser;
 }
 
-const FTSchema = new Schema<IFT>(
-  {},
-  {strict: false}
-);
-
-const FTModel = model<FT>("FT", FTSchema);
-
-class FT implements IFT {
-  constructor() {
-  }
-
-  FA = 0;
-  comments = [];
-  count = 0;
-  equipments = [];
-  general = {};
-  isValid = true;
-  refused = [];
-  status = "draft";
-  validated = [];
-  _id = "";
+export interface ITimeFrame {
+  _id: string;
+  name: string;
+  start: number;
+  end: number;
+  required: ITFRequired[];
+  sliceTime?: number;
+  toSlice?: boolean;
+  timed: boolean;
 }
+
+const RequiredSchema = new Schema<ITFRequired>({
+  _id: { type: String, required: true },
+  type: { type: String, required: true },
+  amount: Number,
+  team: String,
+  user: new Schema<ITFRequiredUser>({
+    _id: { type: Types.ObjectId, ref: "User" },
+    username: String,
+  }),
+});
+
+const TimeFrameSchema = new Schema<ITimeFrame>({
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  start: { type: Number, required: true },
+  end: { type: Number, required: true },
+  required: { type: [RequiredSchema], default: [] },
+  timed: { type: Boolean, default: true },
+  sliceTime: Number,
+  toSlice: Boolean,
+});
+
+const CommentSchema = new Schema<IComment>({
+  topic: { type: String, required: true },
+  text: { type: String, required: true },
+  time: { type: Date, required: true },
+  validator: { type: String, required: true },
+});
+
+const FTSchema = new Schema<IFT>({
+  FA: Number,
+  comments: { type: [CommentSchema], default: [] },
+  refused: { type: [String], default: [] },
+  validated: { type: [String], default: [] },
+  status: { type: String, default: "draft" },
+  equipments: { type: [Object], default: [] },
+  timeframes: { type: [TimeFrameSchema], default: [] },
+  count: { type: Number, required: true },
+  details: Object,
+  general: Object,
+});
+
+const FTModel = model<IFT>("FT", FTSchema);
 
 export default FTModel;
--- backend/src/routes/FT.ts
+++ backend/src/routes/FT.ts
@@ -1,38 +1,47 @@
 import StatusCodes from "http-status-codes";
-import {Request, Response} from "express";
-import FTModel, {IFT} from "@entities/FT";
+import { Request, Response } from "express";
+import FTModel, { IFT } from "@entities/FT";
 import logger from "@shared/Logger";
 import FAModel from "@entities/FA";
 
 export async function getAllFTs(req: Request, res: Response) {
   const mFTs = await FTModel.find({});
-  res.json({data: mFTs});
+  res.json({ data: mFTs });
 }
 
 export async function getFTByID(req: Request, res: Response) {
-  const mFT = await FTModel.findOne({count: +req.params.FTID});
+  const mFT = await FTModel.findOne({ count: +req.params.FTID });
   res.json(mFT);
 }
 
 export async function createFT(req: Request, res: Response) {
   const mFT = <IFT>req.body;
   const count = await FTModel.countDocuments();
-  mFT.count = count + 1
+  mFT.count = count + 1;
   const FT = await FTModel.create(mFT);
   res.json(FT);
 }
 
-export async function updateFT(req: Request, res: Response) {
-  const mFT = <IFT>req.body;
+export async function updateFT(
+  req: Request<Record<string, never>, Record<string, never>, IFT>,
+  res: Response
+): Promise<void> {
+  const mFT = req.body;
   if (mFT._id) {
-    //@ts-ignore
-    await FTModel.findByIdAndUpdate(mFT._id, mFT);
+    try {
+      await FTModel.findByIdAndUpdate(mFT._id, mFT);
+    } catch (e) {
+      logger.err(e);
+    }
     res.sendStatus(StatusCodes.OK);
   } else {
     res.sendStatus(StatusCodes.BAD_REQUEST);
   }
 }
 
+/**
+ * @deprecated
+ */
 export async function unassign(req: Request, res: Response) {
   // unassign a user from an FT
   // const { FTID, _id } = req.body;
@@ -55,19 +64,22 @@ export async function unassign(req: Request, res: Response) {
 
 export async function deleteFT(req: Request, res: Response) {
   const mFT = <IFT>req.body;
-  logger.info(`deleting FT: ${mFT.count}...`)
+  logger.info(`deleting FT: ${mFT.count}...`);
   if (mFT.count) {
-    await FTModel.findOneAndUpdate({count: mFT.count}, {$set: {isValid: false}});
+    await FTModel.findOneAndUpdate(
+      { count: mFT.count },
+      { $set: { isValid: false } }
+    );
     if (mFT.FA) {
-      logger.info(`deleting FT: ${mFT.count} from FA ${mFT.FA}`)
-      const mFA = await FAModel.findOne({count: mFT.FA});
+      logger.info(`deleting FT: ${mFT.count} from FA ${mFT.FA}`);
+      const mFA = await FAModel.findOne({ count: mFT.FA });
       if (mFA && mFA.FTs) {
-        mFA.FTs = mFA.FTs.filter((FT: IFT) => FT.count !== mFT.count);
-        mFA.save()
-        logger.info(`deleted FT`)
+        mFA.FTs = mFA.FTs.filter((FT) => FT.count !== mFT.count);
+        mFA.save();
+        logger.info(`deleted FT`);
       }
     }
-    res.status(StatusCodes.OK).json({mFT});
+    res.status(StatusCodes.OK).json({ mFT });
   } else {
     res.sendStatus(StatusCodes.BAD_REQUEST);
   }
