f1a0e3079dfe4a32ac9840cf5ecdf3e56634ab59
Tom Sampic <tom.sampic26@gmail.com>
2022-02-23 22:01:30 +0100

    test(back): :test_tube: Adding tests for conflict logic functions
    Adding sample test with data comming from the database. Needs database migration to go further.

--- backend/__tests__/unit/services/tfconflicts.test.ts
+++ backend/__tests__/unit/services/tfconflicts.test.ts
@@ -1,12 +1,32 @@
-describe("Testing sortTFByUser", () => {
+import { ITimeFrame } from "@entities/FT";
+import { sortTFByUser } from "@src/services/conflict";
+import * as fs from "fs";
+import { computeTFConflictsWithArray } from "../../../src/services/conflict";
+
+let allTFs: ITimeFrame[];
+let res: any = {};
+
+describe("Testing conflicts service", () => {
   beforeAll(() => {
-    /**
-     * todo: Declare timeFrames
-     * Maybe from a json file
-     */
+    // Load a really huge amount of timeFrames from json file
+    const raw = fs.readFileSync(
+      "__tests__/unit/services/timeFrames_sample.json",
+      "utf-8"
+    );
+    allTFs = JSON.parse(raw);
+    res = sortTFByUser(allTFs);
+  });
+  describe("Testing sortTFByUser", () => {
+    it("should output a non null object", () => {
+      expect(Object.keys(res).length).toBeGreaterThan(0);
+    });
   });
 
-  it("is a sample always passing test", () => {
-    expect(true).toBeTruthy();
+  describe("Testing computeTFConflictsWithArray", () => {
+    it("should not fail", () => {
+      res = computeTFConflictsWithArray(allTFs[0], allTFs);
+      console.log(res);
+      expect(true).toBeTruthy();
+    });
   });
 });
