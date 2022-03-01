b7fc034637469c1558d6becec20f6cfd6cd6ad34
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2022-03-01 17:33:42 +0000

    Resolve "SOFT confirmation mail + remplir leur dispos"

--- backend/src/services/mail.ts
+++ backend/src/services/mail.ts
@@ -60,3 +60,58 @@ export const sendResetMail = async function (
     });
   });
 };
+
+export const sendValidationMail = async function (
+  to: string
+): Promise<unknown | undefined> {
+  const html = `
+  <table style="text-align: center;">
+  <h1>Validation Overbookd</h1>
+  <p>
+    Félicitation ton inscription sur Overbookd est dorénavant complète ! <br/>
+  </p>
+  <h2>    
+    Tu peux maintenant te connecter pour pouvoir renseigner tes dispos. <br/>
+    <span style="color:red">N'oublie pas que plus de dispo = plus rapidement validé !!</span>
+  </h2>
+  <a
+    id="link-button"
+    href="overbookd.${process.env.DOMAIN}/login"
+    style="
+      background-color: green;
+      padding: 1rem 1rem 1rem 1rem;
+      border-radius: 0.5rem;
+      color: white;
+      text-decoration: none;
+      display: inline-block;
+      margin: 1rem;
+    "
+    >Connexion à Overbookd</a
+  >
+  <p>
+    Si le lien ne marche pas, va a cette adresse :
+    overbookd.${process.env.DOMAIN}/login
+  </p>
+
+  <p>
+    Merci de ton investissement ! <br />
+    La Team OB
+  </p>
+</table>`;
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to,
+    subject: "Validation Overbookd",
+    html,
+  };
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(info);
+    });
+  });
+};
