7ad03e478808df0f7cceafa0543d3201a3ec9361
titouan-joseph <titouanjoseph@gmail.com>
2022-03-20 14:12:59 +0100

    feat(passsecu):

---
+++ frontend/pages/passsecu.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-data-table :headers="headers" :items="pass" dense>
+      <template #[`item.fullname`]="{ item }">
+        {{ item.fullname || "az" }}
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Header } from "~/utils/models/Data";
+
+interface Data {
+  headers: Header[];
+  pass: [];
+}
+
+export default Vue.extend({
+  name: "PassSecu",
+  data(): Data {
+    return {
+      headers: [
+        { text: "Nom", value: "fullname" },
+        { text: "Tél", value: "phone" },
+      ],
+      pass: [],
+    };
+  },
+
+  async beforeMount() {
+    this.pass = await this.getAllPassSecu();
+  },
+
+  methods: {
+    async getAllPassSecu() {
+      this.passsecu = (await this.$axios.get("/passsecu")).data;
+      console.log(this.passsecu);
+    },
+  },
+});
+</script>
