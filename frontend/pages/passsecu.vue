029477bacd08d28af06778c704c7a44f585bbb48
titouan-joseph <titouanjoseph@gmail.com>
2022-03-21 02:24:40 +0100

    feat(passsecu): :sparkles: export csv

--- frontend/pages/passsecu.vue
+++ frontend/pages/passsecu.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
-    <v-data-table :headers="headers" :items="pass" dense>
-      <template #[`item.fullname`]="{ item }">
-        {{ item.fullname || "az" }}
-      </template>
-      <template #[`item.phone`]="{ item }">
-        {{ item.phone || "az" }}
-      </template>
-    </v-data-table>
+    <v-row>
+      <v-col md="2">
+        <v-card>
+          <v-card-title>
+            <span class="headline">Export</span>
+          </v-card-title>
+          <v-card-text>
+            <v-btn text @click="exportCSV">Exporter les pass</v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col md="10">
+        <v-data-table
+          :headers="headers"
+          :items="pass"
+          dense
+          :items-per-page="-1"
+        >
+          <template #[`item.fullname`]="{ item }">
+            {{ item.fullname }}
+          </template>
+          <template #[`item.phone`]="{ item }">
+            {{ item.phone }}
+          </template>
+          <template #[`item.email`]="{ item }">
+            {{ item.email }}
+          </template>
+          <template #[`item.licensePlate`]="{ item }">
+            {{ item.licensePlate }}
+          </template>
+          <template #[`item.timeslot`]="{ item }">
+            <div v-for="slot in item.timeslot" :key="slot">
+              {{ slot }}
+              <br />
+            </div>
+          </template>
+          <template #[`item.comment`]="{ item }">
+            {{ item.comment }}
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -27,19 +61,101 @@ export default Vue.extend({
       headers: [
         { text: "Nom", value: "fullname" },
         { text: "Tél", value: "phone" },
+        { text: "Mail", value: "email" },
+        {
+          text: "Plaque",
+          value: "licensePlate",
+          width: "10%",
+          sortable: false,
+        },
+        { text: "Horaire", value: "timeslot", sortable: false },
+        {
+          text: "Commentaire",
+          value: "comment",
+          width: "30%",
+          sortable: false,
+        },
       ],
       pass: [],
     };
   },
 
   async beforeMount() {
-    await this.getAllPassSecu();
+    if (this.$accessor.user.hasRole("secu")) {
+      await this.getAllPassSecu();
+    } else {
+      await this.$router.push({
+        path: "/",
+      });
+    }
   },
 
   methods: {
     async getAllPassSecu() {
       this.pass = (await this.$axios.get("/passsecu")).data;
-      console.log(this.passsecu);
+    },
+    download(filename: string, text: string) {
+      // We use the 'a' HTML element to incorporate file generation into
+      // the browser rather than server-side
+      const element = document.createElement("a");
+      element.setAttribute(
+        "href",
+        "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+      );
+      element.setAttribute("download", filename);
+
+      element.style.display = "none";
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    },
+
+    async exportCSV() {
+      // Parse data into a CSV string to be passed to the download function
+      let csv =
+        "Email;ID;Nom Prenom;Pass_entite;Imatriculation;Vendredi 18-20;Vendredi 20-5;Samedi 5-20;Samedi 20-5;Dimanche 5-20;Dimanche 20-00;PS1;PS2;PS3;PS4;PS5 \n";
+      for (let i = 0; i < this.pass.length; i++) {
+        csv +=
+          this.pass[i].email +
+          ";" +
+          i +
+          ";" +
+          this.pass[i].fullname +
+          ";;" +
+          this.pass[i].licensePlate +
+          ";" +
+          (this.pass[i].timeslot
+            ? this.pass[i].timeslot.includes("Vendredi 18h-20h")
+            : "false") +
+          ";" +
+          (this.pass[i].timeslot
+            ? this.pass[i].timeslot.includes("vendredi 20h-5h")
+            : "false") +
+          ";" +
+          (this.pass[i].timeslot
+            ? this.pass[i].timeslot.includes("samedi 5h-20h")
+            : "false") +
+          ";" +
+          (this.pass[i].timeslot
+            ? this.pass[i].timeslot.includes("samedi 20h-5h")
+            : "false") +
+          ";" +
+          (this.pass[i].timeslot
+            ? this.pass[i].timeslot.includes("dimanche 5h-20h")
+            : "false") +
+          ";" +
+          (this.pass[i].timeslot
+            ? this.pass[i].timeslot.includes("dicmanche 20h-00h")
+            : "false") +
+          ";;;;;" +
+          "\n";
+      }
+
+      const regex = new RegExp(/undefined/i, "g");
+
+      let parsedCSV = csv.replaceAll(regex, "");
+      // Prompt the browser to start file download
+      this.download("passsecu.csv", parsedCSV);
     },
   },
 });
