f003776707dfa9bb293e87fb4572f1b916462e50
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2022-04-06 22:21:45 +0000

    Resolve "ajout d'un bouton pour modifier les pass secu"

--- frontend/components/organisms/form/PassSecuCard.vue
+++ frontend/components/organisms/form/PassSecuCard.vue
@@ -1,37 +1,62 @@
 <template>
-  <v-card>
-    <v-card-title>Pass Sécu</v-card-title>
-    <v-card-text>
-      <v-switch
-        v-model="isPassSecuRequired"
-        label="Besoin de pass sécu"
-      ></v-switch>
+  <div>
+    <v-card>
+      <v-card-title>Pass Sécu</v-card-title>
+      <v-card-text>
+        <v-switch
+          v-model="isPassSecuRequired"
+          label="Besoin de pass sécu"
+        ></v-switch>
 
-      <v-data-table
-        :headers="headers"
-        :items="securityPasses"
-        :items-per-page="-1"
-        hide-default-footer
-      >
-        <template #[`item.action`]="{ index }">
-          <v-btn icon small @click="removeSecurityPass(index)">
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-        </template>
-      </v-data-table>
-
-      <template v-if="isPassSecuRequired">
-        <OverForm
-          :fields="FORM"
-          :disabled="isDisabled"
-          @form-change="onFormChange"
-        ></OverForm>
-        <v-btn text @click="addSecurityPass"
-          >Ajouter demande de pass secu</v-btn
+        <v-data-table
+          :headers="headers"
+          :items="securityPasses"
+          :items-per-page="-1"
+          hide-default-footer
         >
-      </template>
-    </v-card-text>
-  </v-card>
+          <template #[`item.action`]="row">
+            <v-btn icon small @click="removeSecurityPass(row.index)">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+            <v-btn icon small @click="modifSecurityPass(row)">
+              <v-icon small>mdi-circle-edit-outline</v-icon>
+            </v-btn>
+          </template>
+        </v-data-table>
+
+        <template v-if="isPassSecuRequired">
+          <OverForm
+            :fields="FORM"
+            :disabled="isDisabled"
+            @form-change="onFormChange"
+          ></OverForm>
+          <v-btn text @click="addSecurityPass"
+            >Ajouter demande de pass secu</v-btn
+          >
+        </template>
+      </v-card-text>
+    </v-card>
+
+    <v-dialog v-model="isModifPassDialogOpen" width="600">
+      <v-card>
+        <v-card-title>Modification du pass</v-card-title>
+        <OverForm
+          style="margin: 20px"
+          :fields="FORM"
+          :data="mPass"
+          :disabled="isDisabled"
+          @form-change="onModifFormChange"
+        ></OverForm>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="saveSecurityPass"> Enregistrer </v-btn>
+          <v-btn color="error" @click="isModifPassDialogOpen = false">
+            Annuler
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -73,6 +98,9 @@ export default {
         { text: "Action", value: "action" },
       ],
       newSecurityPass: {},
+      mPass: {},
+      mPassIndex: 0,
+      isModifPassDialogOpen: false,
     };
   },
   computed: {
@@ -90,11 +118,26 @@ export default {
     onFormChange(form) {
       this.newSecurityPass = form;
     },
+    onModifFormChange(form) {
+      this.mPass = { ...this.mPass, ...form };
+    },
     addSecurityPass() {
       this.$accessor.FA.addSecurityPass(this.newSecurityPass);
     },
+    saveSecurityPass() {
+      this.$accessor.FA.updateSecurityPass({
+        index: this.mPassIndex,
+        securityPass: this.mPass,
+      });
+      this.isModifPassDialogOpen = false;
+    },
     removeSecurityPass(index) {
       this.$accessor.FA.deleteSecurityPass(index);
+    },
+    modifSecurityPass(pass) {
+      this.mPass = pass.item;
+      this.mPassIndex = pass.index;
+      this.isModifPassDialogOpen = true;
     },
   },
 };
