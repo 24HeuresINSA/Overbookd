0ca52e6f1136de08a626284ca1e1cd10f896df65
DamienCarreau <carreau.damien@gmail.com>
2022-04-09 17:51:54 +0200

    Resolve duplicate in planning page

--- frontend/pages/planning.vue
+++ frontend/pages/planning.vue
@@ -14,7 +14,7 @@
       <v-list v-for="(plan, index) in orgaRequis" :key="index" class="my-4">
         <v-list-item>
           <v-list-item-content>
-            <ShowCalendar :title="plan._id" :slots="plan.slots" />
+            <ShowCalendar :title="plan.userName" :slots="plan.slots" />
             <v-data-table
               :headers="[
                 { text: 'FT', value: 'name', width: '30%' },
