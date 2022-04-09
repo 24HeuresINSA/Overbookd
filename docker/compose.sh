a2316750b2685c5526dd3b74c581a1b357c23b55
titouan-joseph <titouanjoseph@gmail.com>
2022-04-09 16:17:49 +0200

    feat(docker): :art: nex dev workflow

--- docker/compose.sh
+++ docker/compose.sh
@@ -5,7 +5,28 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 case $1 in
 
 "--dev"|"-d") echo "starting dev containers"
-        MY_GID=$(id -g $USER) MY_UID=$(id -u $USER) docker-compose -f $SCRIPT_DIR/docker-compose-dev.yml -p dev --env-file $SCRIPT_DIR/dev.env up ${@:2}
+        case $2 in
+        "--start"|"-s")
+                MY_GID=$(id -g $USER) MY_UID=$(id -u $USER) docker-compose -f $SCRIPT_DIR/docker-compose-dev.yml -p dev --env-file $SCRIPT_DIR/dev.env up -d
+                ;;
+        "--down"|"-d")
+                MY_GID=$(id -g $USER) MY_UID=$(id -u $USER) docker-compose -f $SCRIPT_DIR/docker-compose-dev.yml -p dev --env-file $SCRIPT_DIR/dev.env down
+                ;;
+        "--restart"|"-r")
+                MY_GID=$(id -g $USER) MY_UID=$(id -u $USER) docker-compose -f $SCRIPT_DIR/docker-compose-dev.yml -p dev --env-file $SCRIPT_DIR/dev.env restart
+                ;;
+        "--logs"|"-l")
+                MY_GID=$(id -g $USER) MY_UID=$(id -u $USER) docker-compose -f $SCRIPT_DIR/docker-compose-dev.yml -p dev --env-file $SCRIPT_DIR/dev.env logs -f
+                ;;
+        "-h"|"--help"|*)
+                echo "Usage: ./compose.sh [--dev|-d] [--start|-s] [--down|-d] [--restart|-r] [--logs|-l]"
+                echo "--dev|-d: start dev containers"
+                echo "--start|-s: start dev containers"
+                echo "--down|-d: stop dev containers"
+                echo "--restart|-r: restart dev containers"
+                echo "--logs|-l: show dev containers logs"
+                ;;
+        esac
          ;;
 
 "--utils"|"-u") echo "starting utils containers"
@@ -26,12 +47,12 @@ case $1 in
          docker-compose -f $SCRIPT_DIR/docker-compose-preprod.yml -p preprod --env-file $SCRIPT_DIR/.env up -d
          ;;
 
-*) echo "USAGE"
+"-h"|"--help"|*) echo "USAGE"
 echo "./compose.sh [option]"
 echo "You should use one option :"
 echo "--dev, -d [docker-compose' options]"
 echo "\t Start all needed for development. You need dev.env and ./assets/traefik/tls.yml files to work"
-echo "\t You can add additional docker-compose options here (e.g. --build, --detach, --force-recreate. More information here https://docs.docker.com/compose/reference/up/)"
+echo "\t for more info about this option see ./compose.sh --dev --help"
 echo "--utils, -u"
 echo "\t Strat Traefik and Watchtower services"
 echo "--pord, -p"
