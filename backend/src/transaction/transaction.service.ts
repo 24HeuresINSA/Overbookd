e6f87c85e05a81bd8271be2341ab4a0f73a87319
AntoinePiron <pro.piron.antoine@gmail.com>
2022-09-29 11:58:04 +0200

    feat(backend): :zap: update transaction service

--- backend/src/transaction/transaction.service.ts
+++ backend/src/transaction/transaction.service.ts
@@ -6,6 +6,10 @@ import { Transaction, Prisma } from '@prisma/client';
 export class TransactionService {
   constructor(private prisma: PrismaService) {}
 
+  /**     **/
+  /** GET **/
+  /**     **/
+
   async getAllTransactions(): Promise<Transaction[]> {
     return this.prisma.transaction.findMany();
   }
@@ -14,12 +18,24 @@ export class TransactionService {
     return this.prisma.transaction.findUnique({ where: { id: Number(id) } });
   }
 
+  async getUserTransactions(userId: number): Promise<Transaction[] | null> {
+    return this.prisma.transaction.findMany({
+      where: { OR: [{ from: userId }, { to: userId }] },
+    });
+  }
+
+  /**      **/
+  /** POST **/
+  /**      **/
   async createTransaction(
     data: Prisma.TransactionCreateInput,
   ): Promise<Transaction> {
     return this.prisma.transaction.create({ data });
   }
 
+  /**        **/
+  /** DELETE **/
+  /**        **/
   async deleteTransaction(id: number): Promise<Transaction> {
     return this.prisma.transaction.delete({ where: { id: Number(id) } });
   }
