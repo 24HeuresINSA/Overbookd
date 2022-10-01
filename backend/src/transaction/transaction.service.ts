9707a1f36bb6b3d0e01cf21f2e822e4686f739de
AntoinePiron <pro.piron.antoine@gmail.com>
2022-10-01 12:50:40 +0200

    feat(backend): :sparkles: deposit now update user balance

--- backend/src/transaction/transaction.service.ts
+++ backend/src/transaction/transaction.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Transaction, Prisma } from '@prisma/client';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class TransactionService {
@@ -9,7 +10,6 @@ export class TransactionService {
   /**     **/
   /** GET **/
   /**     **/
-
   async getAllTransactions(): Promise<Transaction[]> {
     return this.prisma.transaction.findMany();
   }
@@ -30,6 +30,60 @@ export class TransactionService {
   async createTransaction(
     data: Prisma.TransactionCreateInput,
   ): Promise<Transaction> {
+    //We check the transaction type
+    switch (data.type) {
+      case 'TRANSFER':
+        //If the transaction is a transfer, we check that the sender and the receiver are different
+        if (data.from === data.to) {
+          throw new HttpException(
+            'Sender and receiver must be different',
+            HttpStatus.FORBIDDEN,
+          );
+        }
+        break;
+      case 'DEPOSIT':
+        //If the transaction is a deposit, we check that the sender is the same as the receiver
+        if (data.from !== data.to) {
+          throw new HttpException(
+            'Sender and receiver must be the same',
+            HttpStatus.FORBIDDEN,
+          );
+        }
+        //If the amount is negative, we throw an error
+        if (data.amount < 0) {
+          throw new HttpException(
+            'Amount must be positive',
+            HttpStatus.FORBIDDEN,
+          );
+        }
+        //We check that the sender is a user
+        const user = await this.prisma.user.findUnique({
+          where: { id: Number(data.from) },
+        });
+        if (!user) {
+          throw new HttpException(
+            'Sender must be a user',
+            HttpStatus.FORBIDDEN,
+          );
+        }
+        //Compute the new balance of the user
+        const newBalance = user.balance + data.amount;
+        //Update the balance of the user
+        await this.prisma.user.update({
+          where: { id: Number(data.from) },
+          data: { balance: newBalance },
+        });
+        break;
+      case 'EXPENSE':
+        console.log('expense');
+        break;
+      default:
+        //If the transaction type is not valid, we throw an error
+        throw new HttpException(
+          'Unknown transaction type',
+          HttpStatus.FORBIDDEN,
+        );
+    }
     return this.prisma.transaction.create({ data });
   }
 
