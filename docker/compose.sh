89c22f2e03597e5c9c5ceebd45642d802d7fc2cd
titouan-joseph <titouanjoseph@gmail.com>
2022-10-01 19:24:44 +0200

    feat(Global): :sparkles: add nex command & help

--- docker/compose.sh
+++ docker/compose.sh
@@ -44,6 +44,11 @@ case $1 in
                 echo "-----------------------------"
                 MY_GID=$(id -g $USER) MY_UID=$(id -u $USER) docker compose -f $SCRIPT_DIR/docker-compose-dev.yml -p dev --env-file $SCRIPT_DIR/dev.env exec backend $3
                 ;;
+        "--bash"|"-b")
+                echo "Bash command dev containers"
+                echo "---------------------------"
+                docker run --rm -it --user $(id -g $USER):$(id -u $USER) -v $SCRIPT_DIR/..:/app node:16.14-alpine3.15 sh
+                ;;
         "-h"|"--help"|*)
                 echo "Usage: ./compose.sh [--dev|-d] [--start|-s] [--down|-d] [--restart|-r] [--logs|-l]"
                 echo "--dev|-d: start dev containers"
@@ -51,6 +56,8 @@ case $1 in
                 echo "--down|-d: stop dev containers"
                 echo "--restart|-r: restart dev containers"
                 echo "--logs|-l: show dev containers logs"
+                echo "--prisma|-p: run prisma command in backend container. Example: ./compose.sh --dev --prisma 'npx prisma migrate dev'"
+                echo "--bash|-b: run interactive bash terminal in onetime node container. To install dependancies for example. Overbookd folder is mounted in /app"
                 ;;
         esac
          ;;
