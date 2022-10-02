8e52e1ff3c0d22e680da1fec1bb7968f751d59e1
AntoinePiron <pro.piron.antoine@gmail.com>
2022-10-02 21:19:51 +0200

    fix(Database): :zap: change db to snake_case

--- backend/prisma/migrations/20221001110424_update_transaction/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Transaction" ALTER COLUMN "isValide" SET DEFAULT true;
---
+++ backend/prisma/migrations/20221002191700_snake_case/migration.sql
@@ -0,0 +1,40 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `driverLicenseDate` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `hasDriverLicense` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `isDeleted` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - Added the required column `driver_license_date` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('DEPOSIT', 'EXPENSE', 'TRANSFER');
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "driverLicenseDate",
+DROP COLUMN "hasDriverLicense",
+DROP COLUMN "isDeleted",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "driver_license_date" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "has_driver_license" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "from" INTEGER NOT NULL,
+    "to" INTEGER NOT NULL DEFAULT -1,
+    "amount" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "context" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "is_valid" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
