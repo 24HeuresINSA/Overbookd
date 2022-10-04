1a1669cef01658536000b8e1950582a6932975e8
DamienCarreau <carreau.damien@gmail.com>
2022-10-04 18:52:48 +0200

    First middleware POC

--- backend/src/transaction/transaction.module.ts
+++ backend/src/transaction/transaction.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer  } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { PrismaService } from '../prisma.service';
+import { logger } from '../middleware/logger.middleware';
 
 @Module({
   imports: [],
   controllers: [TransactionController],
   providers: [TransactionService, PrismaService],
 })
-export class TransactionModule {}
+export class TransactionModule implements NestModule {
+  configure(cusumer: MiddlewareConsumer) {
+    cusumer
+      .apply(logger)
+      .forRoutes(TransactionController);
+  }
+}
