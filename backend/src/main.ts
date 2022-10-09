1e01afacd008f984991b22cf3a0e36d67b1fd5eb
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2022-10-09 13:15:14 +0200

    feat(backend): 🚨 CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://overbookd.24heures.org/',
+  'https://preprod.overbookd.24heures.org/',
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
 
   //Create swagger
   const config = new DocumentBuilder()
