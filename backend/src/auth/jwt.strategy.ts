ef81a26270225b396eeb378ae36170a032bab8ce
titouan-joseph <titouanjoseph@gmail.com>
2022-10-10 18:50:38 +0200

    fix(Back): :ambulance: correct key for jwt

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,6 +1,6 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
 
 @Injectable()
@@ -17,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     return {
       userId: payload.userId,
       username: payload.username,
-      role: payload.teams,
+      role: payload.role,
     };
   }
 }
