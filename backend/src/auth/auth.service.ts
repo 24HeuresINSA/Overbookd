d54cdadf18a291894b4692722f64db2db5b0f7ee
titouan-joseph <titouanjoseph@gmail.com>
2022-10-17 20:04:08 +0200

    fix(Back): :rotating_light:

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import {
   UserPasswordOnly,
   UserService,
