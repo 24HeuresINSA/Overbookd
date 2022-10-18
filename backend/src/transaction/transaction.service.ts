3fdb76daca3feb0e827210f2ffbbb0ce48147418
AntoinePiron <pro.piron.antoine@gmail.com>
2022-10-18 09:10:19 +0200

    feat(transaction): :recycle: better way to check if user exists

--- backend/src/transaction/transaction.service.ts
+++ backend/src/transaction/transaction.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Transaction } from '@prisma/client';
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class TransactionService {
@@ -38,15 +39,10 @@ export class TransactionService {
       );
     }
     //Check if user exists
-    const sender = await this.prisma.user.findUnique({
-      where: { id: Number(data.from) },
-    });
-    const receiver = await this.prisma.user.findUnique({
-      where: { id: Number(data.to) },
-    });
-    if (!sender || !receiver) {
-      throw new HttpException('User does not exist', HttpStatus.BAD_REQUEST);
-    }
+    const users = await this.userExists([data.from, data.to]);
+    const sender = users.find((user) => user.id === data.from);
+    const receiver = users.find((user) => user.id === data.to);
+
     const senderBalance = sender.balance - data.amount;
     const receiverBalance = receiver.balance + data.amount;
     this.prisma.$transaction([
@@ -71,12 +67,8 @@ export class TransactionService {
       //Check if user exists
       const userId =
         transaction.from === -1 ? transaction.to : transaction.from; //We only deal with expense and deposit
-      const user = await this.prisma.user.findUnique({
-        where: { id: Number(userId) },
-      });
-      if (!user) {
-        throw new HttpException('User does not exist', HttpStatus.NOT_FOUND);
-      }
+      const users = await this.userExists([userId]);
+      const user = users.find((user) => user.id === userId);
       const newBalance =
         transaction.type === 'DEPOSIT'
           ? user.balance + transaction.amount
@@ -93,15 +85,6 @@ export class TransactionService {
     return transactions;
   }
 
-  isTransactionOK(transaction: Transaction): void {
-    if (transaction.amount <= 0) {
-      throw new HttpException(
-        'Amount must be positive',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-  }
-
   /**        **/
   /** DELETE **/
   /**        **/
@@ -111,5 +94,27 @@ export class TransactionService {
       where: { id: Number(id) },
       data: { is_deleted: true },
     });
+  }
+
+  /**         **/
+  /** HELPERS **/
+  /**         **/
+  isTransactionOK(transaction: Transaction): void {
+    if (transaction.amount <= 0) {
+      throw new HttpException(
+        'Amount must be positive',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async userExists(userIds: number[]): Promise<User[]> {
+    const users = await this.prisma.user.findMany({
+      where: { id: { in: userIds } },
+    });
+    if (users.length !== userIds.length) {
+      throw new HttpException('User does not exist', HttpStatus.NOT_FOUND);
+    }
+    return users;
   }
 }
