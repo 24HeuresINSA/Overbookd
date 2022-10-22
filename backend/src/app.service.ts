7e58669004304db54c81ea4d4c2515df7380ce36
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2022-10-22 16:45:21 +0000

    fix(Docker): :ambulance: change start:prod script

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { version } from '../package.json';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello from overbookd backend V${version}!`;
   }
 }
