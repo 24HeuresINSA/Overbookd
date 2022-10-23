d29dfe7e12c80a06046e56ce4fb23754820b9fb4
Léo Mouyna <mouynaleo@gmail.com>
2022-10-23 19:07:12 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- backend/src/team/team.service.ts
+++ backend/src/team/team.service.ts
@@ -32,43 +32,50 @@ export class TeamService {
     return teams.map((team) => team.name);
   }
 
-  async updateUserTeams(
-    payload: LinkTeamToUserDto,
-  ): Promise<LinkTeamToUserDto> {
-    // Verify user exists
-    const user = await this.userService.user({ id: payload.userId });
-    if (!user.id) {
-      throw new NotFoundException('User not found');
-    }
-    // Take only the teams that exist
-    const teamsToLink = await this.prisma.team.findMany({
-      where: {
-        name: {
-          in: payload.teams,
-        },
-      },
-    });
+  async updateUserTeams({
+    userId,
+    teams,
+  }: LinkTeamToUserDto): Promise<LinkTeamToUserDto> {
+    await this.checkUserExistence(userId);
+    const teamsToLink = await this.fetchExistingTeams(teams);
+    await this.forceUserTeams(userId, teamsToLink);
 
+    const newLinkedTeams = teamsToLink.map((team) => team.name);
+    return { userId, teams: newLinkedTeams };
+  }
+
+  private async forceUserTeams(userId: number, teamsToLink: Team[]) {
     const deleteAll = this.prisma.user_Team.deleteMany({
       where: {
-        user_id: payload.userId,
+        user_id: userId,
       },
     });
 
     const createNew = this.prisma.user_Team.createMany({
       data: teamsToLink.map((team) => ({
-        user_id: payload.userId,
+        user_id: userId,
         team_id: team.name,
       })),
     });
 
-    try {
-      await this.prisma.$transaction([deleteAll, createNew]);
-    } catch (e) {
-      throw new Error(e);
+    return this.prisma.$transaction([deleteAll, createNew]);
+  }
+
+  private async fetchExistingTeams(teams: string[]): Promise<Team[]> {
+    return await this.prisma.team.findMany({
+      where: {
+        name: {
+          in: teams,
+        },
+      },
+    });
+  }
+
+  private async checkUserExistence(id: number): Promise<void> {
+    const user = await this.userService.user({ id });
+    if (!user.id) {
+      throw new NotFoundException('User not found');
     }
-    payload.teams = teamsToLink.map((team) => team.name);
-    return payload;
   }
 
   async createTeam(payload: { name: string }): Promise<Team> {
