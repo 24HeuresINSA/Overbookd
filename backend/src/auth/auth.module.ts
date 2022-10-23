f8920d6f536da0e6a8107e9b84a9f3941007dfee
titouan-joseph <titouanjoseph@gmail.com>
2022-10-23 23:37:04 +0200

    style: :white_check_mark: all tests pass
    Issue #611

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,15 +1,15 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
-import { HashingUtilsModule } from 'src/hashing-utils/hashing-utils.module';
-import { HashingUtilsService } from 'src/hashing-utils/hashing-utils.service';
-import { UserService } from 'src/user/user.service';
-import { PrismaService } from 'src/prisma.service';
-import { MailService } from 'src/mail/mail.service';
+import { MailService } from '../mail/mail.service';
+import { HashingUtilsModule } from '../hashing-utils/hashing-utils.module';
+import { HashingUtilsService } from '../hashing-utils/hashing-utils.service';
+import { UserService } from '../user/user.service';
+import { PrismaService } from '../prisma.service';
 import { AuthController } from './auth.controller';
 
 @Module({
