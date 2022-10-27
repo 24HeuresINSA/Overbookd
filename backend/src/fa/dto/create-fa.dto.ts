75d33e6ab9528ab24622e9f57c948d64d8cdafec
AntoinePiron <pro.piron.antoine@gmail.com>
2022-10-27 14:14:27 +0200

    feat(FA Backend): :zap: Better DTO with Class validator

--- backend/src/fa/dto/create-fa.dto.ts
+++ backend/src/fa/dto/create-fa.dto.ts
@@ -1,4 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export enum Status {
   DRAFT = 'DRAFT',
@@ -12,6 +20,8 @@ export class CreateFaDto {
     required: true,
     description: 'The name of the fa',
   })
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({
@@ -24,24 +34,29 @@ export class CreateFaDto {
     required: true,
     description: 'The type of the fa',
   })
+  @IsString()
+  @IsNotEmpty()
   type: string;
 
   @ApiProperty({
     required: true,
     description: 'The id of the team whos responsible of the fa',
   })
+  @IsNumber()
   team_id: number;
 
   @ApiProperty({
     required: true,
     description: 'The id of the user who is responsible of the fa',
   })
+  @IsNumber()
   in_charge: number;
 
   @ApiProperty({
     required: true,
     description: 'The id of the location of the fa',
   })
+  @IsNumber()
   location_id: number;
 
   @ApiProperty({
@@ -55,53 +70,68 @@ export class CreateFaDto {
     required: true,
     description: 'The description of the fa',
   })
+  @IsString()
+  @IsNotEmpty()
   description: string;
 
   @ApiProperty({
     required: true,
     description: 'Is the activity publishable on the website',
   })
+  @IsBoolean()
   is_publishable: boolean;
 
   @ApiProperty({
     required: true,
     description: 'Is the activty a major activity',
   })
+  @IsBoolean()
   is_major: boolean;
 
   @ApiProperty({
     required: true,
     description: 'Is the activity for kids',
   })
+  @IsBoolean()
   is_kids: boolean;
 
   @ApiProperty({
     required: false,
     description: 'The id of the users who validated the fa',
   })
+  @IsArray()
+  @IsOptional()
   validated_by: number[];
 
   @ApiProperty({
     required: false,
     description: 'The id of the users who refused the fa',
   })
+  @IsArray()
+  @IsOptional()
   refused_by: number[];
 
   @ApiProperty({
     required: false,
     description: 'The security needs',
   })
+  @IsString()
+  @IsOptional()
   security_needs: string;
 
   @ApiProperty({
     required: false,
     description: 'All of the collaborators ids',
   })
+  @IsArray()
+  @IsOptional()
   collaborator_id: number[];
 
   @ApiProperty({
     required: false,
     description: 'The waterflow needed',
   })
+  @IsNumber()
+  @IsOptional()
   waterflow_required: number;
 }
