8e3526736e58b6dea3f8598ed59f7884e50a62ec
AntoinePiron <pro.piron.antoine@gmail.com>
2022-10-29 19:01:36 +0200

    feat(FA Backend): :sparkles: Update controller with swagger and guards
    #595

--- backend/src/fa/fa.controller.ts
+++ backend/src/fa/fa.controller.ts
@@ -6,37 +6,92 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { FaService } from './fa.service';
 import { CreateFaDto } from './dto/create-fa.dto';
 import { UpdateFaDto } from './dto/update-fa.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/team-auth.guard';
+import { Roles } from 'src/auth/team-auth.decorator';
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { FA } from '@prisma/client';
 
+@ApiBearerAuth()
+@ApiTags('fa')
 @Controller('fa')
 export class FaController {
   constructor(private readonly faService: FaService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('hard')
   @Post()
-  create(@Body() createFaDto: CreateFaDto) {
+  @ApiBody({
+    description: 'Create a new fa',
+    type: CreateFaDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Create a new fa',
+    type: Promise<FA | null>,
+  })
+  create(@Body() createFaDto: CreateFaDto): Promise<FA | null> {
     return this.faService.create(createFaDto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('hard')
   @Get()
-  findAll() {
+  @ApiResponse({
+    status: 200,
+    description: 'Get all fa',
+    type: Array,
+  })
+  findAll(): Promise<FA[] | null> {
     return this.faService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('hard')
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @ApiResponse({
+    status: 200,
+    description: 'Get a fa',
+    type: Promise<FA | null>,
+  })
+  findOne(@Param('id', ParseIntPipe) id: string): Promise<FA | null> {
     return this.faService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('hard')
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFaDto: UpdateFaDto) {
+  @ApiResponse({
+    status: 201,
+    description: 'Update a fa',
+    type: Promise<FA | null>,
+  })
+  @ApiBody({
+    description: 'Update a fa',
+    type: UpdateFaDto,
+  })
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateFaDto: UpdateFaDto,
+  ): Promise<FA | null> {
     return this.faService.update(+id, updateFaDto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('hard')
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  @ApiResponse({
+    status: 201,
+    description: 'Delete a fa',
+    type: Promise<FA | null>,
+  })
+  remove(@Param('id', ParseIntPipe) id: string): Promise<FA | null> {
     return this.faService.remove(+id);
   }
 }
