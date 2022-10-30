c1cec0e17974ec18bcedadfdf02c4fc52c31d27f
Tayaress09 <leon.dumestre@gmail.com>
2022-10-30 10:39:03 -0400

    refactor: :art: Replacement Overform in Signup Page

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -1,73 +1,167 @@
 <template>
   <div>
     <h1>Inscription 👋</h1>
-    <OverForm
-      v-if="signupForm"
-      :fields="signupForm"
-      @form-change="onFormChange"
-    ></OverForm>
-    <p>
-      Une fois le formulaire remplit veuillez vous connecter à Overbookd pour
-      remplir vos dispos ! <br />
-      <span class="important"
-        >Pensez à immédiatement les remplir pour être accepté au plus vite
-        !</span
-      >
-    </p>
-    <v-btn color="primary" @click="submitForm">Envoyer</v-btn>
+    <v-form v-model="formData.isValid">
+      <v-text-field
+        v-model="formData.firstname"
+        type="given-name"
+        label="Prénom*"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="formData.lastname"
+        type="family-name"
+        label="Nom*"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="formData.nickname"
+        label="Surnom"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="formData.password"
+        type="password"
+        label="Mot de passe*"
+        :rules="
+          [
+            (v) =>
+              new RegExp(`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$`).test(v) ||
+              (`au moins une MAJUSCULE, minuscule et un chiffre et au moins 6 caractères`),
+          ]
+        "
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="formData.password2"
+        type="password"
+        label="Confirme ton mot de passe*"
+        :rules="
+          [
+            (v) =>
+              new RegExp(`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$`).test(v) ||
+              (`au moins une MAJUSCULE, minuscule et un chiffre et au moins 6 caractères`),
+          ]
+        "
+        required
+      ></v-text-field>
+
+      <div>
+        <p>Date de naissance*</p>
+        <v-date-picker
+          v-model="formData.birthdate"
+          label="Date de naissance*"
+          :active-picker.sync="activePicker"
+          required
+        ></v-date-picker>
+      </div>
+
+      <v-text-field
+        v-model="formData.email"
+        type="email"
+        label="Gmail (important que ca soit une adresse gmail)*"
+        :rules="
+          [
+            (v) =>
+              new RegExp(`^.+@(gmail.com|24heures.org)$`).test(v) ||
+              (`il y a un problème avec ce champ`),
+          ]
+        "
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="formData.phone"
+        type="tel"
+        label="Ton 06 ?*"
+        :rules="
+          [
+            (v) =>
+              new RegExp(`0[6-7]{1}[0-9]{8}$`).test(v) ||
+              (`il y a un problème avec ce champ`),
+          ]
+        "
+        required
+      ></v-text-field>
+
+      <v-select
+        v-model="formData.department"
+        type="select"
+        label="Département (obligatoire même si non INSA)*"
+        :items="['TC', 'IF', 'BS', 'GCU', 'SGM', 'GI', 'GM', 'GEN', 'FIMI', 'GE', 'AUTRE']"
+        dense
+        required
+      ></v-select>
+
+      <v-select
+        v-model="formData.year"
+        type="select"
+        label="Année (obligatoire même si non INSA)*"
+        :items="['A1', 'A2', 'A3', 'A4', 'A5', 'VIEUX', 'AUTRE']"
+        dense
+        required
+      ></v-select>
+
+      <v-text-field
+        v-model="formData.comment"
+        label="Commentaire"
+      ></v-text-field>
+
+      <p>
+        Une fois le formulaire remplit veuillez vous connecter à Overbookd pour
+        remplir vos dispos ! <br />
+        <span class="important"
+          >Pensez à immédiatement les remplir pour être accepté au plus vite
+          !</span
+        >
+      </p>
+      <v-btn color="primary" @click="submitForm">Envoyer</v-btn>
+    </v-form>
   </div>
 </template>
 
 <script>
-import { getConfig } from "~/common/role";
-import OverForm from "../components/overForm";
-
 export default {
   name: "Signup",
-  components: { OverForm },
   auth: false,
   layout: "none",
 
-  data() {
+  data: () => {
     return {
-      signupForm: undefined,
-      compiledForm: undefined,
-    };
+      formData: {
+        type: Object,
+        default: () => undefined,
+      },
+      activePicker: null,
+    }
   },
 
   async mounted() {
-    // fetch from API to check that sign ups are open
-    const isSignupOpen = this.getConfig("isSignupOpen");
+    const isSignupOpen = true;  //Close or open signup
     if (!isSignupOpen) {
-      alert(this.getConfig("fb_signup_closed"));
+      alert("les inscriptions sont fermées 😱");
       await this.$router.push({
         path: "/login",
       });
-    } else {
-      this.signupForm = this.getConfig("signup_form");
     }
   },
 
   methods: {
-    getConfig(key) {
-      return getConfig(this, key);
-    },
-
-    onFormChange(form) {
-      this.compiledForm = form;
-    },
-
     submitForm() {
-      if (this.compiledForm.password !== this.compiledForm.password2) {
+      console.log(this.formData);
+      if (this.formData.password !== this.formData.password2) {
         alert("Les deux mots de passes ne sont pas les mêmes");
-      } else if (!this.compiledForm.isValid) {
+      } else if (!this.formData.isValid) {
         alert("Les champs avec * sont obligatoires");
       } else {
-        delete this.compiledForm.password2;
-        delete this.compiledForm.isValid;
-        const oldDate = this.compiledForm.birthdate;
-        this.compiledForm.birthdate = new Date(oldDate).toISOString();
-        this.$axios.post("/user", this.compiledForm);
+        delete this.formData.password2;
+        delete this.formData.isValid;
+        const oldDate = this.formData.birthdate;
+        this.formData.birthdate = new Date(oldDate).toISOString();
+        this.$axios.post("/user", this.formData);
         this.$router.push({
           path: "/login",
         });
