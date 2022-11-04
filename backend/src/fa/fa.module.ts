60e0b2fe9697a74e627d81c6ed51fe8601a55413
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-04 15:06:03 +0100

    fix(Back): :bug: add prismaService for FA
    #595

--- backend/src/fa/fa.module.ts
+++ backend/src/fa/fa.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FaService } from './fa.service';
 import { FaController } from './fa.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [FaController],
-  providers: [FaService],
+  providers: [FaService, PrismaService],
 })
 export class FaModule {}
