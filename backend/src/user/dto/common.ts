96e5a8bfeda3180d08f7350f6471c58f252c58a1
titouan-joseph <titouanjoseph@gmail.com>
2022-11-08 21:24:48 +0100

    fix(User): :memo: update user DTO
    Issue #523

--- backend/src/user/dto/common.ts
+++ backend/src/user/dto/common.ts
@@ -1,2 +1,17 @@
 export const upperCaseCharacter = new RegExp(/[A-Z]+/);
 export const number = new RegExp(/[\d]+/);
+
+export const yearEnum = ['A1', 'A2', 'A3', 'A4', 'A5', 'VIEUX', 'AUTRE'];
+export const departementEnum = [
+  'TC',
+  'IF',
+  'GE',
+  'GM',
+  'GI',
+  'GCU',
+  'GEN',
+  'SGM',
+  'BS',
+  'FIMI',
+  'AUTRE',
+];
