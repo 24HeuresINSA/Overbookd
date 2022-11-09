8c4be58a8ac63e603cac1e3cb981898957294ae2
titouan-joseph <titouanjoseph@gmail.com>
2022-11-09 19:20:50 +0100

    style(Back): :monocle_face: no use as in user creation
    Issue #523

--- backend/src/user/dto/common.ts
+++ backend/src/user/dto/common.ts
@@ -1,25 +1,2 @@
 export const upperCaseCharacter = new RegExp(/[A-Z]+/);
 export const number = new RegExp(/[\d]+/);
-
-export enum Years {
-  A1 = 'A1',
-  A2 = 'A2',
-  A3 = 'A3',
-  A4 = 'A4',
-  A5 = 'A5',
-  VIEUX = 'VIEUX',
-  AUTRE = 'AUTRE',
-}
-export enum Departements {
-  TC = 'TC',
-  IF = 'IF',
-  GE = 'GE',
-  GM = 'GM',
-  GI = 'GI',
-  GCU = 'GCU',
-  GEN = 'GEN',
-  SGM = 'SGM',
-  BS = 'BS',
-  FIMI = 'FIMI',
-  AUTRE = 'AUTRE',
-}
