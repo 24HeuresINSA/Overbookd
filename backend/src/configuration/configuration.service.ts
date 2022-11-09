f9b4f4c58b964548bb93599e642a0a499010ce31
Lucas <lucas.zimmermann@protonmail.com>
2022-11-09 21:42:22 +0100

    fix(Back): Old import

--- backend/src/configuration/configuration.service.ts
+++ backend/src/configuration/configuration.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotImplementedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Prisma, Configuration } from '@prisma/client';
 import { CreateConfigurationDto } from './dto/createConfiguration.dto';
