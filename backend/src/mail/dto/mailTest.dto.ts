27638b7f123158619152f3080662ce11079f17d0
titouan-joseph <titouanjoseph@gmail.com>
2022-11-11 18:41:35 +0100

    feat(Back): :sparkles: add nodemailer
    Issue #523

---
+++ backend/src/mail/dto/mailTest.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsDefined, IsString } from 'class-validator';
+
+export class emailTestDto {
+  @ApiProperty({
+    required: true,
+    description: 'The email of the user',
+  })
+  @IsDefined()
+  @IsEmail()
+  to: string;
+
+  @ApiProperty({
+    required: true,
+    description: 'The email of the user',
+  })
+  @IsDefined()
+  @IsString()
+  username: string;
+}
