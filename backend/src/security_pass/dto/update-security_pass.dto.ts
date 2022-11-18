bbb0b1f0d2cc7b98a95f871199e972b05e03cb0f
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-18 09:32:10 +0100

    refactor(FA): :recycle: remove secu pass
    #595

--- backend/src/security_pass/dto/update-security_pass.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSecurityPassDto } from './create-security_pass.dto';
-
-export class UpdateSecurityPassDto extends PartialType(CreateSecurityPassDto) {}
