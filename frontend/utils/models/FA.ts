75edd6f9c9a372ce583c2da0e413f2b4b2d2183c
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-22 20:03:56 +0100

    feat(FA): :sparkles: beginning of the new store

--- frontend/utils/models/FA.ts
+++ frontend/utils/models/FA.ts
@@ -1,78 +1,67 @@
-import { FT } from "~/utils/models/FT";
+export enum Status {
+  DRAFT = "DRAFT",
+  SUBMITTED = "SUBMITTED",
+  VALIDATED = "VALIDATED",
+  REFUSED = "REFUSED",
+}
+
+export enum ElectricityType {
+  ELECTRICITY = "ELECTRICITY",
+  GAS = "GAS",
+  WATER = "WATER",
+  OTHER = "OTHER",
+}
+
+export enum SignaType {
+  BANNIERE = "BANNIERE",
+  PANNEAU = "PANNEAU",
+  PANCARTE = "PANCARTE",
+}
 
 export interface FA {
-  _id?: string;
-  count?: number;
-  status: string;
-  general?: {
-    locations?: string[];
-    name?: string;
-    team?: string;
-    isValid?: boolean;
-    isRequiringPass?: boolean;
-  };
-  details: {};
-  security: {};
-  equipments: { _id: string; name: string; required: number }[];
-  timeframes: { start: Date; end: Date; name: string }[];
-  validated: String[];
-  refused: String[];
-  comments: { time: Date; text: string; validator: string; topic?: string }[];
-  FTs: FT[];
-  isValid: boolean;
-  securityPasses: SecurityPass[];
-  signalisation: Signalisation[];
-  electricityNeeds: ElectricityNeed[];
-}
-
-export interface ElectricityNeed {
-  connectionType: string;
-  power: number;
-}
-
-export interface Signalisation {
+  id?: number;
   name: string;
-  type: string;
-  number: number;
-  text: string;
+  type?: string;
+  team_id?: number;
+  in_charge?: number;
+  created_at?: Date;
+  location_id?: number;
+  status: Status;
+  description?: string;
+  is_publishable?: boolean;
+  is_major?: boolean;
+  is_kids?: boolean;
+  security_needs?: boolean;
+  is_pass_required?: boolean;
+  number_of_pass?: number;
+  water_needs?: string;
+  water_flow_required?: string;
+  is_deleted?: boolean;
+  FA_Collaborators?: FA_Collaborators[];
+  FA_Electricity_needs?: FA_Electricity_needs[];
 }
 
-export interface SecurityPass {
-  fullname: string;
+export interface FA_Collaborators {
+  fa_id: number;
+  collaborator_id: number;
+  is_deleted: boolean;
+  Collaborator: Collaborator;
+}
+
+export interface Collaborator {
+  id: number;
+  firstname: string;
+  lastname: string;
   phone: string;
-  email: string;
-  comment: string;
-  licensePlate: string;
-  timeslots: string[];
+  email?: string;
+  company?: string;
+  comment?: string;
 }
 
-export enum EquipmentTypes {
-  BARS = "BARS",
-  BUREAUTIQUE = "BUREAUTIQUE",
-  BOIS = "BOIS",
-  CANAP = "CANAPE/FAUTEUIL",
-  CONSUMABLE = "CONSUMABLE",
-  CUISINE = "CUISINE",
-  DECO = "DECO",
-  FRIGO = "FRIGO",
-  LITERIE = "LITERIE",
-  MOBILIER = "MOBILIER",
-  PROPRETE = "PROPRETE",
-  SCENE = "SCENE",
-  SECU = "SECU",
-  SIGNA = "SIGNA",
-  OUTILLAGE = "OUTILLAGE",
-  TENTE = "TENTE",
-  AUTRE = "AUTRES MATOS",
-}
-
-export enum BarrieresTypes {
-  BARRIERE = "BARRIERE",
-}
-
-export enum ElecTypes {
-  ALIM = "ALIMENTATION ELECTRIQUE",
-  EAU = "EAU",
-  ECLAIRAGE = "ECLAIRAGE",
-  AUTRE = "AUTRES ELEC",
+export interface FA_Electricity_needs {
+  id: number;
+  fa_id: number;
+  electricity_type: ElectricityType;
+  power: number;
+  comment?: string;
 }
