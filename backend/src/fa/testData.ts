5966f84c4b75e8613b791e9f7c5e4762079fbd25
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-23 23:06:32 +0100

    test(FA): :white_check_mark: add test for signa
    #595

--- backend/src/fa/testData.ts
+++ backend/src/fa/testData.ts
@@ -1,4 +1,5 @@
 import { UpdateFaDto, Status } from './dto/update-fa.dto';
+import { signa_type } from '../fa_signa_needs/dto/create-fa_signa_need.dto';
 
 export const emptyFA: UpdateFaDto = {
   fa: {
@@ -28,7 +29,7 @@ export const collaboratorFA: UpdateFaDto = {
     in_charge: 1,
     location_id: 1,
     status: Status.DRAFT,
-    description: 'An FA wiht no links',
+    description: 'A FA with a Collaborator',
     is_publishable: true,
     is_major: false,
     is_kids: false,
@@ -38,6 +39,27 @@ export const collaboratorFA: UpdateFaDto = {
       firstname: 'Collaborator A',
       lastname: 'Collaborator A',
       phone: '0000000000',
+    },
+  ],
+};
+export const signaFA: UpdateFaDto = {
+  fa: {
+    name: 'Signa FA',
+    type: 'test',
+    team_id: 1,
+    in_charge: 1,
+    location_id: 1,
+    status: Status.DRAFT,
+    description: 'A FA with some signa',
+    is_publishable: true,
+    is_major: false,
+    is_kids: false,
+  },
+  fa_signa_needs: [
+    {
+      signa_type: signa_type.BANNIERE,
+      text: 'Bonjour',
+      count: 1,
     },
   ],
 };
