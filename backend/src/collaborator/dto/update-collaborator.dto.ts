7afa0a5beed95218ba49136c3b27fc1b6199dc56
AntoinePiron <pro.piron.antoine@gmail.com>
2022-11-28 09:25:00 +0100

    feat(FA): :fire: remove useless DTO
    #595

--- backend/src/collaborator/dto/update-collaborator.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCollaboratorDto } from './create-collaborator.dto';
-
-export class UpdateCollaboratorDto extends PartialType(CreateCollaboratorDto) {}
