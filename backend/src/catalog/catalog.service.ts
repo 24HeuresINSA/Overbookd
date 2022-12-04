858b41ad8542070aa3fee5130903d4cf17e47445
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-04 12:59:41 +0100

    fix(Catalog): :ambulance: fix missing export

--- backend/src/catalog/catalog.service.ts
+++ backend/src/catalog/catalog.service.ts
@@ -17,7 +17,7 @@ type GearUpdateForm = GearForm & {
   id: number;
 };
 
-class GearNotFoundException extends NotFoundException {
+export class GearNotFoundException extends NotFoundException {
   constructor(id: number) {
     super(`Gear #${id} doesn't exist`);
   }
