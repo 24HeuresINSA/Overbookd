238eaf379efb88310a4412eedc09ab27bf7a54df
Damien <carreau.damien@gmail.com>
2022-12-28 12:23:51 +0000

    Resolve "Follow-up from "Draft: :sparkles: FA :sparkles:""

--- backend/src/collaborator/collaborator.service.ts
+++ backend/src/collaborator/collaborator.service.ts
@@ -23,37 +23,36 @@ export class CollaboratorService {
     faId: number,
     collab: CreateCollaboratorDto[],
   ): Promise<collaborator[] | null> {
-    return Promise.all(
-      collab.map(async (coll) => {
-        const col = coll.collaborator;
-        if (col.id) {
-          return await this.prisma.collaborator.update({
-            where: { id: col.id },
-            data: {
-              ...col,
-              fa_collaborators: {
-                connect: {
-                  fa_id_collaborator_id: {
-                    fa_id: faId,
-                    collaborator_id: col.id,
-                  },
-                },
-              },
-            },
-          });
-        } else {
-          return await this.prisma.collaborator.create({
-            data: {
-              ...col,
-              fa_collaborators: {
-                create: {
+    const operations = collab.map((coll) => {
+      const col = coll.collaborator;
+      if (col.id) {
+        return this.prisma.collaborator.update({
+          where: { id: col.id },
+          data: {
+            ...col,
+            fa_collaborators: {
+              connect: {
+                fa_id_collaborator_id: {
                   fa_id: faId,
+                  collaborator_id: col.id,
                 },
               },
             },
-          });
-        }
-      }),
-    );
+          },
+        });
+      } else {
+        return this.prisma.collaborator.create({
+          data: {
+            ...col,
+            fa_collaborators: {
+              create: {
+                fa_id: faId,
+              },
+            },
+          },
+        });
+      }
+    });
+    return this.prisma.$transaction(operations);
   }
 }
