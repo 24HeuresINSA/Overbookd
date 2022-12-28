de5a5f6b8a17274e2f9f18822f7a21e95eafc564
Léon Dumestre <leon.dumestre@gmail.com>
2022-12-28 15:56:13 +0100

    feat(Front): :sparkles: Add new electricity type

--- frontend/utils/models/FA.ts
+++ frontend/utils/models/FA.ts
@@ -10,9 +10,14 @@ export enum electricity_type {
   PC16 = "PC16_Prise_classique",
   P17_16A_MONO = "P17_16A_MONO",
   P17_16A_TRI = "P17_16A_TRI",
+  P17_16A_TETRA = "P17_16A_TETRA",
   P17_32A_MONO = "P17_32A_MONO",
   P17_32A_TRI = "P17_32A_TRI",
   P17_32A_TETRA = "P17_32A_TETRA",
+  P17_63A_MONO = "P17_63A_MONO",
+  P17_63A_TRI = "P17_63A_TRI",
+  P17_63A_TETRA = "P17_63A_TETRA",
+  P17_125A_TETRA = "P17_125A_TETRA",
 }
 
 export enum signa_type {
