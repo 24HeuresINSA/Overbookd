f1e1edf2cebd5d817d820b9191138e13a778a93f
DamienCarreau <carreau.damien@gmail.com>
2023-01-08 17:59:38 +0100

    Move groupBy from service to export fonction

--- backend/src/common/common.module.ts
+++ backend/src/common/common.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StatsService } from './services/stats.service';
 import { SlugifyService } from './services/slugify.service';
-import { GroupByService } from './services/group-by.service';
 
 @Module({
-  providers: [SlugifyService, GroupByService, StatsService],
-  exports: [SlugifyService, GroupByService, StatsService],
+  providers: [SlugifyService, StatsService],
+  exports: [SlugifyService, StatsService],
 })
 export class CommonModule {}
