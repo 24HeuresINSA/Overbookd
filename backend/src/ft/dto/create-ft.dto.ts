b197658bf4ec3da01d390e0d579cf2143a4865f1
Antoine Piron <75019585+AntoinePiron@users.noreply.github.com>
2023-01-10 23:02:40 +0100

    feat(FT): :sparkles: setup DTO

--- backend/src/ft/dto/create-ft.dto.ts
+++ backend/src/ft/dto/create-ft.dto.ts
@@ -1,1 +1,12 @@
-export class CreateFtDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateFtDto {
+  @ApiProperty({
+    required: true,
+    description: 'The name of the ft',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
--- backend/src/ft/dto/update-ft.dto.ts
+++ backend/src/ft/dto/update-ft.dto.ts
@@ -1,4 +1,94 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateFtDto } from './create-ft.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+  ValidationArguments,
+} from 'class-validator';
 
-export class UpdateFtDto extends PartialType(CreateFtDto) {}
+enum ftStatus {
+  DRAFT = 'DRAFT',
+  SUBMITTED = 'SUBMITTED',
+  VALIDATED = 'VALIDATED',
+  REFUSED = 'REFUSED',
+  READY = 'READY',
+}
+export class UpdateFtDto {
+  @ApiProperty({
+    required: true,
+    description: 'The name of the ft',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    required: false,
+    description: 'The status of the ft',
+    enum: ftStatus,
+    default: ftStatus.DRAFT,
+  })
+  @IsOptional()
+  @IsEnum(ftStatus, {
+    message: (va: ValidationArguments) =>
+      `${va.property} must be one of ${Object.values(ftStatus)}`,
+  })
+  status?: ftStatus;
+
+  @ApiProperty({
+    required: false,
+    description: 'The id of the parent fa',
+  })
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  parentFaId?: number;
+
+  @ApiProperty({
+    required: false,
+    description: 'Is the activity static',
+    default: false,
+  })
+  @IsOptional()
+  @IsBoolean()
+  isStatic?: boolean;
+
+  @ApiProperty({
+    required: false,
+    description: 'The description of the ft',
+  })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiProperty({
+    required: false,
+    description: 'The id of the user whos responsible of the ft',
+  })
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  userInChargeId?: number;
+
+  @ApiProperty({
+    required: false,
+    description: 'The id of the location of the ft',
+  })
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  locationId?: number;
+
+  @ApiProperty({
+    required: false,
+    description: 'Is the ft deleted',
+    default: false,
+  })
+  @IsOptional()
+  @IsBoolean()
+  isDeleted?: boolean;
+}
