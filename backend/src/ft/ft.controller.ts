dfd97410f3fd43cec10061c04590cc38f7d2f373
Antoine Piron <75019585+AntoinePiron@users.noreply.github.com>
2023-01-10 23:24:29 +0100

    feat(FT): :sparkles: ft controller and services

--- backend/src/ft/ft.controller.ts
+++ backend/src/ft/ft.controller.ts
@@ -4,39 +4,123 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Ft } from '@prisma/client';
+import { Permission } from 'src/auth/permissions-auth.decorator';
+import { PermissionsGuard } from 'src/auth/permissions-auth.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { CreateFtDto } from './dto/create-ft.dto';
+import { FTSearchRequestDto } from './dto/ftSearchRequest.dto';
 import { UpdateFtDto } from './dto/update-ft.dto';
 import { FtService } from './ft.service';
 
+@ApiBearerAuth()
+@ApiTags('ft')
 @Controller('ft')
 export class FtController {
   constructor(private readonly ftService: FtService) {}
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permission('hard')
   @Post()
-  create(@Body() createFtDto: CreateFtDto) {
-    return this.ftService.create(createFtDto);
+  @ApiResponse({
+    status: 201,
+    description: 'The ft has been successfully created.',
+    type: Promise<Ft | null>,
+  })
+  create(@Body() ft: CreateFtDto): Promise<Ft | null> {
+    return this.ftService.create(ft);
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permission('hard')
   @Get()
-  findAll() {
-    return this.ftService.findAll();
+  @ApiResponse({
+    status: 200,
+    description: 'Get all ft',
+    type: Promise<Ft[] | null>,
+  })
+  @ApiQuery({
+    name: 'isDeleted',
+    required: false,
+    type: Boolean,
+    description: 'Get FTs that are deleted',
+  })
+  findAll(@Query() searchRequest: FTSearchRequestDto): Promise<Ft[] | null> {
+    return this.ftService.findAll(searchRequest);
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permission('hard')
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'FT id',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Get ft by id',
+    type: Promise<Ft | null>,
+  })
+  findOne(@Param('id', ParseIntPipe) id: string): Promise<Ft | null> {
     return this.ftService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permission('hard')
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFtDto: UpdateFtDto) {
+  @ApiResponse({
+    status: 200,
+    description: 'ft updated',
+    type: Promise<Ft | null>,
+  })
+  @ApiBody({
+    description: 'Updated ft',
+    type: UpdateFtDto,
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'FT id',
+    required: true,
+  })
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateFtDto: UpdateFtDto,
+  ): Promise<Ft | null> {
     return this.ftService.update(+id, updateFtDto);
   }
 
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permission('hard')
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  @ApiResponse({
+    status: 204,
+    description: 'ft deleted',
+    type: Promise<Ft | null>,
+  })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'FT id',
+    required: true,
+  })
+  remove(@Param('id') id: string): Promise<Ft | null> {
     return this.ftService.remove(+id);
   }
 }
--- backend/src/ft/ft.service.ts
+++ backend/src/ft/ft.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Ft, ftStatus } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
 import { CreateFtDto } from './dto/create-ft.dto';
 import { UpdateFtDto } from './dto/update-ft.dto';
 
+export interface SearchFt {
+  isDeleted: boolean;
+  status?: ftStatus;
+}
+
 @Injectable()
 export class FtService {
-  create(createFtDto: CreateFtDto) {
-    return 'This action adds a new ft';
+  constructor(private prisma: PrismaService) {}
+
+  private buildFindCondition({ isDeleted: is_deleted, status }: SearchFt) {
+    const statusCondition = status ? { status } : {};
+    return { is_deleted, ...statusCondition };
   }
 
-  findAll() {
-    return `This action returns all ft`;
+  async create(ft: CreateFtDto): Promise<Ft | null> {
+    return this.prisma.ft.create({ data: ft });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ft`;
+  async findAll(search: SearchFt): Promise<Ft[] | null> {
+    const where = this.buildFindCondition(search);
+    return this.prisma.ft.findMany({
+      where,
+      orderBy: {
+        id: 'asc',
+      },
+    });
   }
 
-  update(id: number, updateFtDto: UpdateFtDto) {
-    return `This action updates a #${id} ft`;
+  async findOne(id: number): Promise<Ft | null> {
+    return this.prisma.ft.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} ft`;
+  async update(id: number, updateFtDto: UpdateFtDto): Promise<Ft | null> {
+    const ft = this.prisma.ft.findUnique({ where: { id } });
+    if (!ft) {
+      throw new NotFoundException(`ft #${id} not found`);
+    }
+    return this.prisma.ft.update({
+      where: { id },
+      data: updateFtDto,
+    });
+  }
+
+  async remove(id: number): Promise<Ft | null> {
+    const ft = this.prisma.ft.findUnique({ where: { id } });
+    if (!ft) {
+      throw new NotFoundException(`ft #${id} not found`);
+    }
+    return this.prisma.ft.update({
+      where: { id },
+      data: { isDeleted: true },
+    });
   }
 }
