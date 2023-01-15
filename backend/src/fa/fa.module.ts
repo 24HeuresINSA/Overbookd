d65faf3b990689ea7ba4f801b148510987404765
Damien <carreau.damien@gmail.com>
2023-01-15 11:26:19 +0000

    feat: :sparkles: Implement the stats for FAs

--- backend/src/fa/fa.module.ts
+++ backend/src/fa/fa.module.ts
@@ -4,6 +4,12 @@ import { FaController } from './fa.controller';
 import { PrismaService } from '../prisma.service';
 import { CommonModule } from 'src/common/common.module';
 import { GearRequestsModule } from 'src/gear-requests/gearRequests.module';
+import { CommonModule } from 'src/common/common.module';
+import { PrismaGearRepository } from 'src/catalog/repositories';
+import { GearRequestsService } from './gear-requests/gearRequests.service';
+import { PrismaPeriodRepository } from './gear-requests/repositories/period.repository.prisma';
+import { PrismaAnimationRepository } from './gear-requests/repositories/animation.repository.prisma';
+import { PrismaGearRequestRepository } from './gear-requests/repositories/gearRequest.repository.prisma';
 
 @Module({
   imports: [CommonModule, GearRequestsModule],
