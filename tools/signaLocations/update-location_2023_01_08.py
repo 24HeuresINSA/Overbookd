bfb9f7f4a69a1c72cb5bf68e25b53b7d4e9cbcde
Léon Dumestre <leon.dumestre@gmail.com>
2023-01-19 13:47:36 +0000

    fix(signa): update signa locations. #773

--- tools/signaLocations/update-location_2023_01_08.py
+++ tools/signaLocations/update-location_2023_01_08.py
@@ -1,7 +1,4 @@
-import requests
-
-TOKEN = "YOUR_TOKEN"
-DOMAIN = "preprod.overbookd.24heures.org"
+from utils import main as updateLocations
 
 toAddLocations =[
     "Pelouse derrière Cambridge",
@@ -20,53 +17,7 @@ toUpdateLocationForms = [
   { "from": "Devant GCU", "to": "Pelouse GCU" },
 ]
 
-def deleteLocations(locations):
-    for location in locations:
-        print(f"deleting {location['name']}")
-        requests.delete(f"https://{DOMAIN}/api/signa-location/{location['id']}", headers={"Authorization": f"Bearer {TOKEN}"}).json()
-
-def updateLocations(locations):
-    for location in locations:
-        print(f"updating #{location['id']} to {location['name']}")
-        requests.patch(f"https://{DOMAIN}/api/signa-location/{location['id']}", json=location, headers={"Authorization": f"Bearer {TOKEN}"}).json()
-
-
-def createLocations(locations):
-    for location in locations:
-        print(f"creating {location}")
-        json = {"name": location}
-        requests.post(f"https://{DOMAIN}/api/signa-location/", json=json, headers={"Authorization": f"Bearer {TOKEN}"}).json()
-
-def getAllLocations():
-        return requests.get(f"https://{DOMAIN}/api/signa-location/", headers={"Authorization": f"Bearer {TOKEN}"}).json()
-
-def findLocations(locationNames, locations):
-    return [location for location in locations if location['name'] in locationNames]
-
-def getOriginalName(toUpdateLocationForm):
-    return toUpdateLocationForm["from"]
-
-def findToUpdateNewName(previousName, toUpdateLocationForms):
-    for form in toUpdateLocationForms:
-        if previousName == form['from']:
-            return form['to']
-
-    return previousName
-
-def setNewNames(toUpdateLocations):
-    return [{"id": location["id"], "name": findToUpdateNewName(location['name'], toUpdateLocationForms) } for location in toUpdateLocations]
-
-def setToUpdateLocations(toUpdateLocationForms, locations):
-    toUpdateLocationNames = list(map(getOriginalName, toUpdateLocationForms))
-    toUpdateLocations = findLocations(toUpdateLocationNames, locations)
-    return setNewNames(toUpdateLocations)
-
 def main():
-    locations = getAllLocations()
-    toDeleteLocations = findLocations(toDeleteLocationNames, locations)
-    toUpdateLocations = setToUpdateLocations(toUpdateLocationForms, locations)
-    deleteLocations(toDeleteLocations)
-    updateLocations(toUpdateLocations)
-    createLocations(toAddLocations)
+  updateLocations(toAddLocations, toDeleteLocationNames, toUpdateLocationForms)
 
 main()
