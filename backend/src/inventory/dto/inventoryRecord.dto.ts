f6e53ff7d21a0dc0aa3591fe15c9b7ad6a216a29
Léo Mouyna <mouynaleo@gmail.com>
2023-01-20 21:45:18 +0000

    feat(inventory): display all inventory records.

--- backend/src/inventory/dto/inventoryRecord.dto.ts
+++ backend/src/inventory/dto/inventoryRecord.dto.ts
@@ -1,11 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Gear } from 'src/catalog/interfaces';
+import { GearRepresentation } from 'src/common/dto/gearRepresentation.dto';
 import { InventoryRecord } from '../inventory.service';
-import { InventoryGroupedRecordResponseDto } from './inventoryGroupedRecordResponse.dto';
 
-export class InventoryRecordDto
-  extends InventoryGroupedRecordResponseDto
-  implements InventoryRecord
-{
+export class InventoryRecordDto implements InventoryRecord {
+  @ApiProperty({
+    required: true,
+    description: 'Gear quantity',
+    type: Number,
+  })
+  quantity: number;
+  @ApiProperty({
+    required: true,
+    description: 'Gear',
+    type: GearRepresentation,
+  })
+  gear: Gear;
   @ApiProperty({
     required: true,
     description: 'Gear storage location',
