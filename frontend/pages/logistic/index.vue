ba1b8e11515e83a9f862617e119fe55cfe43a4d5
Leo Mouyna <mouynaleo@gmail.com>
2023-01-24 13:48:38 +0100

    feat(log): Add static gear request listing. #792

---
+++ frontend/pages/logistic/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="logistic">
+    <h1>Logistique</h1>
+    <GearRequestListing />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import GearRequestListing from "~/components/GearRequestListing.vue";
+
+export default Vue.extend({
+  name: "Logistic",
+  components: { GearRequestListing },
+});
+</script>
