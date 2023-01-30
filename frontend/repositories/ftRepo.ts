cece8c2bbfdb0808016fc27f413921a95f30bd23
Léon Dumestre <leon.dumestre@gmail.com>
2023-01-30 10:56:14 +0000

    feat: add Feedback, Validation, UserRequests & TeamRequests in store & repo

--- frontend/repositories/ftRepo.ts
+++ frontend/repositories/ftRepo.ts
@@ -1,5 +1,9 @@
 import { NuxtAxiosInstance } from "@nuxtjs/axios";
-import { Feedback } from "~/utils/models/feedback";
+import {
+  Feedback,
+  FeedbackCreation,
+  SavedFeedback,
+} from "~/utils/models/feedback";
 import {
   FTCreation,
   FTSearch,
@@ -8,7 +12,12 @@ import {
   FTTimeWindow,
   FTSimplified,
   FT,
+  FTTeamRequestUpdate,
+  FTUserRequestUpdate,
+  FTTeamRequest,
 } from "~/utils/models/ft";
+import { Reviewer } from "~/utils/models/review";
+import { User } from "~/utils/models/user";
 import { HttpStringified } from "~/utils/types/http";
 
 const resource = "/ft";
@@ -35,6 +44,20 @@ export default {
   deleteFT(context: Context, id: number) {
     return context.$axios.delete(`${resource}/${id}`);
   },
+
+  validateFT(context: Context, ftId: number, reviewer: Reviewer) {
+    return context.$axios.post<HttpStringified<FT>>(
+      `${resource}/${ftId}/validate`,
+      reviewer
+    );
+  },
+  refuseFT(context: Context, ftId: number, reviewer: Reviewer) {
+    return context.$axios.post<HttpStringified<FT>>(
+      `${resource}/${ftId}/refuse`,
+      reviewer
+    );
+  },
+
   updateFTTimeWindow(
     context: Context,
     ftId: number,
@@ -48,7 +71,38 @@ export default {
   deleteFTTimeWindow(context: Context, ftId: number, twId: number) {
     return context.$axios.delete(`${resource}/${ftId}/time-windows/${twId}`);
   },
+  updateFTUserRequests(
+    context: Context,
+    ftId: number,
+    twId: number,
+    userRequests: FTUserRequestUpdate[]
+  ) {
+    return context.$axios.post<HttpStringified<User[]>>(
+      `${resource}/${ftId}/time-windows/${twId}/user-requests`,
+      userRequests
+    );
+  },
 
+  updateFTTeamRequests(
+    context: Context,
+    ftId: number,
+    twId: number,
+    teamRequests: FTTeamRequestUpdate[]
+  ) {
+    return context.$axios.post<HttpStringified<FTTeamRequest[]>>(
+      `${resource}/${ftId}/time-windows/${twId}/team-requests`,
+      teamRequests
+    );
+  },
+
+  addFTFeedback(context: Context, ftId: number, feedback: FeedbackCreation) {
+    return context.$axios.post<HttpStringified<SavedFeedback>>(
+      `${resource}/${ftId}/feedbacks`,
+      feedback
+    );
+  },
+
+  // old requests
   markAsReady(context: Context, FTCount: number, comment: Feedback) {
     return context.$axios.post(`${resource}/${FTCount}/ready`, { comment });
   },
