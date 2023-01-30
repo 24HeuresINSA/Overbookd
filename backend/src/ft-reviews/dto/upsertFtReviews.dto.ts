3b51b2efcd7fff04e0be0fdd3977ec49db7262d9
Léon Dumestre <leon.dumestre@gmail.com>
2023-01-30 12:19:10 +0100

    feat(Back): :sparkles: Add service & refuse request #782

--- backend/src/ft-reviews/dto/upsertFtReviews.dto.ts
+++ backend/src/ft-reviews/dto/upsertFtReviews.dto.ts
@@ -1,13 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, Min } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class UpsertFtReviewsDto {
   @ApiProperty({
     required: true,
-    type: Number,
-    description: 'The id of the reviewer',
+    type: String,
+    description: 'The team code of the reviewer',
   })
-  @IsNumber()
-  @Min(1)
-  userId?: number;
+  @IsString()
+  teamCode: string;
 }
