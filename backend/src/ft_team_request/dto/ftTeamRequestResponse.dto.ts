b22ba55cc4cd21e139bbedb647f34ca7091dd714
Antoine Piron <pro.piron.antoine@gmail.com>
2023-02-04 19:52:15 +0100

    fix(FT): :zap: fix dto

--- backend/src/ft_team_request/dto/ftTeamRequestResponse.dto.ts
+++ backend/src/ft_team_request/dto/ftTeamRequestResponse.dto.ts
@@ -1,6 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { TeamFormDto } from 'src/team/dto/teamFormRequest.dto';
 
+class CompeleteTeamDto extends TeamFormDto {
+  @ApiProperty({
+    type: Number,
+    description: 'The id of the team',
+    example: 1,
+    required: true,
+  })
+  id: number;
+}
+
 export class FtTeamRequestResponseDto {
   @ApiProperty({
     type: Number,
@@ -15,5 +25,5 @@ export class FtTeamRequestResponseDto {
     description: 'The team',
     required: true,
   })
-  team: TeamFormDto;
+  team: CompeleteTeamDto;
 }
