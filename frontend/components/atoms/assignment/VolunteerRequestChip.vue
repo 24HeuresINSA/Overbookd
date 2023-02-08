73b54b4d480e3ba917977b9f55a335991b8f67c9
Léo Mouyna <mouynaleo@gmail.com>
2023-02-08 06:03:43 +0000

    feat(ft): display volunteers required by other fts. #812

---
+++ frontend/components/atoms/assignment/VolunteerRequestChip.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="volunteer-request">
+    <v-tooltip top color="error" :disabled="!hasErrors">
+      <template #activator="{ on, attrs }">
+        <v-chip
+          :close="!disabled"
+          :class="userRequestStatus"
+          v-bind="attrs"
+          @click:close="deleteUserRequest()"
+          v-on="on"
+        >
+          {{ userName }}
+        </v-chip>
+      </template>
+      <v-list-item dark>
+        <v-list-item-content v-for="(message, i) in errorMessages" :key="i">
+          <v-list-item-title> {{ message }} </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-tooltip>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { FTUserRequestImpl } from "~/utils/models/ft";
+import { formatUsername } from "~/utils/user/userUtils";
+export default Vue.extend({
+  name: "VolunteerRequestChip",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    userRequest: {
+      type: FTUserRequestImpl,
+      required: true,
+    },
+  },
+  computed: {
+    userRequestStatus(): string {
+      return this.hasErrors ? "also-requested-by-ft" : "";
+    },
+    userName(): string {
+      return formatUsername(this.userRequest.user);
+    },
+    hasErrors(): boolean {
+      return this.userRequest.alsoRequestedBy.length > 0;
+    },
+    errorMessages(): string[] {
+      if (!this.hasErrors) return [];
+      return this.userRequest.alsoRequestedBy.map(
+        (ft) => `Aussi demande dans la FT #${ft.id} - ${ft.name}`
+      );
+    },
+  },
+  methods: {
+    deleteUserRequest(): void {
+      this.$emit("delete-user-request", this.userRequest);
+    },
+  },
+});
+</script>
