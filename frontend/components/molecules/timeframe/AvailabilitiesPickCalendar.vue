9851444370672c688ca095c364e2ec23ab94b81e
Léon Dumestre <leon.dumestre@gmail.com>
2023-02-12 01:39:59 +0100

    feat(Front): :sparkles: add calendar steps #814

--- frontend/components/molecules/timeframe/AvailabilitiesPickCalendar.vue
+++ frontend/components/molecules/timeframe/AvailabilitiesPickCalendar.vue
@@ -1,24 +1,15 @@
 <template>
-  <div class="flex-row">
+  <div>
     <v-calendar
-      ref="monthCalendar"
-      type="month"
+      ref="refCalendar"
+      type="week"
       :short-weekdays="false"
       :event-ripple="false"
       :weekdays="[1, 2, 3, 4, 5, 6, 0]"
-      start="2023-05-08"
-      end="2023-05-16"
+      :start="start"
+      :end="end"
       color="primary"
-      class="elevation-2 month-calendar"
-    ></v-calendar>
-    <v-calendar
-      ref="dailyCalendar"
-      type="day"
-      :short-weekdays="false"
-      :event-ripple="false"
-      value="2023-05-08"
-      color="primary"
-      class="elevation-2 daily-calendar"
+      class="elevation-1"
     ></v-calendar>
   </div>
 </template>
@@ -28,22 +19,15 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "AvailabilitiesPickCalendar",
+  props: {
+    start: {
+      type: String,
+      default: "",
+    },
+    end: {
+      type: String,
+      default: "",
+    },
+  },
 });
 </script>
-
-<style lang="scss" scoped>
-.flex-row {
-  display: flex;
-
-  .month-calendar {
-    width: 75%;
-    height: 700px;
-    margin-right: 30px;
-  }
-
-  .daily-calendar {
-    width: 25%;
-    height: 700px;
-  }
-}
-</style>
