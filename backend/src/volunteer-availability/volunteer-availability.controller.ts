1deea5e471a94576d58414e9cd06ff0592412f27
Antoine Piron <pro.piron.antoine@gmail.com>
2023-02-17 10:04:04 +0100

    feat(Availabilities): :sparkles: check thaht new availabilities are longer

--- backend/src/volunteer-availability/volunteer-availability.controller.ts
+++ backend/src/volunteer-availability/volunteer-availability.controller.ts
@@ -1,9 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CreateVolunteerAvailabilityDto } from './dto/createVolunteerAvailability.dto';
+import { VolunteerAvailabilityResponseDto } from './dto/volunteerAvailabilityResponse.dto';
 import { VolunteerAvailabilityService } from './volunteer-availability.service';
 
+@ApiBearerAuth()
+@ApiTags('volunteer-availability')
+@ApiBadRequestResponse({
+  description: 'Request is not formated as expected.',
+})
+@ApiForbiddenResponse({
+  description: 'User is not allowed to access this resource.',
+})
 @Controller('volunteer-availability')
 export class VolunteerAvailabilityController {
   constructor(
     private readonly volunteerAvailabilityService: VolunteerAvailabilityService,
   ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':userId')
+  @HttpCode(201)
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+    type: VolunteerAvailabilityResponseDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found.',
+  })
+  add(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body() periods: CreateVolunteerAvailabilityDto,
+  ): Promise<VolunteerAvailabilityResponseDto> {
+    return this.volunteerAvailabilityService.addAvailabilities(userId, periods);
+  }
 }
--- backend/src/volunteer-availability/volunteer-availability.service.ts
+++ backend/src/volunteer-availability/volunteer-availability.service.ts
@@ -1,7 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { VolunteerAvailability } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
+import {
+  CreateVolunteerAvailabilityDto,
+  Period,
+} from './dto/createVolunteerAvailability.dto';
+import { VolunteerAvailabilityResponseDto } from './dto/volunteerAvailabilityResponse.dto';
 
 @Injectable()
 export class VolunteerAvailabilityService {
   constructor(private prisma: PrismaService) {}
+
+  async addAvailabilities(
+    userId: number,
+    createVolunteerAvailability: CreateVolunteerAvailabilityDto,
+  ): Promise<VolunteerAvailabilityResponseDto> {
+    /**
+     * First step : check that new availabilities are longer than older ones
+     * Second step : compute new charisma points based on this availabilities
+     * Third step : update the user with the new charisma points and the new availabilities through a transaction
+     */
+    const newPeriods = createVolunteerAvailability.periods;
+    const oldAvailabilities = await this.prisma.volunteerAvailability
+      .findMany({
+        where: {
+          userId,
+        },
+      })
+      .then((el: VolunteerAvailability[]) => {
+        return el.map((av) => {
+          return {
+            start: av.start,
+            end: av.end,
+          };
+        });
+      });
+    if (oldAvailabilities.length > 0) {
+      const oldAvailabilitiesDuration = oldAvailabilities.reduce(
+        (acc, curr) => acc + this.getPeriodDuration(curr),
+        0,
+      );
+      const newAvailabilitiesDuration = newPeriods.reduce(
+        (acc, curr) => acc + this.getPeriodDuration(curr),
+        0,
+      );
+      if (newAvailabilitiesDuration < oldAvailabilitiesDuration) {
+        throw new ForbiddenException(
+          'New availabilities are shorter than older ones. Please add more periods.',
+        );
+      }
+    }
+    console.log('New availabilities are longer than older ones');
+    return null;
+  }
+
+  private getPeriodDuration(params: Period): number {
+    return Math.floor(
+      (new Date(params.end).getTime() - new Date(params.start).getTime()) /
+        1000,
+    );
+  }
 }
