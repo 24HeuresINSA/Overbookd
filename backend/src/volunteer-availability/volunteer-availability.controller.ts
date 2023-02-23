085b0be8e16cfc80948ca156c76b387f8a6a112d
Antoine Piron <pro.piron.antoine@gmail.com>
2023-02-23 19:29:50 +0100

    feat(Availabilities): :sparkles: add all missing routes
    Now can add/get and override securities for humans

--- backend/src/volunteer-availability/volunteer-availability.controller.ts
+++ backend/src/volunteer-availability/volunteer-availability.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   Param,
   ParseIntPipe,
@@ -15,6 +16,8 @@ import {
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
+import { Permission } from 'src/auth/permissions-auth.decorator';
+import { PermissionsGuard } from 'src/auth/permissions-auth.guard';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { CreateVolunteerAvailabilityDto } from './dto/createVolunteerAvailability.dto';
 import { VolunteerAvailabilityResponseDto } from './dto/volunteerAvailabilityResponse.dto';
@@ -50,5 +53,58 @@ export class VolunteerAvailabilityController {
     @Body() periods: CreateVolunteerAvailabilityDto,
   ): Promise<VolunteerAvailabilityResponseDto> {
     return this.volunteerAvailabilityService.addAvailabilities(userId, periods);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'The records have been successfully retrieved.',
+    type: VolunteerAvailabilityResponseDto,
+    isArray: true,
+  })
+  findAll(): Promise<VolunteerAvailabilityResponseDto[]> {
+    return this.volunteerAvailabilityService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':userId')
+  @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully retrieved.',
+    type: VolunteerAvailabilityResponseDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found.',
+  })
+  findOne(
+    @Param('userId', ParseIntPipe) userId: number,
+  ): Promise<VolunteerAvailabilityResponseDto> {
+    return this.volunteerAvailabilityService.findUserAvailabilities(userId);
+  }
+
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permission('can-affect')
+  @Post(':userId/human')
+  @HttpCode(201)
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+    type: VolunteerAvailabilityResponseDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found.',
+  })
+  overrideHuman(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body() periods: CreateVolunteerAvailabilityDto,
+  ): Promise<VolunteerAvailabilityResponseDto> {
+    return this.volunteerAvailabilityService.addAvailabilities(
+      userId,
+      periods,
+      true,
+    );
   }
 }
--- backend/src/volunteer-availability/volunteer-availability.service.ts
+++ backend/src/volunteer-availability/volunteer-availability.service.ts
@@ -14,6 +14,7 @@ export class VolunteerAvailabilityService {
   async addAvailabilities(
     userId: number,
     createVolunteerAvailability: CreateVolunteerAvailabilityDto,
+    humanOverride = false,
   ): Promise<VolunteerAvailabilityResponseDto> {
     /**
      * First step : check that new availabilities are longer than older ones
@@ -35,7 +36,7 @@ export class VolunteerAvailabilityService {
           };
         });
       });
-    if (oldAvailabilities.length > 0) {
+    if (oldAvailabilities.length > 0 && !humanOverride) {
       const oldAvailabilitiesDuration = oldAvailabilities.reduce(
         (acc, curr) => acc + this.getPeriodDuration(curr),
         0,
@@ -100,6 +101,57 @@ export class VolunteerAvailabilityService {
     return {
       userId,
       periods: availabilities.map((av) => {
+        return {
+          start: av.start,
+          end: av.end,
+        };
+      }),
+    };
+  }
+
+  async findAll(): Promise<VolunteerAvailabilityResponseDto[]> {
+    const allAvailabilities =
+      await this.prisma.volunteerAvailability.findMany();
+    //map group by user id and concat periods
+    const formattedAvailabilities: VolunteerAvailabilityResponseDto[] = [];
+    for (const availability of allAvailabilities) {
+      const user = formattedAvailabilities.find(
+        (el) => el.userId === availability.userId,
+      );
+      if (user) {
+        user.periods.push({
+          start: availability.start,
+          end: availability.end,
+        });
+      } else {
+        formattedAvailabilities.push({
+          userId: availability.userId,
+          periods: [
+            {
+              start: availability.start,
+              end: availability.end,
+            },
+          ],
+        });
+      }
+    }
+    //map for each user as we want to return an array of objects
+    return formattedAvailabilities;
+  }
+
+  async findUserAvailabilities(
+    userId: number,
+  ): Promise<VolunteerAvailabilityResponseDto> {
+    const userAvailabilities = await this.prisma.volunteerAvailability.findMany(
+      {
+        where: {
+          userId,
+        },
+      },
+    );
+    return {
+      userId,
+      periods: userAvailabilities.map((av) => {
         return {
           start: av.start,
           end: av.end,
