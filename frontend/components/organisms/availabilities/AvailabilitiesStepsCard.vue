2be90c4432664bcf3aa30a9131b3af825ddb6556
Léo Mouyna <mouynaleo@gmail.com>
2023-02-27 22:07:32 +0000

    fix(availabilities): 🎨 simple stepper. #846

--- frontend/components/organisms/availabilities/AvailabilitiesStepsCard.vue
+++ frontend/components/organisms/availabilities/AvailabilitiesStepsCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-stepper v-model="step" alt-labels>
+  <v-stepper v-model="step">
     <v-stepper-header>
       <v-stepper-step
         v-for="({ title }, index) in calendarSteps"
@@ -12,34 +12,35 @@
     </v-stepper-header>
 
     <v-stepper-items>
-      <div
+      <v-stepper-content
         v-for="({ period }, index) in calendarSteps"
+        :id="`calendar-part-${index + 1}`"
         :key="`${period.start}-${period.end}`"
+        :step="index + 1"
+        class="content-calendar"
       >
-        <v-stepper-content :step="index + 1">
-          <AvailabilitiesPickCalendar :period="period" />
-          <v-card-actions>
-            <v-btn v-if="index > 0" @click="decrementStep"> Précédent </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn
-              v-if="index < calendarSteps.length - 1"
-              color="primary"
-              :disabled="hasAvailabilityError"
-              @click="incrementStep"
-            >
-              Suivant
-            </v-btn>
-            <v-btn
-              v-else
-              color="success"
-              :disabled="hasAvailabilityError"
-              @click="saveAvailabilities"
-            >
-              Valider
-            </v-btn>
-          </v-card-actions>
-        </v-stepper-content>
-      </div>
+        <AvailabilitiesPickCalendar :period="period" />
+        <v-card-actions>
+          <v-btn v-if="index > 0" @click="decrementStep"> Précédent </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            v-if="index < calendarSteps.length - 1"
+            color="primary"
+            :disabled="hasAvailabilityError"
+            @click="incrementStep"
+          >
+            Suivant
+          </v-btn>
+          <v-btn
+            v-else
+            color="success"
+            :disabled="hasAvailabilityError"
+            @click="saveAvailabilities"
+          >
+            Valider
+          </v-btn>
+        </v-card-actions>
+      </v-stepper-content>
     </v-stepper-items>
   </v-stepper>
 </template>
@@ -131,3 +132,13 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+@media only screen and (max-width: 960px) {
+  @for $index from 1 through 5 {
+    #calendar-part-#{$index} {
+      padding: 24px 0px 16px 0px;
+    }
+  }
+}
+</style>
