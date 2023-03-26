31ae594656a6e5c716e3a574220804c769405596
Antoine Piron <pro.piron.antoine@gmail.com>
2023-03-26 12:10:55 +0200

    feat(Back): :sparkles: complete signa needs dto export

--- backend/src/fa_signa_needs/dto/exportSignaNeedsCsv.dto.ts
+++ backend/src/fa_signa_needs/dto/exportSignaNeedsCsv.dto.ts
@@ -2,10 +2,33 @@ import { ApiProperty } from '@nestjs/swagger';
 import { signa_type } from '@prisma/client';
 
 export class FaSignaNeedsExportCsvDto {
-  @ApiProperty({})
+  @ApiProperty({
+    description: 'Name of the FA',
+    type: String,
+  })
   fa_name: string;
+
+  @ApiProperty({
+    description: 'Type of the signa',
+    enum: signa_type,
+  })
   signa_type: signa_type;
+
+  @ApiProperty({
+    description: 'Text of the signa',
+    type: String,
+  })
   text: string;
+
+  @ApiProperty({
+    description: 'Count of needed signa',
+    type: Number,
+  })
   count: number;
+
+  @ApiProperty({
+    description: 'Comment',
+    type: String,
+  })
   comment?: string;
 }
