9026c933aff2b76e9595c10ecd4bb5eeb201f1f2
Leon Dumestre <leon.dumestre@gmail.com>
2023-03-30 14:36:10 +0200

    feat(Front): :sparkles: split affect pages #934

---
+++ frontend/pages/assignment/orga-task.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container class="assignment-container">
+    <FilterableVolunteerList class="volunteer-list" />
+    <FilterableTimespanList class="task-list" />
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import FilterableVolunteerList from "~/components/organisms/assignment/FilterableVolunteerList.vue";
+import FilterableTimespanList from "~/components/organisms/assignment/FilterableTimespanList.vue";
+import { Volunteer } from "~/utils/models/assignment";
+
+export default Vue.extend({
+  name: "OrgaTask",
+  components: {
+    FilterableVolunteerList,
+    FilterableTimespanList,
+  },
+  computed: {
+    volunteers(): Volunteer[] {
+      return this.$accessor.assignment.volunteers;
+    },
+  },
+  async mounted() {
+    this.$accessor.assignment.setModeOrgaTask();
+    if (!this.volunteers.length) {
+      await this.$accessor.assignment.fetchVolunteers();
+    }
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.assignment-container {
+  padding: 0;
+  left: 0;
+  top: 0;
+  max-width: none;
+  margin-left: 0;
+  margin-right: 0;
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  width: 100%;
+}
+
+.volunteer-list {
+  max-width: 350px;
+}
+
+.task-list {
+  max-width: 450px;
+}
+</style>
