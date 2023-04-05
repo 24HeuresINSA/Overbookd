8a28280faca24165d1720ee5316f2d68a3364fa9
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2023-04-05 19:51:11 +0200

    fix(auth): :bug: correct python script
    Issue #899

--- tools/user/updateMail.py
+++ tools/user/updateMail.py
@@ -5,15 +5,16 @@ DOMAIN = "overbookd.traefik.me"
 
 
 def getAllUsers():
-    return requests.get(f"https://{DOMAIN}/api/user", headers={"Authorization": f"Bearer {TOKEN}"}, timeout=5,).json()
+    return requests.get(f"https://{DOMAIN}/api/user", headers={"Authorization": f"Bearer {TOKEN}"}, timeout=5).json()
 
 
 def updateUserMail(userMail):
     userId = userMail["id"]
-    mail = userMail["email"].lower()
-    requests.put(f"https://{DOMAIN}/api/user/{userId}/mail", json=mail,
-                 headers={"Authorization": f"Bearer {TOKEN}"}, timeout=5)
-    print(f"User {userId} updated")
+    if not userMail["email"].islower():
+        mail = userMail["email"].lower()
+        requests.put(f"https://{DOMAIN}/api/user/{userId}", json={"email": mail},
+                     headers={"Authorization": f"Bearer {TOKEN}"}, timeout=5)
+        print(f"User {userId} updated")
 
 
 def main():
