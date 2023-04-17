9bae1da0ff2c5f4f8d6264de6af0b4152534dcd2
Leon Dumestre <leon.dumestre@gmail.com>
2023-04-17 22:20:22 +0200

    style(Front): :art: assignment style #1012

--- frontend/components/organisms/assignment/list/FilterableTaskList.vue
+++ frontend/components/organisms/assignment/list/FilterableTaskList.vue
@@ -113,7 +113,7 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-$filters-height: 215px;
+$filters-height: 225px;
 $header-footer-height: 100px;
 
 .filterable-task-list {
@@ -129,7 +129,7 @@ $header-footer-height: 100px;
 
 .filters {
   width: 100%;
-  height: $filters-height - 25px;
+  height: $filters-height;
 }
 
 .task-list {
