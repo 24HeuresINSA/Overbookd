4342ebea7c7964cfa0c8445bf376b523fc3ba338
Leon Dumestre <leon.dumestre@gmail.com>
2023-04-18 21:35:05 +0200

    feat(Front): :sparkles: add icon for friend filter #1020

--- frontend/components/molecules/assignment/filter/VolunteerFilters.vue
+++ frontend/components/molecules/assignment/filter/VolunteerFilters.vue
@@ -1,31 +1,43 @@
 <template>
-  <div class="filters">
-    <v-text-field
-      :value="search"
-      class="filters__field"
-      label="Recherche"
-      @input="changeSearch"
-    ></v-text-field>
-    <div class="team_filter_categoy_sort">
-      <SearchTeams
-        :value="teams"
-        class="filters__field"
-        :boxed="false"
-        @change="changeTeams"
-      ></SearchTeams>
-      <div class="sort__category" @click="updateSort">
+  <div class="volunteer-filters">
+    <div class="filters">
+      <div class="filters__column">
+        <v-text-field
+          :value="search"
+          class="filters__field"
+          label="Recherche"
+          @input="changeSearch"
+        ></v-text-field>
+        <SearchTeams
+          :value="teams"
+          class="filters__field"
+          :boxed="false"
+          @change="changeTeams"
+        ></SearchTeams>
+      </div>
+      <div class="filters__column icons">
         <v-tooltip top>
           <template #activator="{ on, attrs }">
-            <v-icon v-if="!sort" v-bind="attrs" v-on="on"> mdi-sort </v-icon>
-            <v-icon v-else-if="sort > 0" v-bind="attrs" v-on="on">
-              mdi-sort-ascending
-            </v-icon>
-            <v-icon v-else v-bind="attrs" v-on="on">
-              mdi-sort-descending
+            <v-icon v-bind="attrs" :color="friendFilter ? 'red' : ''" v-on="on">
+              mdi-account-cancel
             </v-icon>
           </template>
-          <span>Trier par tache</span>
+          <span>Filter si pas d'amis</span>
         </v-tooltip>
+        <div class="sort__category" @click="updateSort">
+          <v-tooltip top>
+            <template #activator="{ on, attrs }">
+              <v-icon v-if="!sort" v-bind="attrs" v-on="on"> mdi-sort </v-icon>
+              <v-icon v-else-if="sort > 0" v-bind="attrs" v-on="on">
+                mdi-sort-ascending
+              </v-icon>
+              <v-icon v-else v-bind="attrs" v-on="on">
+                mdi-sort-descending
+              </v-icon>
+            </template>
+            <span>Trier par tache</span>
+          </v-tooltip>
+        </div>
       </div>
     </div>
     <p class="stats">
@@ -59,6 +71,7 @@ export default Vue.extend({
     search: "",
     teams: [],
     sort: 0,
+    friendFilter: false,
   }),
   methods: {
     changeSearch(search: string) {
@@ -76,15 +89,28 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.filters {
-  width: 100%;
-  height: 140px;
+.volunteer-filters {
   display: flex;
   flex-direction: column;
+
+  width: 100%;
+  height: 140px;
   justify-content: center;
   align-items: center;
   margin-top: 25px;
   padding: 0 25px;
+}
+
+.filters {
+  display: flex;
+
+  &__column {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+  }
 
   &__field {
     width: 100%;
@@ -93,18 +119,15 @@ export default Vue.extend({
   }
 }
 
+.icons {
+  gap: 20px;
+  margin-left: 20px;
+  * {
+    cursor: pointer;
+  }
+}
+
 .stats {
   margin-bottom: 5px;
-}
-
-.sort__category:hover {
-  cursor: pointer;
-}
-
-.team_filter_categoy_sort {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  gap: 5px;
 }
 </style>
