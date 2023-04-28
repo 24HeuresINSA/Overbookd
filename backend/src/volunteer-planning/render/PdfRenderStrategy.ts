5c62788cf8ab1969cdf5811e72cc76e5944b67ad
Leo Mouyna <mouynaleo@gmail.com>
2023-04-28 08:53:38 +0100

    fix(planning): add introduction page in pdf. #1060

--- backend/src/volunteer-planning/render/PdfRenderStrategy.ts
+++ backend/src/volunteer-planning/render/PdfRenderStrategy.ts
@@ -199,32 +199,78 @@ export class PdfRenderStrategy implements RenderStrategy {
   }
 
   private generateContent(tasks: Task[]): Content[] {
+    const introductionPage = this.generateIntroductionPage();
     const securityPlan = this.generateSecurityPage();
     const assignments = tasks.flatMap((task) => this.generateTaskContent(task));
     const cocktailPurpleWorkflows = this.generatePurpleCocktailWorkflows();
-    const switchToPortraitOrientation = this.generateSwitchPageOrientation();
     return [
+      introductionPage,
       securityPlan,
-      switchToPortraitOrientation,
       ...assignments,
       ...cocktailPurpleWorkflows,
     ];
   }
 
-  private generateSwitchPageOrientation(): Content {
-    return {
-      pageBreak: 'before',
-      pageOrientation: 'portrait',
-      text: '',
-    };
+  private generateIntroductionPage(): Content {
+    return [
+      {
+        text: "Bienvenue dans l'aventure 24 heures de l'INSA !",
+        style: ['paragraph', 'bold', 'liteSpaceBetween'],
+      },
+      {
+        text: 'Merci de rendre cet événement possible, vous etes plus 400 a venir nous aider pour ce week-end de folie.',
+        style: ['paragraph', 'largeSpaceBetween'],
+      },
+      {
+        text: "Tu trouveras dans ce livret l'essentiel des informations dont tu as besoin pour profiter et nous filer un coup de main avec:",
+        style: ['paragraph'],
+      },
+      {
+        ul: [
+          'Le plan benevole disponible sous forme de QR Code, il te permettra de trouver ton chemin en toutes circontances avec sa carte interactive',
+          'Le plan secu en page 2, il est la pour te donner une vision specifique du site en soiree (ou est-ce que tu peux rentrer, ou sont les bars etc...)',
+          'Un planning avec une liste de creneaux ou on compte sur toi pour nous aider',
+          "Un rappel du dispositif cocktail purple en toute fin de livret, ce dispositif vise a mettre a l'abris des personnes qui se sentent en danger sur le festival",
+        ],
+        style: ['paragraph', 'liteSpaceBetween'],
+        margin: [75, 0, 50, 5],
+      },
+      {
+        text: "Lorsque tu as du temps libre n'hesite pas a passer au QG pour:",
+        style: ['paragraph'],
+      },
+      {
+        ul: [
+          'te restaurer',
+          'te reposer',
+          'te proposer pour filer un coup de main',
+          "te poser avec d'autres benevoles pour faire connaissance",
+        ],
+        style: ['paragraph', 'liteSpaceBetween'],
+        margin: [75, 0, 50, 5],
+      },
+      {
+        text: 'Le plan benevole',
+        style: ['header'],
+      },
+      {
+        image: join(__dirname, '../../..', '/assets/volunteer_plan_qr.png'),
+        fit: [300, 300],
+        pageBreak: 'after',
+        pageOrientation: 'landscape',
+        style: {
+          alignment: 'center',
+        },
+      },
+    ];
   }
 
   private generateSecurityPage(): Content {
     return {
       image: join(__dirname, '../../..', '/assets/security_plan.png'),
       fit: [680, 680],
-      pageBreak: 'before',
-      pageOrientation: 'landscape',
+      pageBreak: 'after',
+      pageOrientation: 'portrait',
       style: {
         alignment: 'center',
       },
