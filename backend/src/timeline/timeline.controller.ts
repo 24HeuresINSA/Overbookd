db66e2bac7c960c615c0df2f8cdd85b63c74ba0b
Léon Dumestre <leon.dumestre@gmail.com>
2023-04-28 17:25:18 +0000

    feat(Timeline): Récupérer les FAs en cours

---
+++ backend/src/timeline/timeline.controller.ts
@@ -0,0 +1,60 @@
+import { Controller, Get, HttpCode, Query, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Permission } from 'src/auth/permissions-auth.decorator';
+import { PermissionsGuard } from 'src/auth/permissions-auth.guard';
+import { TimelineResponseDto } from './dto/timelineResponse.dto';
+import { TimelineService } from './timeline.service';
+
+@ApiBearerAuth()
+@ApiTags('timeline')
+@ApiBadRequestResponse({
+  description: 'Request is not formated as expected',
+})
+@ApiForbiddenResponse({
+  description: "User can't access this resource",
+})
+@ApiNotFoundResponse({
+  description: 'Resource not found',
+})
+@Controller('timeline')
+export class TimelineController {
+  constructor(private readonly timelineService: TimelineService) {}
+
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permission('can-view-timeline')
+  @Get()
+  @HttpCode(200)
+  @ApiQuery({
+    name: 'start',
+    required: true,
+    description: 'The start of timelines',
+    type: Date,
+  })
+  @ApiQuery({
+    name: 'end',
+    required: true,
+    description: 'The end of timelines',
+    type: Date,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The timeline has been successfully retrieved.',
+    type: TimelineResponseDto,
+    isArray: true,
+  })
+  async getTimelines(
+    @Query('start') start: Date,
+    @Query('end') end: Date,
+  ): Promise<TimelineResponseDto[]> {
+    return this.timelineService.getTimelines(new Date(start), new Date(end));
+  }
+}
---
+++ backend/src/timeline/timeline.service.ts
@@ -0,0 +1,122 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { Period } from 'src/volunteer-availability/domain/period.model';
+import { Timeline, TimelineFt } from './timeline.model';
+
+interface DatabaseFT {
+  id: number;
+  name: string;
+  timeWindows: DatabaseTimespans[];
+}
+
+interface DatabaseTimespans {
+  timespans: Period[];
+}
+
+interface DatabaseTimeline {
+  id: number;
+  name: string;
+  fts: DatabaseFT[];
+}
+
+@Injectable()
+export class TimelineService {
+  constructor(private prisma: PrismaService) {}
+
+  async getTimelines(start: Date, end: Date): Promise<Timeline[]> {
+    const where = this.buildTimelineCondition(start, end);
+    const select = this.buildTimelineSelection(start, end);
+
+    const timelines = await this.prisma.fa.findMany({
+      where,
+      select,
+    });
+    return formatTimelines(timelines);
+  }
+
+  private buildTimelineSelection(start: Date, end: Date) {
+    const ftsSelection = this.buildFtsSelection(start, end);
+    const ftsCondition = this.buildFtsCondition(start, end);
+
+    return {
+      id: true,
+      name: true,
+      fts: {
+        select: ftsSelection,
+        where: ftsCondition,
+      },
+    };
+  }
+
+  private buildTimelineCondition(start: Date, end: Date) {
+    const ftsCondition = this.buildFtsCondition(start, end);
+    return { fts: { some: ftsCondition } };
+  }
+
+  private buildFtsSelection(start: Date, end: Date) {
+    const timeWindowSelection = this.buildTimeWindowsWithTimespansSelection(
+      start,
+      end,
+    );
+    return {
+      id: true,
+      name: true,
+      ...timeWindowSelection,
+    };
+  }
+
+  private buildTimeWindowsWithTimespansSelection(start: Date, end: Date) {
+    const overlapPeriodCondition = this.buildOverlapPeriodCondition(start, end);
+    return {
+      timeWindows: {
+        select: {
+          timespans: {
+            where: overlapPeriodCondition,
+            select: {
+              start: true,
+              end: true,
+            },
+          },
+        },
+      },
+    };
+  }
+
+  private buildFtsCondition(start: Date, end: Date) {
+    const overlapPeriodCondition = this.buildOverlapPeriodCondition(start, end);
+    return {
+      timeWindows: {
+        some: {
+          timespans: { some: overlapPeriodCondition },
+        },
+      },
+    };
+  }
+
+  private buildOverlapPeriodCondition(start: Date, end: Date) {
+    return { start: { lt: end }, end: { gt: start } };
+  }
+}
+
+function formatTimelines(timelines: DatabaseTimeline[]): Timeline[] {
+  return timelines.map(formatTimeline);
+}
+
+function formatTimeline(timeline: DatabaseTimeline): Timeline {
+  const fts = formatFts(timeline.fts);
+  return {
+    fa: {
+      id: timeline.id,
+      name: timeline.name,
+    },
+    fts,
+  };
+}
+
+function formatFts(fts: DatabaseFT[]): TimelineFt[] {
+  return fts.map((ft) => ({
+    id: ft.id,
+    name: ft.name,
+    timespans: ft.timeWindows.flatMap(({ timespans }) => timespans),
+  }));
+}
