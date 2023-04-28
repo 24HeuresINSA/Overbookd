daeb6bde99c3205c0b1310a92e86024bc16833bb
Leo Mouyna <mouynaleo@gmail.com>
2023-04-28 09:52:47 +0100

    fix(planning): adjust image size. #1060

--- backend/src/volunteer-planning/render/pdf/securityPlan.ts
+++ backend/src/volunteer-planning/render/pdf/securityPlan.ts
@@ -5,7 +5,7 @@ export class SecurityPlan {
   static generatePage(): Content {
     return {
       image: join(__dirname, '../../../..', '/assets/security_plan.png'),
-      fit: [680, 680],
+      fit: [700, 700],
       pageBreak: 'after',
       style: {
         alignment: 'center',
