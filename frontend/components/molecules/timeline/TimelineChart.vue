3592e2d359dd832f844b3c5a47e3f6629348a5b7
Léon Dumestre <leon.dumestre@gmail.com>
2023-04-29 08:47:59 +0000

    feat(Timeline): Timeline page creation #1063

---
+++ frontend/components/molecules/timeline/TimelineChart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <client-only>
+      <LineChart :options="options" />
+    </client-only>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TimelineChart",
+  data: () => ({
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              autoSkip: true,
+              maxTicksLimit: 20,
+            },
+          },
+        ],
+      },
+    },
+  }),
+});
+</script>
---
+++ frontend/pages/timeline.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Timeline</h1>
+    <TimelineComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TimelineComponent from "~/components/organisms/timeline/Timeline.vue";
+
+export default Vue.extend({
+  name: "Timeline",
+  components: { TimelineComponent },
+});
+</script>
