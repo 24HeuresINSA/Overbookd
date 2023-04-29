9471d5a700d286f1be7c12d8d4a34a36d2ef0131
Leo Mouyna <mouynaleo@gmail.com>
2023-04-29 07:36:35 +0200

    fix(timeline): display tasks from events. #1065

--- frontend/components/molecules/timeline/TimelineEvents.vue
+++ frontend/components/molecules/timeline/TimelineEvents.vue
@@ -2,6 +2,13 @@
   <div class="timeline-events">
     <div v-for="event in events" :key="event.fa.id" class="timeline-event">
       <h2>{{ event.fa.name }}</h2>
+      <div
+        v-for="task in event.fts"
+        :key="task.id"
+        class="timeline-event__task"
+      >
+        <h3>{{ task.name }}</h3>
+      </div>
     </div>
   </div>
 </template>
@@ -26,15 +33,18 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .timeline-events {
   min-width: 100%;
-  padding: 5px 10px;
+  padding: 10px;
   display: flex;
   flex-direction: column;
-  gap: 5px;
+  gap: 10px;
   .timeline-event {
     min-width: 100%;
-    min-height: 100px;
     border-radius: 10px;
     background-color: $timeline-fa-content-background-color;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    padding-bottom: 5px;
 
     h2 {
       text-align: center;
@@ -42,6 +52,20 @@ export default Vue.extend({
       background-color: $timeline-fa-title-background-color;
       border-top-left-radius: 10px;
       border-top-right-radius: 10px;
+    }
+
+    &__task {
+      min-width: 100%;
+      min-height: 100px;
+      border-radius: 8px;
+      background-color: $timeline-task-content-background-color;
+      h3 {
+        text-align: center;
+        min-width: 100%;
+        background-color: $timeline-task-title-background-color;
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+      }
     }
   }
 }
