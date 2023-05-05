bb87c5fabbf243e4a77770df58d63920605448cc
Léo Mouyna <mouynaleo@gmail.com>
2023-05-05 07:01:50 +0000

    fix(timeline): display timespan assignees on click. #1085

--- frontend/components/molecules/timeline/TimelineEvents.vue
+++ frontend/components/molecules/timeline/TimelineEvents.vue
@@ -10,9 +10,8 @@
           width: computeTaskWidth(task),
           marginLeft: computeTaskLeftMargin(task),
         }"
-        @click="openFtInNewTab(task.id)"
       >
-        <h3>
+        <h3 @click="openFtInNewTab(task.id)">
           {{ task.name }}
           <v-icon v-show="task.hasPriority" color="orange">
             mdi-alert-circle
@@ -26,35 +25,52 @@
             width: computeTimeWindowWidth(task, timeWindow),
             marginLeft: computeTimeWindowLeftMargin(task, timeWindow),
           }"
-        ></div>
+        >
+          <div
+            v-for="timespan in timeWindow.timespans"
+            :key="timespan.id"
+            class="timespan"
+            :style="{
+              width: computeTimespanWidth(timeWindow, timespan),
+            }"
+            @click="openTimespanDetailsDialog(timespan.id)"
+          ></div>
+        </div>
       </div>
     </div>
+    <v-dialog v-model="displayTimespanDetailsDialog" width="1000px">
+      <FTTimespanDetails @close-dialog="closeTimespanDetailsDialog" />
+    </v-dialog>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TeamChip from "~/components/atoms/chip/TeamChip.vue";
-import { Period, getPeriodDuration } from "~/utils/models/period";
+import FTTimespanDetails from "~/components/organisms/festivalEvent/ft/FTTimespanDetails.vue";
+import { Period } from "~/utils/models/period";
 import {
   TimelineEvent,
   TimelineFt,
   TimelineTimeWindow,
+  TimelineTimespan,
 } from "~/utils/models/timeline";
 import { marginPercent, widthPercent } from "~/utils/timeline/placement";
 
 export default Vue.extend({
   name: "TimelineEvents",
-  components: { TeamChip },
+  components: { TeamChip, FTTimespanDetails },
+  data: () => {
+    return {
+      displayTimespanDetailsDialog: false,
+    };
+  },
   computed: {
     events(): TimelineEvent[] {
       return this.$accessor.timeline.filteredEvents;
     },
-    period(): Period {
+    range(): Period {
       return this.$accessor.timeline.period;
-    },
-    duration(): number {
-      return getPeriodDuration(this.period);
     },
   },
   created() {
@@ -69,13 +85,24 @@ export default Vue.extend({
         ...task.timeWindows.map(({ end }) => end.getTime())
       );
       return {
-        start: new Date(Math.max(lowestStartDate, this.period.start.getTime())),
-        end: new Date(Math.min(highestEndDate, this.period.end.getTime())),
+        start: new Date(Math.max(lowestStartDate, this.range.start.getTime())),
+        end: new Date(Math.min(highestEndDate, this.range.end.getTime())),
       };
+    },
+    adjustPeriodToRange(period: Period): Period {
+      const PeriodStart = period.start.getTime();
+      const rangeStart = this.range.start.getTime();
+      const periodEnd = period.end.getTime();
+      const rangeEnd = this.range.end.getTime();
+
+      const start = new Date(Math.max(PeriodStart, rangeStart));
+      const end = new Date(Math.min(periodEnd, rangeEnd));
+
+      return { start, end };
     },
     computeTaskWidth(task: TimelineFt): string {
       const taskPeriod = this.buildTaskPeriod(task);
-      const width = widthPercent(this.period, taskPeriod);
+      const width = widthPercent(this.range, taskPeriod);
       return `${width.toFixed(2)}%`;
     },
     computeTimeWindowWidth(
@@ -83,12 +110,22 @@ export default Vue.extend({
       timeWindow: TimelineTimeWindow
     ): string {
       const taskPeriod = this.buildTaskPeriod(task);
-      const width = widthPercent(taskPeriod, timeWindow);
+      const timeWindowPeriod = this.adjustPeriodToRange(timeWindow);
+      const width = widthPercent(taskPeriod, timeWindowPeriod);
+      return `${width.toFixed(2)}%`;
+    },
+    computeTimespanWidth(
+      timeWindow: TimelineTimeWindow,
+      timespan: TimelineTimespan
+    ): string {
+      const timeWindowPeriod = this.adjustPeriodToRange(timeWindow);
+      const timespanPeriod = this.adjustPeriodToRange(timespan);
+      const width = widthPercent(timeWindowPeriod, timespanPeriod);
       return `${width.toFixed(2)}%`;
     },
     computeTaskLeftMargin(task: TimelineFt): string {
       const taskPeriod = this.buildTaskPeriod(task);
-      const margin = marginPercent(this.period, taskPeriod);
+      const margin = marginPercent(this.range, taskPeriod);
       return `${margin.toFixed(2)}%`;
     },
     computeTimeWindowLeftMargin(
@@ -101,6 +138,13 @@ export default Vue.extend({
     },
     openFtInNewTab(ftId: number) {
       window.open(`/ft/${ftId}`, "_blank");
+    },
+    openTimespanDetailsDialog(timespanId: number) {
+      this.$accessor.assignment.fetchTimespanDetails(timespanId);
+      this.displayTimespanDetailsDialog = true;
+    },
+    closeTimespanDetailsDialog() {
+      this.displayTimespanDetailsDialog = false;
     },
   },
 });
@@ -132,8 +176,8 @@ export default Vue.extend({
     &__task {
       border-radius: 8px;
       background-color: $timeline-task-content-background-color;
-      cursor: pointer;
       h3 {
+        cursor: pointer;
         text-align: center;
         min-width: 100%;
         background-color: $timeline-task-title-background-color;
@@ -141,16 +185,33 @@ export default Vue.extend({
         border-top-right-radius: 8px;
       }
       .timewindow {
+        --time-window-height: 20px;
         margin-top: 5px;
-        min-height: 20px;
+        min-height: var(--time-window-height);
         cursor: pointer;
         background-color: $timeline-timewindow-background-color;
-        border-radius: 10px;
+        border-radius: calc(var(--time-window-height) / 2);
+        display: flex;
         &:last-of-type {
           margin-bottom: 10px;
         }
         &:first-of-type {
           margin-top: 10px;
+        }
+        .timespan {
+          min-height: 100%;
+          border-right-color: black;
+          border-right-width: 3px;
+          border-right-style: solid;
+          &:first-of-type {
+            border-bottom-left-radius: calc(var(--time-window-height) / 2);
+            border-top-left-radius: calc(var(--time-window-height) / 2);
+          }
+          &:last-of-type {
+            border-bottom-right-radius: calc(var(--time-window-height) / 2);
+            border-top-right-radius: calc(var(--time-window-height) / 2);
+            border-right: none;
+          }
         }
       }
     }
