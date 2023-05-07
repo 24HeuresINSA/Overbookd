d216f08d4b11b41ee38c6d7adf5eb6a7e1afb03f
Leo Mouyna <mouynaleo@gmail.com>
2023-05-07 02:59:27 +0200

    fix(need-help): add forgotten file. #1091

---
+++ frontend/components/molecules/need-help/NeedHelpFormCard.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-card class="need">
+    <v-card-title> Besoin </v-card-title>
+    <v-card-text class="forms">
+      <fieldset>
+        <legend>Plage horaire</legend>
+        <DateTimeField v-model="start" label="Début" :boxed="false" />
+        <DateTimeField v-model="end" label="Fin" :boxed="false" />
+        <v-btn color="success" class="btn" @click="getVolunteers">
+          Appliquer
+        </v-btn>
+      </fieldset>
+      <fieldset>
+        <legend>Filtres</legend>
+        <SearchTeams
+          v-model="teams"
+          label="Filtrer par équipe"
+          :boxed="false"
+        />
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-outline-search"
+          label="Nom du bénévole"
+          clearable
+          clear-icon="mdi-close-circle-outline"
+          :solo="false"
+          :filled="false"
+        ></v-text-field>
+      </fieldset>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import DateTimeField from "~/components/atoms/field/date/DateTimeField.vue";
+import SearchTeams from "~/components/atoms/field/search/SearchTeams.vue";
+import { Period } from "~/utils/models/period";
+import { Team } from "~/utils/models/team";
+
+export default Vue.extend({
+  name: "NeedHelpFormCard",
+  components: { DateTimeField, SearchTeams },
+  data: () => {
+    return {
+      start: new Date(),
+      end: new Date(),
+    };
+  },
+  computed: {
+    period(): Period {
+      return {
+        start: this.start,
+        end: this.end,
+      };
+    },
+    search: {
+      get(): string {
+        return this.$accessor.needHelp.search;
+      },
+      set(value: string | null) {
+        this.$accessor.needHelp.updateSearch(value);
+      },
+    },
+    teams: {
+      get(): Team[] {
+        return this.$accessor.needHelp.teams;
+      },
+      set(value: Team[]) {
+        this.$accessor.needHelp.updateTeams(value);
+      },
+    },
+  },
+  created() {
+    this.start = this.$accessor.needHelp.start;
+    this.end = this.$accessor.needHelp.end;
+  },
+  methods: {
+    getVolunteers() {
+      this.$accessor.needHelp.updatePeriod(this.period);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.forms {
+  position: sticky;
+  top: $header-height;
+}
+
+fieldset {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 10px;
+}
+</style>
