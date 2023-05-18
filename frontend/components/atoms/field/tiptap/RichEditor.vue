48ccad7b621033800fbdf54d81d9e5be790a6bce
Léon Dumestre <leon.dumestre@gmail.com>
2023-05-18 12:31:06 +0000

    refactor(Front): new Tiptap and RichEditor #920

---
+++ frontend/components/atoms/field/tiptap/RichEditor.vue
@@ -0,0 +1,98 @@
+<template>
+  <div v-if="editor" class="editor">
+    <TiptapMenu class="editor__header" :editor="editor" :disabled="true" />
+    <EditorContent
+      class="editor__content"
+      :class="{ disabled: disabled }"
+      :editor="editor"
+    />
+  </div>
+</template>
+
+<script>
+import StarterKit from "@tiptap/starter-kit";
+import { Editor, EditorContent } from "@tiptap/vue-2";
+
+import TiptapMenu from "./TiptapMenu.vue";
+
+export default {
+  name: "RichEditor",
+  components: {
+    EditorContent,
+    TiptapMenu,
+  },
+  props: {
+    data: {
+      type: String,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      editor: null,
+    };
+  },
+  watch: {
+    data() {
+      this.editor.commands.setContent(this.data ?? "");
+      this.editor.setEditable(!this.disabled);
+    },
+  },
+  mounted() {
+    this.editor = new Editor({
+      extensions: [StarterKit],
+      content: this.data ?? "",
+      editable: !this.disabled,
+      onBlur: () => this.update(),
+    });
+  },
+  beforeUnmount() {
+    this.editor.destroy();
+  },
+  methods: {
+    update() {
+      this.$emit("change", this.editor.getHTML());
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.editor {
+  background-color: #fff;
+  border: 3px solid #424242;
+  border-radius: 0.75rem;
+  color: #0d0d0d;
+  display: flex;
+  flex-direction: column;
+  max-height: 26rem;
+
+  &__header {
+    align-items: center;
+    background: #424242;
+    border-bottom: 3px solid #424242;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+    display: flex;
+    flex: 0 0 auto;
+    flex-wrap: wrap;
+    padding: 0.25rem;
+  }
+
+  &__content {
+    flex: 1 1 auto;
+    overflow-x: hidden;
+    overflow-y: auto;
+    padding: 1rem 1rem 0 1rem;
+    -webkit-overflow-scrolling: touch;
+  }
+}
+
+.disabled {
+  opacity: 0.8;
+}
+</style>
---
+++ frontend/components/atoms/field/tiptap/TiptapMenuItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <button
+    class="menu-item"
+    type="button"
+    :class="{ 'is-active': isActive ? isActive() : null }"
+    :title="title"
+    @click="action"
+  >
+    <svg class="remix">
+      <use :xlink:href="`${remixiconUrl}#ri-${icon}`" />
+    </svg>
+  </button>
+</template>
+
+<script>
+import remixiconUrl from "remixicon/fonts/remixicon.symbol.svg";
+
+export default {
+  name: "TiptapMenuItem",
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    action: {
+      type: Function,
+      required: true,
+    },
+
+    isActive: {
+      type: Function,
+      default: null,
+    },
+  },
+
+  data() {
+    return {
+      remixiconUrl,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.menu-item {
+  background: transparent;
+  border: none;
+  border-radius: 0.4rem;
+  color: #fff !important;
+  cursor: pointer;
+  height: 1.75rem;
+  padding: 0.25rem;
+  margin-right: 0.25rem;
+  width: 1.75rem;
+
+  svg {
+    fill: currentColor;
+    height: 100%;
+    width: 100%;
+  }
+
+  &.is-active,
+  &:hover {
+    background-color: #303030;
+  }
+}
+</style>
