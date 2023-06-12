c255fb7b363fd50341468a3db131660a3134fedf
Leon Dumestre <leon.dumestre@gmail.com>
2023-06-12 19:47:21 +0200

    chore(Front): update nuxt packages #1101

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -28,9 +28,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: "~/plugins/chart.js", mode: "client" }],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
