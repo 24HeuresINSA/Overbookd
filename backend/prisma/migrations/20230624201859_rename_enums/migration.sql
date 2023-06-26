306543516da17ee3b6e2be6d6052c67997b762fe
Leon Dumestre <leon.dumestre@gmail.com>
2023-06-26 13:27:28 +0200

    refactor: remove timeWindowType #1105

--- backend/prisma/migrations/20230624201859_rename_enums/migration.sql
+++ backend/prisma/migrations/20230624201859_rename_enums/migration.sql
@@ -5,3 +5,8 @@ ALTER TYPE "Status" RENAME TO "fa_status";
 ALTER TYPE "subject_type" RENAME TO "fa_feedback_subject_type";
 ALTER TYPE "ft_subject_type" RENAME TO "ft_feedback_subject_type";
 ALTER TYPE "time_windows_type" RENAME TO "time_window_type";
+
+-- AlterTable
+ALTER TABLE "fa_time_window" DROP COLUMN "type";
+-- DropEnum
+DROP TYPE "time_window_type";
