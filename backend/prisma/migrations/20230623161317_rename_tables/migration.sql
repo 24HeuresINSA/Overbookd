5300f31ab9beaf612ca676959690aabf96785832
Leon Dumestre <leon.dumestre@gmail.com>
2023-06-26 09:41:36 +0200

    refactor: is_major to is_flagship #1103

--- backend/prisma/migrations/20230623161317_rename_tables/migration.sql
+++ backend/prisma/migrations/20230623161317_rename_tables/migration.sql
@@ -208,7 +208,7 @@ ALTER TABLE "fa_site_publish_animation" RENAME COLUMN "photo_link " TO "photo_li
 ALTER TABLE "fa" RENAME COLUMN "fa_type_name" TO "fa_type";
 ALTER TABLE "fa" RENAME COLUMN "security_needs" TO "security_need";
 ALTER TABLE "fa" RENAME COLUMN "water_needs" TO "water_need";
-ALTER TABLE "fa_site_publish_animation" RENAME COLUMN "isMajor" TO "is_major";
+ALTER TABLE "fa_site_publish_animation" RENAME COLUMN "isMajor" TO "is_flagship";
 ALTER TABLE "assignment" RENAME COLUMN "timespan_id" TO "time_span_id";
 ALTER TABLE "availability" RENAME COLUMN "timeslot_id" TO "time_slot_id";
 ALTER TABLE "ft_team_request" RENAME COLUMN "time_windows_id" TO "ft_time_window_id";
