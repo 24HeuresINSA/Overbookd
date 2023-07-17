91f5b7a93a806083c2d9316fc6a5bad11c165ab1
Leo Mouyna <mouynaleo@gmail.com>
2023-07-17 08:25:52 +0200

    fix: configuration and signa location dtos. #1152

--- apps/api/src/signa_location/signa_location.controller.ts
+++ apps/api/src/signa_location/signa_location.controller.ts
@@ -3,19 +3,21 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   ParseIntPipe,
   Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../authentication/jwt-auth.guard';
 import { Permission } from '../authentication/permissions-auth.decorator';
 import { PermissionsGuard } from '../authentication/permissions-auth.guard';
 import { CreateSignaLocationDto } from './dto/create-signa_location.dto';
 import { UpdateSignaLocationDto } from './dto/update-signa_location.dto';
 import { SignaLocationService } from './signa_location.service';
+import { SignaLocationRepresentation } from '../fa/fa.model';
 
 @ApiBearerAuth()
 @ApiTags('signa-location')
@@ -26,6 +28,13 @@ export class SignaLocationController {
   @UseGuards(JwtAuthGuard, PermissionsGuard)
   @Permission('manage-location')
   @Post()
+  @ApiBody({
+    type: CreateSignaLocationDto,
+  })
+  @ApiResponse({
+    status: 201,
+    type: SignaLocationRepresentation,
+  })
   create(@Body() createSignaLocationDto: CreateSignaLocationDto) {
     return this.signaLocationService.create(createSignaLocationDto);
   }
@@ -33,6 +42,11 @@ export class SignaLocationController {
   @UseGuards(JwtAuthGuard, PermissionsGuard)
   @Permission('hard')
   @Get()
+  @ApiResponse({
+    status: 200,
+    isArray: true,
+    type: SignaLocationRepresentation,
+  })
   findAll() {
     return this.signaLocationService.findAll();
   }
@@ -40,6 +54,10 @@ export class SignaLocationController {
   @UseGuards(JwtAuthGuard, PermissionsGuard)
   @Permission('hard')
   @Get(':id')
+  @ApiResponse({
+    status: 200,
+    type: SignaLocationRepresentation,
+  })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.signaLocationService.findOne(id);
   }
@@ -47,6 +65,10 @@ export class SignaLocationController {
   @UseGuards(JwtAuthGuard, PermissionsGuard)
   @Permission('orga')
   @Patch(':id')
+  @ApiResponse({
+    status: 200,
+    type: SignaLocationRepresentation,
+  })
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateSignaLocationDto: UpdateSignaLocationDto,
@@ -57,6 +79,8 @@ export class SignaLocationController {
   @UseGuards(JwtAuthGuard, PermissionsGuard)
   @Permission('manage-location')
   @Delete(':id')
+  @HttpCode(204)
+  @ApiResponse({ status: 204 })
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.signaLocationService.remove(id);
   }
