0ca380b324b6813f27d2007c484cad82442ad75d
Leon Dumestre <leon.dumestre@gmail.com>
2023-08-02 13:03:16 +0200

    refactor: replace current register description #1164

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -467,7 +467,29 @@ async function main() {
   console.log('Creating of register form config');
   const registerFormConfig: Configuration = {
     key: 'registerForm',
-    value: { description: "" },
+    value: { 
+      description: `<p>Hello et merci d'être venu jusqu'ici ! 🤗</p>
+        <p>
+          Le festival des 24 heures de l'INSA n'est possible que grâce aux
+          plus de
+          <strong>350 bénévoles</strong> qui, comme toi, ont accepté de venir
+          nous donner un coup de main pour préparer, organiser et faire en
+          sorte que les festivaliers passent le meilleur moment possible. 🎉
+        </p>
+        <p>
+          Pour que l'on puisse t'accueillir dans les meilleures conditions il
+          nous faut cependant quelques informations sur toi et le temps que tu
+          souhaites consacrer à nous aider.
+        </p>
+        <p>
+          Si tu as la moindre question sur le festival et ce que tu peux faire
+          pour nous aider, n'hésite pas a contacter les
+          <a href="mailto:humains@24heures.org">responsables bénévoles</a>.
+          <br />
+          Ils seront tes principaux interlocuteurs en amont et en aval du
+          festival. 😎
+        </p>`
+    },
   };
   await prisma.configuration.upsert({
     where: { key: 'registerForm' },
--- apps/web/pages/register.vue
+++ apps/web/pages/register.vue
@@ -18,27 +18,7 @@
         </v-stepper-step>
 
         <v-stepper-content step="1">
-          <p>Hello et merci d'être venu jusqu'ici ! 🤗</p>
-          <p>
-            Le festival des 24 heures de l'INSA n'est possible que grâce aux
-            plus de
-            <strong>350 bénévoles</strong> qui, comme toi, ont accepté de venir
-            nous donner un coup de main pour préparer, organiser et faire en
-            sorte que les festivaliers passent le meilleur moment possible. 🎉
-          </p>
-          <p>
-            Pour que l'on puisse t'accueillir dans les meilleures conditions il
-            nous faut cependant quelques informations sur toi et le temps que tu
-            souhaites consacrer à nous aider.
-          </p>
-          <p>
-            Si tu as la moindre question sur le festival et ce que tu peux faire
-            pour nous aider, n'hésite pas a contacter les
-            <a href="mailto:humains@24heures.org">responsables bénévoles</a>.
-            <br />
-            Ils seront tes principaux interlocuteurs en amont et en aval du
-            festival. 😎
-          </p>
+          {{ registerDescription } }}
           <v-btn color="primary" @click="step = 2"> C'est parti ! 🚀 </v-btn>
           <v-btn text @click="returnToLogin"> Annuler </v-btn>
         </v-stepper-content>
