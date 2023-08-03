feb89ff8b7457207e5d161a274eb3020ca5f0d26
Leon Dumestre <leon.dumestre@gmail.com>
2023-08-03 20:47:59 +0200

    fix(Api): elec need migration #1173

--- apps/api/prisma/migrations/20230803181430_elec_need_required_field/migration.sql
+++ apps/api/prisma/migrations/20230803181430_elec_need_required_field/migration.sql
@@ -1,13 +1,10 @@
-/*
-  Warnings:
+UPDATE fa_electricity_need
+SET device = '_', count = 1
+WHERE device IS NULL OR count IS NULL;
 
-  - Made the column `count` on table `fa_electricity_need` required. This step will fail if there are existing NULL values in that column.
-  - Made the column `device` on table `fa_electricity_need` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "fa_electricity_need" DROP COLUMN "count";
-ALTER TABLE "fa_electricity_need" DROP COLUMN "device";
-
-ALTER TABLE "fa_electricity_need" ADD COLUMN "count" integer NOT NULL;
-ALTER TABLE "fa_electricity_need" ADD COLUMN "device" integer NOT NULL;
+-- Then, add the non-null constraints on the 'device' and 'count' columns
+ALTER TABLE fa_electricity_need
+ALTER COLUMN device SET NOT NULL,
+ALTER COLUMN count SET NOT NULL,
+ALTER COLUMN power SET DEFAULT 1,
+ALTER COLUMN count SET DEFAULT 1;
