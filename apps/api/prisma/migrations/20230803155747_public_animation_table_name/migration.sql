a801d26ef744758522db56de12c8f62442909e15
Leon Dumestre <leon.dumestre@gmail.com>
2023-08-04 10:01:10 +0200

    fix: migration #1169

--- apps/api/prisma/migrations/20230803155747_public_animation_table_name/migration.sql
+++ apps/api/prisma/migrations/20230803155747_public_animation_table_name/migration.sql
@@ -1,2 +1,8 @@
 -- RenameTable
 ALTER TABLE "fa_site_publish_animation" RENAME TO "public_animation";
+
+-- AlterTable
+ALTER TABLE "public_animation" RENAME CONSTRAINT "fa_site_publish_animation_pkey" TO "public_animation_pkey";
+
+-- RenameForeignKey
+ALTER TABLE "public_animation" RENAME CONSTRAINT "fa_site_publish_animation_fa_id_fkey" TO "public_animation_fa_id_fkey";
