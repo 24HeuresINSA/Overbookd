93222ece2b0affe5e85553bacd162eff156023fe
Leo Mouyna <mouynaleo@gmail.com>
2023-08-21 16:55:12 +0200

    feat(ci): expose prune command on each package. #1200

--- domains/volunteer-availability/src/volunteer-availability.spec.ts
+++ domains/volunteer-availability/src/volunteer-availability.spec.ts
@@ -20,29 +20,29 @@ describe("volunteer availability", () => {
     describe("when start is before end", () => {
       describe("when period starts in party shift [18:00-06:00]", () => {
         it("should be able to create availability on pair hours", () => {
-          const period: Period = {
+          const period = Period.init({
             start: new Date("2023-05-12 22:00+02:00"),
             end: new Date("2023-05-13 02:00+02:00"),
-          };
+          });
           const availability = Availability.fromPeriod(period);
           expect(availability.start).toBe(period.start);
           expect(availability.end).toBe(period.end);
         });
         describe("when starting on odd hour", () => {
           it("should be able to create availability ", () => {
-            const period: Period = {
+            const period = Period.init({
               start: new Date("2023-05-13 01:00+02:00"),
               end: new Date("2023-05-13 03:00+02:00"),
-            };
+            });
             const availability = Availability.fromPeriod(period);
             expect(availability.start).toBe(period.start);
             expect(availability.end).toBe(period.end);
           });
           it("should handle UTC date", () => {
-            const period: Period = {
+            const period = Period.init({
               start: new Date("2023-05-13 17:00Z"),
               end: new Date("2023-05-13 19:00Z"),
-            };
+            });
             const availability = Availability.fromPeriod(period);
             expect(availability.start).toBe(period.start);
             expect(availability.end).toBe(period.end);
@@ -51,29 +51,29 @@ describe("volunteer availability", () => {
       });
       describe("when period starts in night shift [06:00-10:00]", () => {
         it("should be able to create availability on pair hours", () => {
-          const period: Period = {
+          const period = Period.init({
             start: new Date("2023-05-13 06:00+02:00"),
             end: new Date("2023-05-13 08:00+02:00"),
-          };
+          });
           const availability = Availability.fromPeriod(period);
           expect(availability.start).toBe(period.start);
           expect(availability.end).toBe(period.end);
         });
         describe("when starting in odd hour", () => {
           it("should inform that start should be a pair hour", () => {
-            const period: Period = {
+            const period = Period.init({
               start: new Date("2023-05-13 07:00+02:00"),
               end: new Date("2023-05-13 09:00+02:00"),
-            };
+            });
             expect(() => Availability.fromPeriod(period)).toThrow(
               AVAILABILITY_ERROR_MESSAGES.START_HOUR,
             );
           });
           it("should handle UTC date", () => {
-            const period: Period = {
+            const period = Period.init({
               start: new Date("2023-05-13 05:00Z"),
               end: new Date("2023-05-13 07:00Z"),
-            };
+            });
             expect(() => Availability.fromPeriod(period)).toThrow(
               AVAILABILITY_ERROR_MESSAGES.START_HOUR,
             );
@@ -82,39 +82,39 @@ describe("volunteer availability", () => {
       });
       describe("when period starts in day shift [10:00-18:00]", () => {
         it("should be able to create availability on pair hours", () => {
-          const period: Period = {
+          const period = Period.init({
             start: new Date("2023-05-13 12:00+02:00"),
             end: new Date("2023-05-13 16:00+02:00"),
-          };
+          });
           const availability = Availability.fromPeriod(period);
           expect(availability.start).toBe(period.start);
           expect(availability.end).toBe(period.end);
         });
         describe("when starting in odd hour", () => {
           it("should inform that start should be a pair hour", () => {
-            const period: Period = {
+            const period = Period.init({
               start: new Date("2023-05-13 11:00+02:00"),
               end: new Date("2023-05-13 13:00+02:00"),
-            };
+            });
             expect(() => Availability.fromPeriod(period)).toThrow(
               AVAILABILITY_ERROR_MESSAGES.START_HOUR,
             );
           });
           it("should handle UTC date", () => {
-            const period: Period = {
+            const period = Period.init({
               start: new Date("2023-05-13 09:00Z"),
               end: new Date("2023-05-13 11:00Z"),
-            };
+            });
             expect(() => Availability.fromPeriod(period)).toThrow(
               AVAILABILITY_ERROR_MESSAGES.START_HOUR,
             );
           });
         });
         it("should inform that start should be a pair hour", () => {
-          const period: Period = {
+          const period = Period.init({
             start: new Date("2023-05-13 11:00+02:00"),
             end: new Date("2023-05-13 13:00+02:00"),
-          };
+          });
           expect(() => Availability.fromPeriod(period)).toThrow(
             AVAILABILITY_ERROR_MESSAGES.START_HOUR,
           );
@@ -150,10 +150,10 @@ describe("volunteer availability", () => {
   describe("period duration", () => {
     describe("when duration is at least 2 hours", () => {
       it("should create availability", () => {
-        const period: Period = {
+        const period = Period.init({
           start: new Date("2023-05-13 01:00+02:00"),
           end: new Date("2023-05-13 10:00+02:00"),
-        };
+        });
         const availability = Availability.fromPeriod(period);
         expect(availability.start).toBe(period.start);
         expect(availability.end).toBe(period.end);
@@ -161,10 +161,10 @@ describe("volunteer availability", () => {
     });
     describe("when duration is less than 2 hours", () => {
       it("should inform that period should last at least 2 hours", () => {
-        const period: Period = {
+        const period = Period.init({
           start: new Date("2023-05-13 01:00+02:00"),
           end: new Date("2023-05-13 02:00+02:00"),
-        };
+        });
         expect(() => Availability.fromPeriod(period)).toThrow(
           AVAILABILITY_ERROR_MESSAGES.MINIMUM_PERIOD_DURATION,
         );
