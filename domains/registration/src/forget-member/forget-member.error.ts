029c72f77265b0420fa2a139c24d6bce4bba65cd
Léo Mouyna <mouynaleo@gmail.com>
2023-09-04 13:14:24 +0200

    fix: suggest secretary only as contact when in debt. #1220

--- domains/registration/src/forget-member/forget-member.error.ts
+++ domains/registration/src/forget-member/forget-member.error.ts
@@ -2,7 +2,7 @@ export const ASSINGED_IN_FUTUR_TASK_ERROR_MESSAGE =
   "Nous ne pouvons pas t'effacer d'Overbookd.\nTu es affecté à une tâche à venir.\nContacte les responsables bénévoles pour t'aider.";
 
 export const IN_DEBT_ERROR_MESSAGE =
-  "Nous ne pouvons pas t'effacer d'Overbookd.\nTu as des dettes auprès de l'association.\nContacte le trésorier ou le secrétaire général pour t'aider.";
+  "Nous ne pouvons pas t'effacer d'Overbookd.\nTu as des dettes auprès de l'association.\nContacte le secrétaire général pour t'aider.";
 
 class ForgetMemberError extends Error {}
 
