a46cb296387c71d61f522b9765368fb4d95d8f4c
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2023-09-09 20:54:30 +0000

    refactor: change sql script for data cleanning

--- tools/sql-script/clean-data-48e.sql
+++ tools/sql-script/clean-data-48e.sql
@@ -4,11 +4,9 @@ DELETE FROM "charisma_period";
 DELETE FROM "collaborator";
 DELETE FROM "fa_electricity_need";
 DELETE FROM "fa_feedback";
-DELETE FROM "fa_refuse";
 DELETE FROM "fa_signa_need";
 DELETE FROM "public_animation";
 DELETE FROM "fa_time_window";
-DELETE FROM "fa_validation";
 DELETE FROM "fa";
 DELETE FROM "friend";
 DELETE FROM "ft_feedback";
@@ -27,9 +25,8 @@ DELETE FROM "volunteer_availability";
 UPDATE "team" SET code = 'beboo' WHERE code = 'maman';
 UPDATE "team" SET name = 'BeBoo' WHERE name = 'maman';
 
--- Add new teams : roti and benevole
+-- Add new team benevole
 INSERT INTO "team" (code, name, color, icon) VALUES
-('roti', 'roti', '#40F6D8', 'mdi-glass-mug-variant'),
 ('benevole', 'bénévole', '#09A1C4', 'mdi-account');
 
 -- Set too false all has_payed_contributions to false for all user
@@ -37,8 +34,8 @@ UPDATE "user" SET has_payed_contributions = false;
 
 -- Remove all user team except hard, vieux, admin, fen, voiture, camion, conducteur, conducteur-fen
 DELETE FROM "user_team" WHERE
-  team_id NOT IN
-    (SELECT DISTINCT "team".id FROM "team"
+  team_code NOT IN
+    (SELECT DISTINCT "team".code FROM "team"
     WHERE code IN ('hard', 'vieux', 'admin', 'fen', 'voiture', 'camion', 'conducteur', 'conducteur-fen'));
 
 -- Remove hard team from user having hard and vieux
@@ -46,24 +43,30 @@ DELETE FROM "user_team" WHERE
   user_id IN
     (SELECT DISTINCT "user".id FROM "user"
     JOIN "user_team" ON "user".id = "user_team".user_id
-    JOIN "team" ON "user_team".team_id = "team".id
+    JOIN "team" ON "user_team".team_code = "team".code
     WHERE code = 'hard')
 AND
   user_id IN
     (SELECT DISTINCT "user".id FROM "user"
     JOIN "user_team" ON "user".id = "user_team".user_id
-    JOIN "team" ON "user_team".team_id = "team".id
+    JOIN "team" ON "user_team".team_code = "team".code
     WHERE code = 'vieux')
 AND
-  team_id IN
-    (SELECT DISTINCT "team".id FROM "team"
+  team_code IN
+    (SELECT DISTINCT "team".code FROM "team"
     WHERE code = 'hard');
 
 
 -- Change user team hard to vieux
-UPDATE "user_team" SET team_id =
-  (SELECT DISTINCT "team".id FROM "team"
+UPDATE "user_team" SET team_code =
+  (SELECT DISTINCT "team".code FROM "team"
   WHERE code = 'vieux')
-WHERE team_id IN
-  (SELECT DISTINCT "team".id FROM "team"
+WHERE team_code IN
+  (SELECT DISTINCT "team".code FROM "team"
   WHERE code = 'hard');
+
+-- Add benevole team to vieux user
+INSERT INTO "user_team" ("user_id", "team_code")
+SELECT "user_id", 'benevole'
+FROM "user_team"
+WHERE "team_code" = 'vieux';
