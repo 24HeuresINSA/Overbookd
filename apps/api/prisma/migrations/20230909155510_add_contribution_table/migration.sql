403df0a15704d4f0d15e3f2b0277379acf66e1cb
Leon Dumestre <leon.dumestre@gmail.com>
2023-09-09 17:55:52 +0200

    feat(api): add expiration table #1211

---
+++ apps/api/prisma/migrations/20230909155510_add_contribution_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "contribution" (
+    "id" SERIAL NOT NULL,
+    "is_payed" BOOLEAN NOT NULL DEFAULT false,
+    "payment_date" TIMESTAMP(3) NOT NULL,
+    "expiration_date" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "contribution_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "contribution" ADD CONSTRAINT "contribution_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
