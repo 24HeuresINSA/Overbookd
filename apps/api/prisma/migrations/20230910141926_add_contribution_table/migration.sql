e2799df221cd218fcbb30931c23bf2e0005eede7
Leon Dumestre <leon.dumestre@gmail.com>
2023-09-10 23:42:18 +0200

    refactor: remove hasPayedContribution in db #1211

---
+++ apps/api/prisma/migrations/20230910141926_add_contribution_table/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `has_payed_contributions` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "has_payed_contributions";
+
+-- CreateTable
+CREATE TABLE "contribution" (
+    "user_id" INTEGER NOT NULL,
+    "expiration_date" TIMESTAMP(3) NOT NULL,
+    "payment_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" DOUBLE PRECISION NOT NULL DEFAULT 100,
+
+    CONSTRAINT "contribution_pkey" PRIMARY KEY ("user_id","expiration_date")
+);
+
+-- AddForeignKey
+ALTER TABLE "contribution" ADD CONSTRAINT "contribution_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
