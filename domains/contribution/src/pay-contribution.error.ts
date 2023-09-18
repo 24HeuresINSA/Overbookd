39ed44e45198e7a6e31798f4cb1f0af9595ad2a3
Leon Dumestre <leon.dumestre@gmail.com>
2023-09-18 18:20:58 +0200

    fix: export class #1211

--- domains/contribution/src/pay-contribution.error.ts
+++ domains/contribution/src/pay-contribution.error.ts
@@ -3,7 +3,7 @@ export const HAS_ALREADY_PAYED_ERROR_MESSAGE =
 
 export const INSUFFICIENT_AMOUNT_ERROR_MESSAGE = "Le montant doit être supérieur ou égal à 1€";
 
-export class PayContributionError extends Error {}
+class PayContributionError extends Error {}
 
 export class HasAlreadyPayed extends PayContributionError {
   constructor() {
