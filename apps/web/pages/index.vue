fca08bb814ed4690f66a1fd6f8b5771ebb302c23
Leo Mouyna <mouynaleo@gmail.com>
2023-09-22 20:00:14 +0200

    chore: clean home page. #1231

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,126 +1,40 @@
 <template>
-  <div>
-    <v-container class="align-stretch flex-wrap align-content-start">
-      <v-row>
-        <v-col cols="12" sm="6" md="4">
-          <UserCard />
-        </v-col>
+  <div class="home">
+    <h1>Bienvenue sur Overbookd 👋</h1>
 
-        <v-col v-if="canDownloadPlanning" cols="12" sm="6" md="8">
-          <PlanningDownloadCard />
-        </v-col>
-
-        <v-col v-else cols="12" sm="6" md="8">
-          <v-card>
-            <v-card-title><h3>Tu n'as pas été validé</h3></v-card-title>
-            <v-card-text style="font-size: 1.1em">
-              <p>
-                Tu n'as pas encore été validé par les responsables bénévoles
-                <br />
-                N'hésite pas
-                <strong> a compléter tes disponibilités </strong> pour augmenter
-                tes chances.
-              </p>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" class="cta" to="/availabilities">
-                rajouter des disponibilités
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="4">
-          <FriendsCard />
-        </v-col>
-        <v-col cols="12" sm="6" md="8">
-          <PersonnalAccountCard v-if="havePersonnalAccount" />
-          <CommentEditionCard v-else />
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-text-field class="search"></v-text-field>
 
     <SnackNotificationContainer />
-    <!-- On commente cette partie pour le moment pour que les personnes sans role puissent accéder a leur dispo
-    <v-dialog v-model="hasNotBeenApproved" max-width="600" persistent>
-      <v-card>
-        <v-card-title>Oupsss</v-card-title>
-        <v-card-text>
-          Merci de rejoindre l'asso mais il faut qu'un admin active ton compte
-          (demande à Maëlle)..
-        </v-card-text>
-        <v-card-actions>
-          <v-btn text @click="logout">DÉCONNEXION</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    -->
   </div>
 </template>
 
 <script>
-import UserCard from "~/components/organisms/user/data/UserCard.vue";
+import Vue from "vue";
 import SnackNotificationContainer from "~/components/molecules/snack/SnackNotificationContainer.vue";
-import PersonnalAccountCard from "~/components/organisms/user/personnalAccount/PersonnalAccountCard.vue";
-import FriendsCard from "~/components/molecules/friend/FriendsCard.vue";
-import CommentEditionCard from "~/components/organisms/user/data/CommentEditionCard.vue";
-import PlanningDownloadCard from "~/components/organisms/planning/PlanningDownloadCard.vue";
-import {
-  DOWNLOAD_PLANNING,
-  HAVE_PERSONNAL_ACCOUNT,
-} from "@overbookd/permission";
 
-export default {
+export default Vue.extend({
+  name: "Home",
   components: {
-    UserCard,
     SnackNotificationContainer,
-    PersonnalAccountCard,
-    FriendsCard,
-    CommentEditionCard,
-    PlanningDownloadCard,
   },
-
-  computed: {
-    me() {
-      return this.$accessor.user.me;
-    },
-    hasNotBeenApproved() {
-      // user is not or could not be loaded from the store
-      if (!this.me) {
-        return false; // not loaded yet
-      }
-      // user has no team
-      return this.me.team === undefined || this.me.team.length === 0;
-    },
-    canDownloadPlanning() {
-      return this.$accessor.user.can(DOWNLOAD_PLANNING);
-    },
-    havePersonnalAccount() {
-      return this.$accessor.user.can(HAVE_PERSONNAL_ACCOUNT);
-    },
-  },
-  async mounted() {
-    this.$accessor.user.fetchUser();
-  },
-  methods: {
-    async logout() {
-      await this.$auth.logout();
-      await this.$router.push({
-        path: "/login",
-      });
-    },
-    isAvailabilityUpdateActive() {
-      return true;
-    },
-    isManif() {
-      return true;
-    },
-  },
-};
+});
 </script>
+
 <style lang="scss" scoped>
-.cta {
-  color: white;
+.home {
+  margin: 10px 30px;
+  @media only screen and (max-width: $mobile-max-width) {
+    margin: 10px;
+  }
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  .search {
+    min-width: 80%;
+    max-width: 2000px;
+    @media only screen and (max-width: $mobile-max-width) {
+      min-width: 95%;
+    }
+  }
 }
 </style>
