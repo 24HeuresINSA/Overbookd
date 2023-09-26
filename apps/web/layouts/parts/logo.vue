02731bae940e11dac077d6e351582c6c7df0d3a2
Leo Mouyna <mouynaleo@gmail.com>
2023-09-26 13:47:10 +0200

    chore: anis easter eggs. #1248

---
+++ apps/web/layouts/parts/logo.vue
@@ -0,0 +1,54 @@
+<template>
+  <nuxt-link class="application" to="/">
+    <img class="logo" :src="`/img/logo/${logo}`" @click="increment" />
+    <span class="version"> {{ version }}</span>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+const version = process.env.OVERBOOKD_VERSION;
+
+export default Vue.extend({
+  name: "Logo",
+  data: () => ({
+    counter: 0,
+  }),
+  computed: {
+    version(): string {
+      return `v${version}` ?? "";
+    },
+    logo(): string {
+      if (this.counter >= 51) return "Pastis.png";
+      if (this.counter >= 24) return "Ricard.png";
+      return "overbookd_logo_noir.png";
+    },
+  },
+  methods: {
+    increment() {
+      this.counter = this.counter + 1;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.application {
+  display: flex;
+  gap: 3px;
+  align-items: center;
+
+  .logo {
+    max-width: 200px;
+    max-height: 51px;
+  }
+
+  .version {
+    font-weight: 600;
+    @media only screen and (max-width: $mobile-max-width) {
+      display: none;
+    }
+  }
+}
+</style>
