59a3a4b24a84a9f074a8ac55a806649da73b842f
DamienCarreau <carreau.damien@gmail.com>
2023-10-12 20:31:53 +0000

    style: :art: Make the location map editor works

--- apps/web/components/molecules/signa/location/LocationMap.vue
+++ apps/web/components/molecules/signa/location/LocationMap.vue
@@ -7,26 +7,24 @@
           :zoom="zoom"
           :center="center"
           :options="{ name: 'LocationMap' }"
-          @click="userAction"
-          @mousemove="userHover"
         >
           <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
           <l-marker
-            v-for="(latlng, index) in pointList"
+            v-for="(latlng, index) in points"
             :key="index"
             :lat-lng="latlng"
           >
             <l-tooltip>Hello!</l-tooltip>
           </l-marker>
           <l-polyline
-            v-for="(line, index) in lineList"
+            v-for="(line, index) in lines"
             :key="index"
             :lat-lngs="line"
           >
             <l-tooltip>Hello!</l-tooltip>
           </l-polyline>
           <l-polygon
-            v-for="(polygon, index) in polygonList"
+            v-for="(polygon, index) in polygons"
             :key="index"
             :lat-lngs="polygon"
           >
@@ -35,10 +33,6 @@
         </l-map>
       </client-only>
     </div>
-    <v-select v-model="action" :items="actions"></v-select>
-    <v-btn :disabled="!actionInProcess" @click="finishAction">
-      Finir l'edition
-    </v-btn>
   </section>
 </template>
 
@@ -53,78 +47,10 @@ export default defineComponent({
       '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     zoom: 16,
     center: [45.784045, 4.876916],
-    action: "Point",
-    actions: ["Point", "Ligne", "Zone"],
-    points: [[45.784045, 4.876916]] as number[][],
+    points: [] as number[][],
     lines: [] as number[][][],
     polygons: [] as number[][][],
-    actionInProcess: null as null | string,
-    mouseLatlng: null as null | number[],
   }),
-  methods: {
-    userHover({ latlng }: { latlng: number[] }) {
-      this.mouseLatlng = latlng;
-    },
-    userAction({ latlng }: { latlng: number[] }) {
-      switch (this.action) {
-        case "Point":
-          this.addMarker(latlng);
-          break;
-        case "Ligne":
-          this.addLinePoint(latlng);
-          break;
-        case "Zone":
-          this.addPolygonPoint(latlng);
-          break;
-      }
-    },
-    addMarker(latlng: number[]) {
-      this.points.push(latlng);
-    },
-    addLinePoint(latlng: number[]) {
-      if (this.actionInProcess) {
-        this.lines[this.lines.length - 1].push(latlng);
-      } else {
-        this.lines.push([latlng]);
-        this.actionInProcess = "Ligne";
-      }
-    },
-    addPolygonPoint(latlng: number[]) {
-      if (this.actionInProcess) {
-        this.polygons[this.polygons.length - 1].push(latlng);
-      } else {
-        this.polygons.push([latlng]);
-        this.actionInProcess = "Zone";
-      }
-    },
-    finishAction() {
-      this.actionInProcess = null;
-    },
-  },
-  computed: {
-    pointList(): number[][] {
-      if (this.action === "Point" && this.mouseLatlng) {
-        return [...this.points, this.mouseLatlng];
-      }
-      return this.points;
-    },
-    lineList(): number[][][] {
-      if (this.action === "Ligne" && this.actionInProcess && this.mouseLatlng) {
-        const lastLine = this.lines[this.lines.length - 1];
-        const otherLines = this.lines.slice(0, this.lines.length - 1);
-        return [...otherLines, [...lastLine, this.mouseLatlng]];
-      }
-      return this.lines;
-    },
-    polygonList(): number[][][] {
-      if (this.action === "Zone" && this.actionInProcess && this.mouseLatlng) {
-        const lastPolygon = this.polygons[this.polygons.length - 1];
-        const otherPolygons = this.polygons.slice(0, this.polygons.length - 1);
-        return [...otherPolygons, [...lastPolygon, this.mouseLatlng]];
-      }
-      return this.polygons;
-    },
-  },
 });
 </script>
 
--- apps/web/components/molecules/signa/location/LocationMapEditor.vue
+++ apps/web/components/molecules/signa/location/LocationMapEditor.vue
@@ -1,25 +1,26 @@
 <template>
   <section class="map-section">
-    <div class="map">
+    <v-lazy>
       <client-only>
-        <l-map
-          id="LocationMapEditor"
-          :zoom="zoom"
-          :center="center"
-          :options="{ name: 'LocationMapEditor' }"
-          @click="userAction"
-          @mousemove="userHover"
-        >
-          <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-          <l-marker v-if="point" :lat-lng="point"> </l-marker>
-          <l-polyline v-if="line" :lat-lngs="line"> </l-polyline>
-          <l-polygon v-if="polygon" :lat-lngs="polygon"> </l-polygon>
-        </l-map>
+        <div class="map">
+          <l-map
+            :zoom="zoom"
+            :center="center"
+            :options="{ name: 'LocationMapEditor' }"
+            @click="userAction"
+            @mousemove="userHover"
+          >
+            <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+            <l-marker v-if="point" :lat-lng="point"></l-marker>
+            <l-polyline v-if="line" :lat-lngs="line"></l-polyline>
+            <l-polygon v-if="polygon" :lat-lngs="polygon"></l-polygon>
+          </l-map>
+        </div>
       </client-only>
-    </div>
+    </v-lazy>
     <div class="editor-container">
       <div><v-select v-model="action" :items="actions"></v-select></div>
-      <v-btn :disabled="!actionInProcess" @click="finishAction">
+      <v-btn :disabled="editionDone" @click="finishAction">
         Finir l'edition
       </v-btn>
       <v-btn @click="reset"> Réinitialiser </v-btn>
@@ -29,6 +30,11 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
+type Selection = {
+  type: "POINT" | "ROAD" | "AREA";
+  coordinates: number[] | number[][];
+};
 
 export default defineComponent({
   name: "LocationMapEditor",
@@ -40,20 +46,47 @@ export default defineComponent({
     center: [45.784045, 4.876916],
     action: "Point",
     actions: ["Point", "Ligne", "Zone"],
-    point: null as null | number[],
-    line: null as null | number[][],
-    polygon: null as null | number[][],
-    actionInProcess: null as null | string,
+    editionDone: false,
     mouseLatlng: null as null | number[],
+    selection: null as null | Selection,
   }),
+  computed: {
+    point(): null | number[] {
+      if (this.selection && this.selection.type === "POINT") {
+        return this.selection.coordinates as number[];
+      }
+      return null;
+    },
+    line(): null | number[][] {
+      if (this.selection && this.selection.type === "ROAD") {
+        if (!this.editionDone && this.action === "Ligne" && this.mouseLatlng) {
+          const lastLine = this.selection.coordinates as number[][];
+          return [...lastLine, this.mouseLatlng];
+        }
+        return this.selection.coordinates as number[][];
+      }
+      return null;
+    },
+    polygon(): null | number[][] {
+      if (this.selection && this.selection.type === "AREA") {
+        if (!this.editionDone && this.action === "Zone" && this.mouseLatlng) {
+          const lastPolygon = this.selection.coordinates as number[][];
+          return [...lastPolygon, this.mouseLatlng];
+        }
+        return this.selection.coordinates as number[][];
+      }
+      return null;
+    },
+  },
   methods: {
     userHover({ latlng }: { latlng: number[] }) {
       this.mouseLatlng = latlng;
     },
     userAction({ latlng }: { latlng: number[] }) {
+      if (this.editionDone) return;
       switch (this.action) {
         case "Point":
-          this.addMarker(latlng);
+          this.setPoint(latlng);
           break;
         case "Ligne":
           this.addLinePoint(latlng);
@@ -63,34 +96,41 @@ export default defineComponent({
           break;
       }
     },
-    addMarker(latlng: number[]) {
+    setPoint(latlng: number[]) {
       if (this.line || this.polygon) return;
-      this.point = latlng;
+      this.selection = {
+        type: "POINT",
+        coordinates: latlng,
+      };
     },
     addLinePoint(latlng: number[]) {
       if (this.point || this.polygon) return;
-      if (this.line) {
-        this.line.push(latlng);
+      if (this.selection && this.selection.type === "ROAD") {
+        this.selection.coordinates = [...this.selection.coordinates, latlng] as number[][];
       } else {
-        this.line = [latlng];
+        this.selection = {
+          type: "ROAD",
+          coordinates: [latlng],
+        };
       }
     },
     addPolygonPoint(latlng: number[]) {
       if (this.point || this.line) return;
-      if (this.polygon) {
-        this.polygon.push(latlng);
+      if (this.selection && this.selection.type === "AREA") {
+        this.selection.coordinates = [...this.selection.coordinates, latlng] as number[][];
       } else {
-        this.polygon = [latlng];
+        this.selection = {
+          type: "AREA",
+          coordinates: [latlng],
+        };
       }
     },
     finishAction() {
-      this.actionInProcess = null;
+      this.editionDone = true;
     },
     reset() {
-      this.point = null;
-      this.line = null;
-      this.polygon = null;
-      this.actionInProcess = null;
+      this.selection = null;
+      this.editionDone = false;
     },
   },
 });
@@ -98,7 +138,7 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .map {
-  height: 40vh;
+  height: 60vh;
 }
 
 .editor-container {
