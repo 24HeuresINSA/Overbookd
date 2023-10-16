ad60bb7043264f336a2535124d9667e2704c11a8
Leon Dumestre <leon.dumestre@gmail.com>
2023-10-16 13:50:48 +0200

    fix: module poviders #1285

--- apps/api/src/festival-activity/festival-activity.module.ts
+++ apps/api/src/festival-activity/festival-activity.module.ts
@@ -1,9 +1,20 @@
 import { Module } from "@nestjs/common";
 import { FestivalActivityController } from "./festival-activity.controller";
 import { FestivalActivityService } from "./festival-activity.service";
+import { InMemoryFestivalActivityRepository } from "./repository/festival-activity-repository.inmemory";
 
 @Module({
   controllers: [FestivalActivityController],
-  providers: [FestivalActivityService],
+  providers: [
+    {
+      provide: InMemoryFestivalActivityRepository,
+      useFactory: () => new InMemoryFestivalActivityRepository(),
+    },
+    {
+      provide: FestivalActivityService,
+      useFactory: (festivalActivities: InMemoryFestivalActivityRepository) =>
+        new FestivalActivityService(festivalActivities),
+    },
+  ],
 })
 export class FestivalActivityModule {}
