964b838d5a9631fc125f0b9428814f52574bc83a
Leon Dumestre <leon.dumestre@gmail.com>
2023-10-23 11:15:37 +0200

    fix: caps in test #1299

--- libraries/period/src/period.spec.ts
+++ libraries/period/src/period.spec.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { INVALID_PERIOD_ERROR_MESSAGE, Period } from "./period";
 
-describe("Create a period", () => {
-  describe("When start is before end", () => {
+describe("create a period", () => {
+  describe("when start is before end", () => {
     it("should create a period", () => {
       const period = Period.init({
         start: new Date("2023-05-17 09:00"),
@@ -14,7 +14,7 @@ describe("Create a period", () => {
     });
   });
 
-  describe("When start is after end", () => {
+  describe("when start is after end", () => {
     it("should indicate that start must be before end", () => {
       expect(() =>
         Period.init({
