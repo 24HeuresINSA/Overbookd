5b3e70f7963788c06a3afae4e6a0b31fe19c7637
Leo Mouyna <mouynaleo@gmail.com>
2023-11-12 21:33:21 +0100

    chore(personal-account): inline error messages. #1324

--- domains/personal-account/src/barrel-prices/define-barrel-price.error.ts
+++ domains/personal-account/src/barrel-prices/define-barrel-price.error.ts
@@ -1,12 +1,10 @@
 export class SimilarBarrelExist extends Error {
   constructor(slug: string) {
-    const message = `Un fût de ${slug} est déjà configuré.`;
-    super(message);
+    super(`Un fût de ${slug} est déjà configuré.`);
   }
 }
 export class BarrelNotConfigured extends Error {
   constructor(slug: string) {
-    const message = `Aucun fût de ${slug} n'est encore configuré`;
-    super(message);
+    super(`Aucun fût de ${slug} n'est encore configuré`);
   }
 }
