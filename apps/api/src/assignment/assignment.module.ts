20cf2c2e7b61a4ff0dce6123ba36d6a422b9723d
Leo Mouyna <mouynaleo@gmail.com>
2023-11-21 17:44:23 +0100

    chore(api): clean some module init. #1340

--- apps/api/src/assignment/assignment.module.ts
+++ apps/api/src/assignment/assignment.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { UserService } from "../user/user.service";
 import { VolunteerAvailabilityService } from "../volunteer-availability/volunteer-availability.service";
 import { PrismaService } from "../prisma.service";
 import { AssignmentController } from "./assignment.controller";
@@ -7,6 +6,7 @@ import { AssignmentService } from "./assignment.service";
 import { FtTimeSpanService } from "./ft-time-span.service";
 import { VolunteerService } from "./volunteer.service";
 import { MailModule } from "../mail/mail.module";
+import { UserModule } from "../user/user.module";
 
 @Module({
   controllers: [AssignmentController],
@@ -16,8 +16,7 @@ import { MailModule } from "../mail/mail.module";
     FtTimeSpanService,
     PrismaService,
     VolunteerAvailabilityService,
-    UserService,
   ],
-  imports: [MailModule],
+  imports: [MailModule, UserModule],
 })
 export class AssignmentModule {}
--- apps/api/src/authentication/authentication.module.ts
+++ apps/api/src/authentication/authentication.module.ts
@@ -1,21 +1,21 @@
-import { forwardRef, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { HashingUtilsModule } from "../hashing-utils/hashing-utils.module";
 import { HashingUtilsService } from "../hashing-utils/hashing-utils.service";
-import { PrismaService } from "../prisma.service";
 import { UserModule } from "../user/user.module";
-import { UserService } from "../user/user.service";
 import { AuthenticationController } from "./authentication.controller";
 import { AuthenticationService } from "./authentication.service";
 import { jwtConstants } from "./constants";
 import { JwtStrategy } from "./jwt.strategy";
 import { MailModule } from "../mail/mail.module";
+import { PrismaModule } from "../prisma.module";
 
 @Module({
   imports: [
-    forwardRef(() => UserModule),
+    UserModule,
     PassportModule,
+    PrismaModule,
     MailModule,
     JwtModule.register({
       secret: jwtConstants.secret,
@@ -25,13 +25,7 @@ import { MailModule } from "../mail/mail.module";
     }),
     HashingUtilsModule,
   ],
-  providers: [
-    AuthenticationService,
-    JwtStrategy,
-    HashingUtilsService,
-    UserService,
-    PrismaService,
-  ],
+  providers: [AuthenticationService, JwtStrategy, HashingUtilsService],
   exports: [AuthenticationService],
   controllers: [AuthenticationController],
 })
