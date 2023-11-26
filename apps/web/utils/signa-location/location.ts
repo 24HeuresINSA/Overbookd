3380ac75767fc24560c5870943e6839eb9845a72
DamienCarreau <carreau.damien@gmail.com>
2023-11-26 15:19:16 +0000

    refactor: :art: Beautify code

--- apps/web/utils/signa-location/location.ts
+++ apps/web/utils/signa-location/location.ts
@@ -2,5 +2,6 @@ import { Coordinate, GeoJson } from "@overbookd/signa";
 
 export type Location = {
   addCoordinate(coordinate: Coordinate): void;
+  isNear(coordinate: Coordinate, radius: number): boolean;
   geoJson: GeoJson;
 };
