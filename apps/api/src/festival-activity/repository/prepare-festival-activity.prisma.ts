dceecd642619f69ee483e5966ccd9cc93fb521ed
Leon Dumestre <leon.dumestre@gmail.com>
2023-11-27 20:28:03 +0100

    fix(festival-activity): reviews format #1345

--- apps/api/src/festival-activity/repository/prepare-festival-activity.prisma.ts
+++ apps/api/src/festival-activity/repository/prepare-festival-activity.prisma.ts
@@ -2,8 +2,18 @@ import {
   FestivalActivity,
   InReview,
   InquiryRequest,
+  NOT_ASKING_TO_REVIEW,
   PrepareFestivalActivityRepository,
   PreviewFestivalActivity,
+  ReviewStatus,
+  Reviewer,
+  barrieres,
+  comcom,
+  elec,
+  humain,
+  matos,
+  secu,
+  signa,
 } from "@overbookd/festival-activity";
 import { PrismaService } from "../../prisma.service";
 import {
@@ -11,16 +21,22 @@ import {
   SELECT_PREVIEW_FESTIVAL_ACTIVITY,
 } from "../festival-activity.query";
 
-type DatabasePreview = Omit<PreviewFestivalActivity, "team"> & {
-  teamCode: FestivalActivity["inCharge"]["team"];
+type DatabaseReview = {
+  team: Reviewer;
+  status: ReviewStatus;
 };
 
-type DatabaseGeneral = Omit<
-  FestivalActivity["general"],
-  "team" | "timeWindows"
-> & {
+type DatabasePreview = Omit<PreviewFestivalActivity, "team" | "reviews"> & {
   teamCode: FestivalActivity["inCharge"]["team"];
+  reviews: DatabaseReview[];
+};
+
+type DatabaseGeneral = Omit<FestivalActivity["general"], "timeWindows"> & {
   generalTimeWindows: FestivalActivity["general"]["timeWindows"];
+};
+
+type DatabaseInCharge = Omit<FestivalActivity["inCharge"], "team"> & {
+  teamCode: FestivalActivity["inCharge"]["team"];
 };
 
 type DatabaseInquiryRequest = {
@@ -38,15 +54,14 @@ type DatabaseInquiry = {
 };
 
 type DatabaseFestivalActivity = DatabaseGeneral &
-  FestivalActivity["inCharge"] &
-  FestivalActivity["inquiry"] &
+  DatabaseInCharge &
   FestivalActivity["signa"] &
   FestivalActivity["security"] &
   FestivalActivity["supply"] &
   DatabaseInquiry & {
     id: FestivalActivity["id"];
     status: FestivalActivity["status"];
-    reviews: InReview["reviews"];
+    reviews: DatabaseReview[];
   };
 
 export class PrismaPrepareFestivalActivityRepository
@@ -61,7 +76,7 @@ export class PrismaPrepareFestivalActivityRepository
     return activities.map(this.formatPreviewFestivalActivity);
   }
 
-  async findById(id: number): Promise<FestivalActivity> {
+  async findById(id: FestivalActivity["id"]): Promise<FestivalActivity> {
     const activity = await this.prisma.festivalActivity.findUnique({
       where: { id },
       select: SELECT_FESTIVAL_ACTIVITY,
@@ -83,7 +98,7 @@ export class PrismaPrepareFestivalActivityRepository
       name: activity.name,
       team: activity.teamCode,
       adherent: activity.adherent,
-      reviews: activity.reviews,
+      reviews: this.formatReviews(activity.reviews),
     };
   }
 
@@ -93,7 +108,7 @@ export class PrismaPrepareFestivalActivityRepository
     return {
       id: activity.id,
       status: activity.status,
-      reviews: activity.reviews,
+      reviews: this.formatReviews(activity.reviews),
       general: {
         name: activity.name,
         description: activity.description,
@@ -123,19 +138,42 @@ export class PrismaPrepareFestivalActivityRepository
     } as FestivalActivity;
   }
 
+  private formatReviews(
+    reviews: DatabaseReview[],
+  ): InReview["reviews"] | undefined {
+    if (reviews.length === 0) return undefined;
+    return {
+      humain: this.findReviewStatusByTeam(reviews, humain),
+      signa: this.findReviewStatusByTeam(reviews, signa),
+      barrieres: this.findReviewStatusByTeam(reviews, barrieres),
+      comcom: this.findReviewStatusByTeam(reviews, comcom),
+      elec: this.findReviewStatusByTeam(reviews, elec),
+      matos: this.findReviewStatusByTeam(reviews, matos),
+      secu: this.findReviewStatusByTeam(reviews, secu),
+    };
+  }
+
+  private findReviewStatusByTeam(
+    reviews: DatabaseReview[],
+    team: Reviewer,
+  ): ReviewStatus {
+    const review = reviews.find((review) => review.team === team);
+    return review?.status ?? NOT_ASKING_TO_REVIEW;
+  }
+
   private formatInquirySection(
     inquiry: DatabaseInquiry,
   ): FestivalActivity["inquiry"] {
     const gears = inquiry.inquiries
-      .filter((req) => req.catalogItem.category.owner.code === "matos")
+      .filter((req) => req.catalogItem.category.owner.code === matos)
       .map(this.formatInquiryRequest);
 
     const barriers = inquiry.inquiries
-      .filter((req) => req.catalogItem.category.owner.code === "barrieres")
+      .filter((req) => req.catalogItem.category.owner.code === barrieres)
       .map(this.formatInquiryRequest);
 
     const electricity = inquiry.inquiries
-      .filter((req) => req.catalogItem.category.owner.code === "elec")
+      .filter((req) => req.catalogItem.category.owner.code === elec)
       .map(this.formatInquiryRequest);
 
     return {
