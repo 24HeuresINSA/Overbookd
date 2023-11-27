4c0c470d1e5be076aa321db30a5efadf3a553e1e
Leon Dumestre <leon.dumestre@gmail.com>
2023-11-27 12:57:16 +0100

    prefeat(festival-activity): implement find & findALl in api #1345

---
+++ apps/api/src/festival-activity/repository/prepare-festival-activity.prisma.ts
@@ -0,0 +1,140 @@
+import {
+  FestivalActivity,
+  InReview,
+  InquiryRequest,
+  PrepareFestivalActivityRepository,
+  PreviewFestivalActivity,
+} from "@overbookd/festival-activity";
+import { PrismaService } from "../../prisma.service";
+import {
+  SELECT_FESTIVAL_ACTIVITY,
+  SELECT_PREVIEW_FESTIVAL_ACTIVITY,
+} from "../festival-activity.query";
+
+type DatabaseGeneral = Omit<
+  FestivalActivity["general"],
+  "team" | "timeWindows"
+> & {
+  teamCode: FestivalActivity["inCharge"]["team"];
+  generalTimeWindows: FestivalActivity["general"]["timeWindows"];
+};
+
+type DatabaseInquiryRequest = {
+  slug: InquiryRequest["slug"];
+  quantity: InquiryRequest["quantity"];
+  catalogItem: {
+    name: InquiryRequest["name"];
+    category: { owner: { code: string } };
+  };
+};
+
+type DatabaseInquiry = {
+  inquiryTimeWindows: FestivalActivity["inquiry"]["timeWindows"];
+  inquiries: DatabaseInquiryRequest[];
+};
+
+type DatabaseFestivalActivity = DatabaseGeneral &
+  FestivalActivity["inCharge"] &
+  FestivalActivity["inquiry"] &
+  FestivalActivity["signa"] &
+  FestivalActivity["security"] &
+  FestivalActivity["supply"] &
+  DatabaseInquiry & {
+    id: FestivalActivity["id"];
+    status: FestivalActivity["status"];
+    reviews: InReview["reviews"];
+  };
+
+export class PrismaPrepareFestivalActivityRepository
+  implements PrepareFestivalActivityRepository
+{
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<PreviewFestivalActivity[]> {
+    return this.prisma.festivalActivity.findMany({
+      select: SELECT_PREVIEW_FESTIVAL_ACTIVITY,
+    });
+  }
+
+  async findById(id: number): Promise<FestivalActivity> {
+    const activity = await this.prisma.festivalActivity.findUnique({
+      where: { id },
+      select: SELECT_FESTIVAL_ACTIVITY,
+    });
+    return this.formatFestivalActivity(activity);
+  }
+
+  async save(activity: FestivalActivity): Promise<FestivalActivity> {
+    // TODO: save activity in database
+    return activity;
+  }
+
+  private formatFestivalActivity(
+    activity: DatabaseFestivalActivity,
+  ): FestivalActivity {
+    return {
+      id: activity.id,
+      status: activity.status,
+      reviews: activity.reviews,
+      general: {
+        name: activity.name,
+        description: activity.description,
+        categories: activity.categories,
+        toPublish: activity.toPublish,
+        isFlagship: activity.isFlagship,
+        photoLink: activity.photoLink,
+        timeWindows: activity.generalTimeWindows,
+      },
+      inCharge: {
+        team: activity.teamCode,
+        adherent: activity.adherent,
+        contractors: activity.contractors,
+      },
+      signa: {
+        location: activity.location,
+        signages: activity.signages,
+      },
+      security: {
+        specialNeed: activity.specialNeed,
+      },
+      supply: {
+        electricity: activity.electricity,
+        water: activity.water,
+      },
+      inquiry: this.formatInquirySection(activity),
+    } as FestivalActivity;
+  }
+
+  private formatInquirySection(
+    inquiry: DatabaseInquiry,
+  ): FestivalActivity["inquiry"] {
+    const gears = inquiry.inquiries
+      .filter((req) => req.catalogItem.category.owner.code === "matos")
+      .map(this.formatInquiryRequest);
+
+    const barriers = inquiry.inquiries
+      .filter((req) => req.catalogItem.category.owner.code === "barrieres")
+      .map(this.formatInquiryRequest);
+
+    const electricity = inquiry.inquiries
+      .filter((req) => req.catalogItem.category.owner.code === "elec")
+      .map(this.formatInquiryRequest);
+
+    return {
+      timeWindows: inquiry.inquiryTimeWindows,
+      gears,
+      barriers,
+      electricity,
+    };
+  }
+
+  private formatInquiryRequest(
+    request: DatabaseInquiryRequest,
+  ): InquiryRequest {
+    return {
+      slug: request.slug,
+      name: request.catalogItem.name,
+      quantity: request.quantity,
+    };
+  }
+}
