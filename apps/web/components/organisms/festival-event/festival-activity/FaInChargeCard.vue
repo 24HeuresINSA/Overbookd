89bb15019842f62d81b757385572a9fc66f1ab39
Leon Dumestre <leon.dumestre@gmail.com>
2023-11-29 00:34:39 +0100

    refactor(festival-activity): add in charge section #1352

---
+++ apps/web/components/organisms/festival-event/festival-activity/FaInChargeCard.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-card>
+    <v-card-title>Responsable</v-card-title>
+
+    <v-card-subtitle>
+      N'hésite pas si tu as des questions à contacter
+      <a href="mailto:humain@24heures.org">humain@24heures.org</a>. Tu peux
+      aussi t'aider en allant voir les FA d'avant sur
+      <a href="https://cetaitmieuxavant.24heures.org">cetaitmieuxavant</a> en te
+      connectant avec jeuneetcon@24heures.org.
+    </v-card-subtitle>
+
+    <v-card-text>
+      <SearchTeam
+        :team="team"
+        label="Équipe"
+        :boxed="false"
+        @change="updateTeam($event)"
+      />
+
+      <SearchUser
+        :user="inCharge.adherent"
+        label="Adherent"
+        :boxed="false"
+        :list="volunteers"
+        @change="updateAdherent($event)"
+      />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SearchUser from "~/components/atoms/field/search/SearchUser.vue";
+import SearchTeam from "~/components/atoms/field/search/SearchTeam.vue";
+import { FestivalActivity } from "@overbookd/festival-activity";
+import { User, UserPersonalData } from "@overbookd/user";
+import { Team } from "~/utils/models/team.model";
+
+export default defineComponent({
+  name: "FaGeneralCard",
+  components: { SearchUser, SearchTeam },
+  computed: {
+    mFA(): FestivalActivity {
+      return this.$accessor.festivalActivity.selectedActivity;
+    },
+    inCharge(): FestivalActivity["inCharge"] {
+      return this.mFA.inCharge;
+    },
+    team(): Team | null {
+      return this.inCharge.team
+        ? this.$accessor.team.getTeamByCode(this.inCharge.team)
+        : null;
+    },
+    volunteers(): UserPersonalData[] {
+      return this.$accessor.user.volunteers;
+    },
+  },
+  async mounted() {
+    if (this.volunteers.length === 0) {
+      await this.$accessor.user.fetchVolunteers();
+    }
+  },
+  methods: {
+    updateTeam(team: string) {
+      console.log("update team", team);
+      // TODO: update team
+    },
+    updateAdherent(adherent: User) {
+      console.log("update adherent", adherent);
+      // TODO: update name
+    },
+  },
+});
+</script>
