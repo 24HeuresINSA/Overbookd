8bf688a86347467446e046ae2bccf6eb0ddd405f
Léon Dumestre <leon.dumestre@gmail.com>
2023-12-07 20:33:38 +0000

    fix(festival-activity): lock signa approve

--- apps/web/components/organisms/festival-event/festival-activity/SignaCard.vue
+++ apps/web/components/organisms/festival-event/festival-activity/SignaCard.vue
@@ -59,7 +59,6 @@ import {
   Location,
   PrepareSignageCreation,
   isDraft,
-  APPROVED,
   signa,
   REJECTED,
 } from "@overbookd/festival-activity";
@@ -83,8 +82,8 @@ export default defineComponent({
     },
     cantApprove(): boolean {
       if (isDraft(this.mFA)) return true;
-
-      return this.mFA.reviews.signa === APPROVED;
+      return true;
+      //return this.mFA.reviews.signa === APPROVED;
     },
     cantReject(): boolean {
       if (isDraft(this.mFA)) return true;
