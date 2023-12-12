177934a6021e7c742a3ae0bd0b8c142b65f99897
TristanDevin <tristan.devin@insa-lyon.fr>
2023-12-12 16:07:46 +0100

    fix(web): :bug: fix path url

--- apps/web/utils/image/image.repository.ts
+++ apps/web/utils/image/image.repository.ts
@@ -9,7 +9,7 @@ export class ImageRepository {
   ): Promise<string | undefined> {
     const token = context.$axios.defaults.headers.common["Authorization"];
     if (!token) return undefined;
-    const response = await fetch(`${process.env.BASE_URL}/${path}`, {
+    const response = await fetch(new URL(path, process.env.BASE_URL), {
       method: "GET",
       headers: {
         Authorization: `${token}`,
