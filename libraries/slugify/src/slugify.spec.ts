5fe0cbc9a1e03283720c7aafeb50ed3ee4b8f1cc
Léo Mouyna <mouynaleo@gmail.com>
2023-12-12 14:58:02 +0100

    test(slugify): add test case. #1410

--- libraries/slugify/src/slugify.spec.ts
+++ libraries/slugify/src/slugify.spec.ts
@@ -3,14 +3,15 @@ import { SlugifyService } from "./slugify";
 
 describe("slugify service", () => {
   describe.each`
-    sentence                         | expected
-    ${"Bonjour"}                     | ${"bonjour"}
-    ${"Léo Mouyna"}                  | ${"leo-mouyna"}
-    ${"Ça m'énerve"}                 | ${"ca-m-enerve"}
-    ${"Cœur"}                        | ${"coeur"}
-    ${"Œil"}                         | ${"oeil"}
-    ${"vis Ø12mm"}                   | ${"vis-o12mm"}
-    ${"VIP / PRESSE / ORGANISATEUR"} | ${"vip-presse-organisateur"}
+    sentence                        | expected
+    ${"Bonjour"}                    | ${"bonjour"}
+    ${"Léo Mouyna"}                 | ${"leo-mouyna"}
+    ${"Ça m'énerve"}                | ${"ca-m-enerve"}
+    ${"Cœur"}                       | ${"coeur"}
+    ${"Œil"}                        | ${"oeil"}
+    ${"vis Ø12mm"}                  | ${"vis-o12mm"}
+    ${"VIP/PRESSE/ORGANISATEUR"}    | ${"vip-presse-organisateur"}
+    ${"PASS 1 SOIR / PASS 2 SOIRS"} | ${"pass-1-soir-pass-2-soirs"}
   `("when slugify $sentence", ({ sentence, expected }) => {
     it(`should return ${expected}`, () => {
       expect(SlugifyService.apply(sentence)).toBe(expected);
