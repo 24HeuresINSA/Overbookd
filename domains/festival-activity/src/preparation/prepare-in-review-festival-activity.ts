1c48e6fa32154239a5fd61206c8a2e4d222f5bff
Leon Dumestre <leon.dumestre@gmail.com>
2023-12-19 00:09:31 +0100

    fix: function order #1408

--- domains/festival-activity/src/preparation/prepare-in-review-festival-activity.ts
+++ domains/festival-activity/src/preparation/prepare-in-review-festival-activity.ts
@@ -353,6 +353,15 @@ export class PrepareInReviewFestivalActivity implements Prepare<Reviewable> {
     return { ...this.activity, signa };
   }
 
+  linkSignageToCatalogItem(link: LinkSignageCatalogItem): Reviewable {
+    const signages = Signages.build(
+      this.activity.signa.signages,
+    ).linkCatalogItem(link).entries;
+
+    const signa = { ...this.activity.signa, signages };
+    return { ...this.activity, signa };
+  }
+
   updateSecurity(form: PrepareSecurityUpdate): Reviewable {
     this.checkIfSecurityAlreadyApproved();
     const specialNeed =
@@ -365,15 +374,6 @@ export class PrepareInReviewFestivalActivity implements Prepare<Reviewable> {
 
     const security = { ...this.activity.inCharge, specialNeed, freePass };
     return { ...this.activity, security };
-  }
-
-  linkSignageToCatalogItem(link: LinkSignageCatalogItem): Reviewable {
-    const signages = Signages.build(
-      this.activity.signa.signages,
-    ).linkCatalogItem(link).entries;
-
-    const signa = { ...this.activity.signa, signages };
-    return { ...this.activity, signa };
   }
 
   private checkIfSecurityAlreadyApproved() {
