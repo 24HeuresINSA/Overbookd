99d77cc2b331dc4ceda7664854fb4bb627c780e6
LucVDH <luke.vandh@gmail.com>
2024-01-02 16:09:34 +0100

    prefeat(recap-matos): create expansion panels, fetch gear and display the name of the gear #1421

--- apps/web/pages/matos/gear-recap.vue
+++ apps/web/pages/matos/gear-recap.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
     <h1>Récap Matos</h1>
+    <v-expansion-panels>
+      <v-expansion-panel v-for="gear in gears" :key="gear.slug">
+        <v-expansion-panel-header>
+          {{ gear.name }}
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Contenu à définir
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { defineComponent } from "vue";
+import { Gear } from "~/utils/models/catalog.model";
 
-export default Vue.extend({
+export default defineComponent({
   name: "GearRecap",
-  components: {},
   head: () => ({
     title: "Récap Matos",
   }),
+  computed: {
+    gears(): Gear[] {
+      return this.$accessor.catalogGear.gears;
+    },
+  },
+  mounted() {
+    this.$accessor.catalogGear.fetchGears({});
+  },
 });
 </script>
