42c17d75acbd9daf0ada28172f80470af71ac1b7
Leon Dumestre <leon.dumestre@gmail.com>
2024-01-03 17:38:37 +0100

    fix(summary-gear): delta type in gear preview #1439

--- apps/api/src/summary-gear/repository/summary-gear-repository.prisma.ts
+++ apps/api/src/summary-gear/repository/summary-gear-repository.prisma.ts
@@ -1,11 +1,23 @@
 import { SummaryGearPreview } from "@overbookd/http";
 import { PrismaService } from "../../prisma.service";
 import { SummaryGears } from "../summary-gear.service";
+import { SELECT_GEAR_PREVIEW } from "./summary-gear.query";
+
+type DatabaseGearPreview = Omit<SummaryGearPreview, "delta">;
 
 export class PrismaSummaryGears implements SummaryGears {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll(): Promise<SummaryGearPreview[]> {
-    return this.prisma.catalogGear.findMany();
+    const gears = await this.prisma.catalogGear.findMany({
+      select: SELECT_GEAR_PREVIEW,
+    });
+    return gears.map(this.formatGearPreview);
+  }
+
+  private async formatGearPreview(
+    gear: DatabaseGearPreview,
+  ): Promise<SummaryGearPreview> {
+    return { ...gear, delta: 0 }; // TODO: implement delta
   }
 }
