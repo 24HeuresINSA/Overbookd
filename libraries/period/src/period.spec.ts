73cfe6dc1276c7ec5a862e18938339db298d1afd
Leon Dumestre <leon.dumestre@gmail.com>
2024-01-04 15:03:46 +0100

    refactor(period): clean tests #1439

--- libraries/period/src/period.spec.ts
+++ libraries/period/src/period.spec.ts
@@ -2,10 +2,26 @@ import { describe, expect, it } from "vitest";
 import {
   END_BEFORE_START_ERROR_MESSAGE,
   EndBeforeStart,
-  IProvidePeriod,
   Period,
 } from "./period";
 import { ONE_MINUTE_IN_MS, ONE_SECOND_IN_MS } from "./duration.constant";
+import {
+  friday08hto09h,
+  friday08hto08h15,
+  friday08h15to08h30,
+  friday08h30to08h45,
+  friday08h45To09h,
+  friday23h59m50toSaturday00h00m02,
+  friday23h59m50to23h59m55,
+  friday23h59m55toSaturday00h,
+  saturday00hTo00h00m02,
+  friday09h30to10h,
+  friday09htoSaturday01h,
+  friday08hto08h30,
+  friday07hto08h30,
+  friday08h45to09h30,
+  friday08h45to10h,
+} from "./period.test-utils";
 
 describe("Create a period", () => {
   describe("when start is before end", () => {
@@ -69,14 +85,14 @@ describe("Check period validity", () => {
 
 describe("Split period with interval", () => {
   describe.each`
-    readablePeriod                                  | period                                                                                          | intervalInMs             | readableInterval | expectedPeriods
-    ${"2023-05-17 08:00 to 09:00"}                  | ${{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T09:00+02:00") }}       | ${15 * ONE_MINUTE_IN_MS} | ${"15min"}       | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:15+02:00") }, { start: new Date("2023-05-17T08:15+02:00"), end: new Date("2023-05-17T08:30+02:00") }, { start: new Date("2023-05-17T08:30+02:00"), end: new Date("2023-05-17T08:45+02:00") }, { start: new Date("2023-05-17T08:45+02:00"), end: new Date("2023-05-17T09:00+02:00") }]}
-    ${"2024-01-01 23:59:50 to 2024-01-02 00:00:02"} | ${{ start: new Date("2024-01-01T23:59:50+02:00"), end: new Date("2024-01-02T00:00:02+02:00") }} | ${5 * ONE_SECOND_IN_MS}  | ${"5sec"}        | ${[{ start: new Date("2024-01-01T23:59:50+02:00"), end: new Date("2024-01-01T23:59:55+02:00") }, { start: new Date("2024-01-01T23:59:55+02:00"), end: new Date("2024-01-02T00:00:00+02:00") }, { start: new Date("2024-01-02T00:00:00+02:00"), end: new Date("2024-01-02T00:00:02+02:00") }]}
+    readablePeriod                            | period                              | intervalInMs             | readableInterval | expectedPeriods
+    ${"friday 08:00 to 09:00"}                | ${friday08hto09h}                   | ${15 * ONE_MINUTE_IN_MS} | ${"15min"}       | ${[friday08hto08h15, friday08h15to08h30, friday08h30to08h45, friday08h45To09h]}
+    ${"friday 23:59:50 to saturday 00:00:02"} | ${friday23h59m50toSaturday00h00m02} | ${5 * ONE_SECOND_IN_MS}  | ${"5sec"}        | ${[friday23h59m50to23h59m55, friday23h59m55toSaturday00h, saturday00hTo00h00m02]}
   `(
     "when period start is $readablePeriod",
     ({ period, intervalInMs, readableInterval, expectedPeriods }) => {
       it(`should split period with a ${readableInterval} interval`, () => {
-        const periods = Period.init(period).splitWithIntervalInMs(intervalInMs);
+        const periods = period.splitWithIntervalInMs(intervalInMs);
         expect(periods).toEqual(expectedPeriods);
       });
     },
@@ -85,15 +101,12 @@ describe("Split period with interval", () => {
 
 describe("Sort periods", () => {
   describe.each`
-    readablePeriods                                                         | periods                                                                                                                                                                             | expectedPeriods
-    ${"2023-05-17 09:00 to 09:15 and 2023-05-17 08:15 to 08:30"}            | ${[{ start: new Date("2023-05-17T09:00+02:00"), end: new Date("2023-05-17T09:15+02:00") }, { start: new Date("2023-05-17T08:15+02:00"), end: new Date("2023-05-17T08:30+02:00") }]} | ${[{ start: new Date("2023-05-17T08:15+02:00"), end: new Date("2023-05-17T08:30+02:00") }, { start: new Date("2023-05-17T09:00+02:00"), end: new Date("2023-05-17T09:15+02:00") }]}
-    ${"2023-05-17 09:00 to 12:00 and 2023-05-17 09:00 to 2023-05-18 01:00"} | ${[{ start: new Date("2023-05-17T09:00+02:00"), end: new Date("2023-05-17T12:00+02:00") }, { start: new Date("2023-05-17T09:00+02:00"), end: new Date("2023-05-18T01:00+02:00") }]} | ${[{ start: new Date("2023-05-17T09:00+02:00"), end: new Date("2023-05-18T01:00+02:00") }, { start: new Date("2023-05-17T09:00+02:00"), end: new Date("2023-05-17T12:00+02:00") }]}
+    readablePeriods                                        | periods                                       | expectedPeriods
+    ${"friday 09:30 to 10:00 and 08:15 to 08:30"}          | ${[friday09h30to10h, friday08h15to08h30]}     | ${[friday08h15to08h30, friday09h30to10h]}
+    ${"friday 09:00 to 10:00 and 09:00 to saturday 01:00"} | ${[friday09h30to10h, friday09htoSaturday01h]} | ${[friday09htoSaturday01h, friday09h30to10h]}
   `("when periods are $readablePeriods", ({ periods, expectedPeriods }) => {
     it("should sort periods", () => {
-      const initPeriods = periods.map((period: IProvidePeriod) =>
-        Period.init(period),
-      );
-      const sortedPeriods = Period.sort(initPeriods);
+      const sortedPeriods = Period.sort(periods);
       console.log(sortedPeriods, expectedPeriods);
       expect(sortedPeriods).toEqual(expectedPeriods);
     });
@@ -102,21 +115,18 @@ describe("Sort periods", () => {
 
 describe("Merge contiguous periods", () => {
   describe.each`
-    readablePeriods                                                                            | periods                                                                                                                                                                                                                                                                     | expectedPeriods                                                                                                                                                                     | readableExpectedPeriods
-    ${"2023-05-17 08:00 to 08:15"}                                                             | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:15+02:00") }]}                                                                                                                                                                                 | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:15+02:00") }]}                                                                                         | ${"2023-05-17 08:00 to 08:15"}
-    ${"2023-05-17 08:00 to 08:15 and 2023-05-17 08:15 to 08:30"}                               | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:15+02:00") }, { start: new Date("2023-05-17T08:15+02:00"), end: new Date("2023-05-17T08:30+02:00") }]}                                                                                         | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:30+02:00") }]}                                                                                         | ${"2023-05-17 08:00 to 08:30"}
-    ${"2023-05-17 08:00 to 08:15 and 2023-05-17 08:00 to 08:30"}                               | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:15+02:00") }, { start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:30+02:00") }]}                                                                                         | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:30+02:00") }]}                                                                                         | ${"2023-05-17 08:00 to 08:30"}
-    ${"2023-05-17 08:00 to 08:15 and 2023-05-17 08:30 to 08:45"}                               | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:15+02:00") }, { start: new Date("2023-05-17T08:30+02:00"), end: new Date("2023-05-17T08:45+02:00") }]}                                                                                         | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:15+02:00") }, { start: new Date("2023-05-17T08:30+02:00"), end: new Date("2023-05-17T08:45+02:00") }]} | ${"2023-05-17 08:00 to 08:15 and 2023-05-17 08:30 to 08:45"}
-    ${"2023-05-17 08:00 to 08:15 and 2023-05-17 07:00 to 08:30"}                               | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:15+02:00") }, { start: new Date("2023-05-17T07:00+02:00"), end: new Date("2023-05-17T08:30+02:00") }]}                                                                                         | ${[{ start: new Date("2023-05-17T07:00+02:00"), end: new Date("2023-05-17T08:30+02:00") }]}                                                                                         | ${"2023-05-17 07:00 to 08:30"}
-    ${"2023-05-17 08:00 to 08:30 and 2023-05-17 08:45 to 09:30 and 2023-05-17 09:30 to 10:00"} | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:30+02:00") }, { start: new Date("2023-05-17T08:45+02:00"), end: new Date("2023-05-17T09:30+02:00") }, { start: new Date("2023-05-17T09:30+02:00"), end: new Date("2023-05-17T10:00+02:00") }]} | ${[{ start: new Date("2023-05-17T08:00+02:00"), end: new Date("2023-05-17T08:30+02:00") }, { start: new Date("2023-05-17T08:45+02:00"), end: new Date("2023-05-17T10:00+02:00") }]} | ${"2023-05-17 08:00 to 08:30 and 2023-05-17 08:45 to 10:00"}
+    readablePeriods                                                  | periods                                                     | expectedPeriods                           | readableExpectedPeriods
+    ${"friday 08:00 to 08:15"}                                       | ${[friday08hto08h15]}                                       | ${[friday08hto08h15]}                     | ${"friday 08:00 to 08:15"}
+    ${"friday 08:00 to 08:15 and 08:15 to 08:30"}                    | ${[friday08hto08h15, friday08h15to08h30]}                   | ${[friday08hto08h30]}                     | ${"friday 08:00 to 08:30"}
+    ${"friday 08:00 to 08:15 and 08:00 to 08:30"}                    | ${[friday08hto08h15, friday08hto08h30]}                     | ${[friday08hto08h30]}                     | ${"friday 08:00 to 08:30"}
+    ${"friday 08:00 to 08:15 and 08:30 to 08:45"}                    | ${[friday08hto08h15, friday08h30to08h45]}                   | ${[friday08hto08h15, friday08h30to08h45]} | ${"friday 08:00 to 08:15 and 08:30 to 08:45"}
+    ${"friday 08:00 to 08:15 and 07:00 to 08:30"}                    | ${[friday08hto08h15, friday07hto08h30]}                     | ${[friday07hto08h30]}                     | ${"friday 07:00 to 08:30"}
+    ${"friday 08:00 to 08:30 and 08:45 to 09:30 and 09:30 to 10:00"} | ${[friday08hto08h30, friday08h45to09h30, friday09h30to10h]} | ${[friday08hto08h30, friday08h45to10h]}   | ${"friday 08:00 to 08:30 and 08:45 to 10:00"}
   `(
     "when periods are $readablePeriods",
     ({ periods, expectedPeriods, readableExpectedPeriods }) => {
       it(`should merge contiguous periods to ${readableExpectedPeriods}`, () => {
-        const initPeriods = periods.map((period: IProvidePeriod) =>
-          Period.init(period),
-        );
-        const mergedPeriods = Period.mergeContiguous(initPeriods);
+        const mergedPeriods = Period.mergeContiguous(periods);
         expect(mergedPeriods).toEqual(expectedPeriods);
       });
     },
