ab2bb16c7c620b0aca46df7adb18f16154528353
Leo Mouyna <mouynaleo@gmail.com>
2024-01-20 10:52:08 +0100

    fix(trombinoscope): enable on mobile. #1481

--- apps/web/utils/pages/pages-list.ts
+++ apps/web/utils/pages/pages-list.ts
@@ -91,7 +91,7 @@ export const pages: Page[] = [
     to: "/trombinoscope",
     description:
       "Liste de tous les bénévoles avec leur photo ce qui permet de mettre un visage sur un nom avant de se rencontrer",
-    mobileSupport: false,
+    mobileSupport: true,
     keywords: ["trombinoscope", "photos"],
   },
   {
