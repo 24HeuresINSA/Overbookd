beb151f0f8282e5c77eb18660b1110d5953766cc
Leon Dumestre <leon.dumestre@gmail.com>
2024-01-23 23:22:11 +0100

    refactor(festival-activity): inquiry request table #1500

--- apps/api/prisma/migrations/20240123221702_add_ft_inquiry_requests/migration.sql
+++ apps/api/prisma/migrations/20240123221702_add_ft_inquiry_requests/migration.sql
@@ -1,3 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the `inquiry_request` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "inquiry_request" RENAME CONSTRAINT "inquiry_request_fa_id_fkey" TO "festival_activity_inquiry_request_fa_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "inquiry_request" RENAME CONSTRAINT "inquiry_request_slug_fkey" TO "festival_activity_inquiry_request_slug_fkey";
+
+-- AlterTable
+ALTER TABLE "inquiry_request" RENAME CONSTRAINT "inquiry_request_pkey" TO "festival_activity_inquiry_request_pkey";
+
+-- RenameTable
+ALTER TABLE "inquiry_request" RENAME TO "festival_activity_inquiry_request";
+
 -- CreateTable
 CREATE TABLE "festival_task_inquiry_request" (
     "slug" TEXT NOT NULL,
