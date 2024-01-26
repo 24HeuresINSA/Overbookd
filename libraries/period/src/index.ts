e3d0f282329ad42f30a96991819fbac1bce516ef
Leo Mouyna <mouynaleo@gmail.com>
2024-01-26 14:42:41 +0100

    feat(period): expose an Over Date class. #1514

--- libraries/period/src/index.ts
+++ libraries/period/src/index.ts
@@ -2,3 +2,5 @@ export * from "./duration.constant";
 export { Duration } from "./duration";
 export type { IProvidePeriod } from "./period";
 export { Period, EndBeforeStart } from "./period";
+export { OverDate, isDateString, isHour } from "./date";
+export type { DateString, Hour } from "./date";
