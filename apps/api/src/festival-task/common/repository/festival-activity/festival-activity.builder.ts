209d5bb005f257aa737544aa9934ab8e57577286
Léo Mouyna <mouynaleo@gmail.com>
2024-02-08 12:12:45 +0000

    fix(festival-task): indicate activity does request water or electricity supply. #1536

--- apps/api/src/festival-task/common/repository/festival-activity/festival-activity.builder.ts
+++ apps/api/src/festival-task/common/repository/festival-activity/festival-activity.builder.ts
@@ -5,11 +5,16 @@ export class FestivalActivityBuilder {
   static fromDatabase(
     activityData: DatabaseFestivalActivity,
   ): FestivalTask["festivalActivity"] {
+    const doesRequestElectricitySupply = activityData.electricity.length > 0;
+    const doesRequestWaterSupply = activityData.water !== null;
+    const hasSupplyRequest =
+      doesRequestElectricitySupply || doesRequestWaterSupply;
     return {
       id: activityData.id,
       name: activityData.name,
       status: activityData.status,
       location: activityData.location,
+      hasSupplyRequest,
       timeWindows: activityData.generalTimeWindows,
       inquiry: {
         timeWindows: activityData.inquiryTimeWindows,
