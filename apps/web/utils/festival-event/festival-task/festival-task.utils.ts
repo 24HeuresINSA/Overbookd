b96085b597ba380ee63d158f0efa14512d92212f
Léo Mouyna <mouynaleo@gmail.com>
2024-02-16 18:02:24 +0100

    chore(web): fix type issues. #1565

--- apps/web/utils/festival-event/festival-task/festival-task.utils.ts
+++ apps/web/utils/festival-event/festival-task/festival-task.utils.ts
@@ -1,9 +1,26 @@
-import { FestivalTask } from "@overbookd/festival-event";
+import { DRAFT, FestivalTask } from "@overbookd/festival-event";
 import { HttpStringified } from "@overbookd/http";
 import { CastDraft } from "./draft";
+import { CastInReview } from "./in-review";
 
 export function castTaskWithDate(
-  activity: HttpStringified<FestivalTask>,
+  task: HttpStringified<FestivalTask>,
 ): FestivalTask {
-  return CastDraft.withDate(activity);
+  if (isHttpDraft(task)) {
+    return CastDraft.withDate(task);
+  }
+  return CastInReview.withDate(task);
+}
+
+type Draft = Extract<
+  FestivalTask,
+  {
+    status: typeof DRAFT;
+  }
+>;
+
+function isHttpDraft(
+  task: HttpStringified<FestivalTask>,
+): task is HttpStringified<Draft> {
+  return task.status === DRAFT;
 }
---
+++ apps/web/utils/festival-event/festival-task/in-review.ts
@@ -0,0 +1,56 @@
+import { FestivalTask, IN_REVIEW } from "@overbookd/festival-event";
+import { HttpStringified } from "@overbookd/http";
+import { castTimeWindowWithDate } from "../cast-time-windows";
+
+type InReview = Extract<FestivalTask, { status: typeof IN_REVIEW }>;
+
+export class CastInReview {
+  static withDate(inReview: HttpStringified<InReview>): InReview {
+    return {
+      ...inReview,
+      festivalActivity: this.castActivityWithDate(inReview.festivalActivity),
+      mobilizations: this.mobilizationsWithDate(inReview.mobilizations),
+      feedbacks: this.feedbacksWithDate(inReview.feedbacks),
+      history: this.historyWithDate(inReview.history),
+    };
+  }
+
+  private static castActivityWithDate(
+    festivalActivity: HttpStringified<InReview["festivalActivity"]>,
+  ): InReview["festivalActivity"] {
+    return {
+      ...festivalActivity,
+      timeWindows: festivalActivity.timeWindows.map(castTimeWindowWithDate),
+      inquiry: {
+        ...festivalActivity.inquiry,
+        timeWindows: festivalActivity.inquiry.timeWindows.map(
+          castTimeWindowWithDate,
+        ),
+      },
+    };
+  }
+
+  private static mobilizationsWithDate(
+    mobilizations: HttpStringified<InReview["mobilizations"]>,
+  ): InReview["mobilizations"] {
+    return mobilizations.map((mobilization) => ({
+      ...mobilization,
+      ...castTimeWindowWithDate(mobilization),
+    }));
+  }
+
+  private static feedbacksWithDate(
+    feedbacks: HttpStringified<InReview["feedbacks"]>,
+  ): InReview["feedbacks"] {
+    return feedbacks.map((feedback) => ({
+      ...feedback,
+      publishedAt: new Date(feedback.publishedAt),
+    }));
+  }
+
+  private static historyWithDate(
+    history: HttpStringified<InReview["history"]>,
+  ): InReview["history"] {
+    return history.map((event) => ({ ...event, at: new Date(event.at) }));
+  }
+}
