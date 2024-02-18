5ec89875d13648db9348eb068447baff3126c0c7
Léo Mouyna <mouynaleo@gmail.com>
2024-02-18 14:57:28 +0000

    prefeat(festival-task): assign humain reviewer on reviewing task. #1577

---
+++ domains/festival-event/src/festival-task/ask-for-review/reviewers.inmemory.ts
@@ -0,0 +1,9 @@
+import { Reviewers, ReviewerStat } from "./ask-for-review";
+
+export class InMemoryReviewers implements Reviewers {
+  constructor(private readonly stats: ReviewerStat[] = []) {}
+
+  getAll(): Promise<ReviewerStat[]> {
+    return Promise.resolve(this.stats);
+  }
+}
