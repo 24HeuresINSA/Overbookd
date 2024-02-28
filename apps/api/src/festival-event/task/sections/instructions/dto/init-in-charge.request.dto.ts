dd9e2ba0a451651f97d7a91556717ed72c76d213
Leon Dumestre <leon.dumestre@gmail.com>
2024-02-28 22:21:59 +0100

    fix: improve dto #1618

--- apps/api/src/festival-event/task/sections/instructions/dto/init-in-charge.request.dto.ts
+++ apps/api/src/festival-event/task/sections/instructions/dto/init-in-charge.request.dto.ts
@@ -1,14 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { InitInChargeForm } from "@overbookd/http";
-import { IsArray, IsNotEmpty } from "class-validator";
+import { IsArray, IsNotEmpty, IsString } from "class-validator";
 
 export class InitInChargeRequestDto implements InitInChargeForm {
-  @ApiProperty({})
+  @ApiProperty({
+    type: [Number],
+    description: "The list of volunteer ids to be in charge",
+  })
   @IsArray()
   @IsNotEmpty()
   volunteers: number[];
 
-  @ApiProperty({})
+  @ApiProperty({
+    type: String,
+    description: "The instruction for the in charge volunteers",
+  })
+  @IsString()
   @IsNotEmpty()
   instruction: string;
 }
