a6a63a7db13407077f607ca9f7efe37f02c827f3
Léo Mouyna <mouynaleo@gmail.com>
2024-03-07 07:54:54 +0000

    style(registration): adapt register description to membership. #1636

--- apps/web/pages/register.vue
+++ apps/web/pages/register.vue
@@ -20,7 +20,32 @@
         </v-stepper-step>
 
         <v-stepper-content step="1">
-          <div v-safe-html="registerDescription"></div>
+          <div v-if="isVolunteerRegistration">
+            <p>Hello et merci d'être venu jusqu'ici ! 🤗</p>
+            <p>
+              Le festival des 24 heures de l'INSA n'est possible que grâce aux
+              plus de <strong>350 bénévoles</strong> qui, comme toi, ont accepté
+              de venir nous donner un coup de main pour préparer, organiser et
+              faire en sorte que les festivaliers passent le meilleur moment
+              possible. 🎉
+            </p>
+            <p>
+              Pour que l'on puisse t'accueillir dans les meilleures conditions
+              il nous faut cependant quelques informations sur toi et le temps
+              que tu souhaites consacrer à nous aider.
+            </p>
+            <p>
+              Si tu as la moindre question sur le festival et ce que tu peux
+              faire pour nous aider, n'hésite pas a
+              <nuxt-link to="mailto:humains@24heures.org">
+                contacter les responsables bénévoles
+              </nuxt-link>
+              .
+              <br />Ils seront tes principaux interlocuteurs en amont et en aval
+              du festival. 😎
+            </p>
+          </div>
+          <div v-else v-safe-html="registerDescription"></div>
           <v-btn color="primary" @click="step = 2"> C'est parti ! 🚀 </v-btn>
           <v-btn text @click="returnToLogin"> Annuler </v-btn>
         </v-stepper-content>
@@ -224,7 +249,7 @@ export default Vue.extend({
       return token ?? undefined;
     },
     isVolunteerRegistration(): boolean {
-      return this.token !== undefined;
+      return this.token === undefined;
     },
     cleanComment(): string | undefined {
       if (!this.comment) return undefined;
