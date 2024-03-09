986dfa0131a2e0daf39da147d249fcfb793ff6d0
Polo <polo.diemunsch@gmail.com>
2024-03-09 21:58:40 +0100

    feat(web): add textareas to fill bug report template directly in BugReport card. #1643

--- apps/web/utils/bug-report/bug-report.constant.ts
+++ apps/web/utils/bug-report/bug-report.constant.ts
@@ -1,21 +1,23 @@
 export class BugReport {
-  static get template(): string {
+  static template(
+    expectedBehavior: string,
+    actualBehavior: string,
+    stepsToReproduce: string,
+  ): string {
     return `# URL ou page
 ${window.location.href}
 
 # Comportement attendu
 <!---Décrivez ci-dessous le comportement souhaité.--->
-
+${expectedBehavior}
 
 # Comportement constaté
 <!---Décrivez ci-dessous le comportement observé.--->
+${actualBehavior}
 
 # Étapes pour reproduire
 <!---Listez ci-dessous les étapes à suivre pour reproduire le problème.--->
-
- - Étape 1
- - Étape 2
- ...
+${stepsToReproduce}
 
 # Informations complémentaires
 version: ${process.env.OVERBOOKD_VERSION}  
