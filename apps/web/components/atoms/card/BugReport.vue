3440229840a399afbf395f9ff945fbbf2de94bac
Polo Diemunsch <polo.diemunsch@gmail.com>
2024-03-10 11:29:57 +0000

    fix(web): write bug report on app. #1643

--- apps/web/components/atoms/card/BugReport.vue
+++ apps/web/components/atoms/card/BugReport.vue
@@ -3,30 +3,55 @@
     <v-card-title>Signaler un bug ou feature request</v-card-title>
     <v-card-text>
       <h4>
-        Pour signaler un bug veuillez envoie un mail à <br />
+        Pour signaler un bug, envoie un mail à <br />
         <code>{{ bugReportEmail }}</code>
         <br />
         en précisant le problème rencontré et en joignant une capture d'écran.
+        <br />
         Tu peux cliquer sur le bouton "Envoyer le mail" pour ouvrir ton client
         mail. Si rien ne se passe, tu peux utiliser les deux autres bontons pour
         copier l'adresse mail et le template. <br />
-        ⚠ Utilise bien le modèle pour ton mail, cela nous aide vraiment pour
-        notre travail. Merci🙏
+        ⚠️ Le formulaire ci-dessous permet de remplir le modèle. Utilise le bien
+        pour ton mail, cela nous aide vraiment pour notre travail. Merci 🙏
       </h4>
     </v-card-text>
+    <v-card-text>
+      <v-textarea
+        class="expected-behaviour-input"
+        label="Comportement attendu"
+        no-resize="true"
+        rows="4"
+        @change="updateExpectedBehaviour"
+      />
+      <v-textarea
+        class="actual-behaviour-input"
+        label="Comportement constaté"
+        no-resize="true"
+        rows="4"
+        @change="updateActualBehaviour"
+      />
+      <v-textarea
+        class="steps-to-reproduce-input"
+        label="Étapes pour reproduire"
+        :placeholder="stepsToReproducePlaceholder"
+        no-resize="true"
+        rows="4"
+        @change="updateStepsToReproduce"
+      />
+    </v-card-text>
     <v-card-actions class="actions">
+      <v-btn id="copy-email-button" @click="copyEmail">
+        Copier l'adresse mail
+      </v-btn>
+      <v-btn id="copy-template-button" @click="copyIssueTemplate">
+        Copier le modèle
+      </v-btn>
       <v-btn
+        id="send-email-button"
         :href="`mailto:${bugReportEmail}`"
-        color="primary"
         @click="copyIssueTemplate"
       >
         Envoyer le mail
-      </v-btn>
-      <v-btn color="blue-grey" @click="copyEmail">
-        Copier l'adresse mail
-      </v-btn>
-      <v-btn color="purple" @click="copyIssueTemplate">
-        Copier le modèle
       </v-btn>
     </v-card-actions>
   </v-card>
@@ -41,19 +66,40 @@ export default Vue.extend({
   data: () => ({
     askQuestion: false,
     reportBug: false,
+    expectedBehaviour: "",
+    actualBehaviour: "",
+    stepsToReproduce: "",
   }),
   computed: {
     bugReportEmail(): string {
       return "contact-project+24-heures-insa-overbookd-mono-31598236-issue-@incoming.gitlab.com";
     },
+    stepsToReproducePlaceholder(): string {
+      return "- Étape 1\n- Étape 2\n...";
+    },
   },
   methods: {
+    updateExpectedBehaviour(expectedBehaviour: string) {
+      this.expectedBehaviour = expectedBehaviour;
+    },
+    updateActualBehaviour(actualBehaviour: string) {
+      this.actualBehaviour = actualBehaviour;
+    },
+    updateStepsToReproduce(stepsToReproduce: string) {
+      this.stepsToReproduce = stepsToReproduce;
+    },
     async copyEmail() {
       await navigator.clipboard.writeText(this.bugReportEmail);
       this.$accessor.notif.pushNotification({ message: "Email copié ✅" });
     },
     async copyIssueTemplate() {
-      await navigator.clipboard.writeText(BugReport.template);
+      await navigator.clipboard.writeText(
+        BugReport.template(
+          this.expectedBehaviour,
+          this.actualBehaviour,
+          this.stepsToReproduce,
+        ),
+      );
       this.$accessor.notif.pushNotification({ message: "Modèle copié ✅" });
     },
   },
@@ -67,5 +113,14 @@ export default Vue.extend({
   gap: 15px;
   align-items: center;
   justify-content: center;
+
+  #send-email-button {
+    background-color: deepskyblue;
+  }
+
+  #copy-email-button,
+  #copy-template-button {
+    background-color: orange;
+  }
 }
 </style>
