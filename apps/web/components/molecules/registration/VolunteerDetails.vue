04661fb848e293e9fe0e7ef5180e1937273cec60
Léo Mouyna <mouynaleo@gmail.com>
2024-03-12 15:06:41 +0000

    fix(registration): init availabilities display on festival date week. #1651

---
+++ apps/web/components/molecules/registration/VolunteerDetails.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="volunteer-details">
+    <div class="picture-and-comment">
+      <v-icon class="defaultProfilePicture"> mdi-account-circle </v-icon>
+      <v-textarea
+        :value="volunteer.comment"
+        class="comment-input"
+        label="Commentaire"
+        rows="20"
+        readonly
+      ></v-textarea>
+    </div>
+    <div class="contact-and-availabilities">
+      <div class="contact">
+        <span class="phone">
+          <v-btn icon :href="phoneLink">
+            <v-icon>mdi-phone</v-icon>
+          </v-btn>
+          <v-text-field
+            :value="volunteer.mobilePhone"
+            label="Téléphone portable"
+            readonly
+          ></v-text-field>
+        </span>
+        <span class="email">
+          <v-btn icon :href="emailLink">
+            <v-icon>mdi-email-outline</v-icon>
+          </v-btn>
+          <v-text-field
+            :value="volunteer.email"
+            label="Email"
+            inputmode="email"
+            readonly
+          ></v-text-field>
+        </span>
+      </div>
+      <AvailabilitiesSumupV2
+        :readonly="false"
+        :availabilities="availabilities"
+        @update:availabilities="updateVolunteerAvailabilities"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { EnrollableVolunteer } from "@overbookd/http";
+import { Period } from "@overbookd/period";
+import { formatPhoneLink, formatEmailLink } from "~/utils/user/user.utils";
+import AvailabilitiesSumupV2 from "../availabilities/AvailabilitiesSumupV2.vue";
+
+export default defineComponent({
+  name: "VolunteerDetails",
+  components: { AvailabilitiesSumupV2 },
+  props: {
+    volunteer: {
+      type: Object as () => EnrollableVolunteer,
+      required: true,
+    },
+  },
+  computed: {
+    availabilities(): Period[] {
+      return this.volunteer.availabilities.map((period) => Period.init(period));
+    },
+    emailLink(): string {
+      return formatEmailLink(this.volunteer.email);
+    },
+    phoneLink(): string {
+      return formatPhoneLink(this.volunteer.mobilePhone);
+    },
+  },
+  methods: {
+    async updateVolunteerAvailabilities(availabilities: Period[]) {
+      const volunteerId = this.volunteer.id;
+      await this.$accessor.volunteerAvailability.overrideVolunteerAvailabilities(
+        {
+          volunteerId,
+          availabilities,
+        },
+      );
+      this.$accessor.registration.fetchVolunteerInformation(volunteerId);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.volunteer-details {
+  min-width: 100%;
+  margin: 15px 0px;
+  display: flex;
+  gap: 15px;
+
+  .defaultProfilePicture {
+    font-size: 150px;
+    align-self: center;
+    &.small {
+      font-size: 55px;
+      @media only screen and (max-width: $mobile-max-width) {
+        font-size: 45px;
+      }
+    }
+  }
+
+  .picture-and-comment {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  .contact-and-availabilities {
+    flex-grow: 5;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    .contact {
+      display: flex;
+      gap: 10px;
+      justify-content: space-evenly;
+      .phone,
+      .email {
+        display: flex;
+        gap: 5px;
+        align-items: baseline;
+        min-width: 40%;
+      }
+    }
+  }
+}
+</style>
--- apps/web/pages/registrations/volunteer.vue
+++ apps/web/pages/registrations/volunteer.vue
@@ -30,53 +30,7 @@
 
       <template #expanded-item="{ item }">
         <td :colspan="headers.length">
-          <div class="volunteer-details">
-            <div class="picture-and-comment">
-              <v-icon class="defaultProfilePicture">
-                mdi-account-circle
-              </v-icon>
-              <v-textarea
-                :value="item.comment"
-                class="comment-input"
-                label="Commentaire"
-                rows="20"
-                readonly
-              ></v-textarea>
-            </div>
-            <div class="contact-and-availabilities">
-              <div class="contact">
-                <span class="phone">
-                  <v-btn icon :href="formatPhoneLink(item.mobilePhone)">
-                    <v-icon>mdi-phone</v-icon>
-                  </v-btn>
-                  <v-text-field
-                    v-model="item.mobilePhone"
-                    label="Téléphone portable"
-                    readonly
-                  ></v-text-field>
-                </span>
-                <span class="email">
-                  <v-btn icon :href="formatEmailLink(item.email)">
-                    <v-icon>mdi-email-outline</v-icon>
-                  </v-btn>
-                  <v-text-field
-                    v-model="item.email"
-                    label="Email"
-                    inputmode="email"
-                    readonly
-                  ></v-text-field>
-                </span>
-              </div>
-              <AvailabilitiesSumupV2
-                :readonly="false"
-                :availabilities="castToPeriods(item.availabilities)"
-                @update:availabilities="
-                  (availabilities) =>
-                    updateVolunteerAvailabilities(item.id, availabilities)
-                "
-              />
-            </div>
-          </div>
+          <VolunteerDetails :volunteer="item" />
         </td>
       </template>
     </v-data-table>
@@ -88,17 +42,12 @@
 import { defineComponent } from "vue";
 import { EnrollableVolunteer } from "@overbookd/http";
 import { removeItemAtIndex } from "@overbookd/list";
-import { IProvidePeriod, Period } from "@overbookd/period";
 import SnackNotificationContainer from "~/components/molecules/snack/SnackNotificationContainer.vue";
 import TeamChip from "~/components/atoms/chip/TeamChip.vue";
 import { formatLocalDate } from "~/utils/date/date.utils";
 import { Header } from "~/utils/models/data-table.model";
-import {
-  formatPhoneLink,
-  formatUserNameWithNickname,
-  formatEmailLink,
-} from "~/utils/user/user.utils";
-import AvailabilitiesSumupV2 from "~/components/molecules/availabilities/AvailabilitiesSumupV2.vue";
+import { formatUserNameWithNickname } from "~/utils/user/user.utils";
+import VolunteerDetails from "~/components/molecules/registration/VolunteerDetails.vue";
 
 type RegistrationsData = {
   headers: Header[];
@@ -107,7 +56,7 @@ type RegistrationsData = {
 
 export default defineComponent({
   name: "RegistrationsSoft",
-  components: { SnackNotificationContainer, TeamChip, AvailabilitiesSumupV2 },
+  components: { SnackNotificationContainer, TeamChip, VolunteerDetails },
   data: (): RegistrationsData => ({
     headers: [
       { text: "Inscription", value: "registeredAt" },
@@ -148,71 +97,10 @@ export default defineComponent({
         volunteerIndex,
       );
     },
-    castToPeriods(periodProviders: IProvidePeriod[]): Period[] {
-      return periodProviders.map((period) => Period.init(period));
-    },
     enroll(volunteer: EnrollableVolunteer) {
       this.$accessor.registration.enrollNewVolunteers([volunteer]);
     },
-    async updateVolunteerAvailabilities(
-      volunteerId: EnrollableVolunteer["id"],
-      availabilities: Period[],
-    ) {
-      await this.$accessor.volunteerAvailability.overrideVolunteerAvailabilities(
-        {
-          volunteerId,
-          availabilities,
-        },
-      );
-      this.$accessor.registration.fetchVolunteerInformation(volunteerId);
-    },
-    formatPhoneLink,
-    formatEmailLink,
     formatUserNameWithNickname,
   },
 });
 </script>
-
-<style lang="scss" scoped>
-.defaultProfilePicture {
-  font-size: 150px;
-  align-self: center;
-  &.small {
-    font-size: 55px;
-    @media only screen and (max-width: $mobile-max-width) {
-      font-size: 45px;
-    }
-  }
-}
-
-.volunteer-details {
-  min-width: 100%;
-  margin: 15px 0px;
-  display: flex;
-  gap: 15px;
-  .picture-and-comment {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-  }
-  .contact-and-availabilities {
-    flex-grow: 5;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    .contact {
-      display: flex;
-      gap: 10px;
-      justify-content: space-evenly;
-      .phone,
-      .email {
-        display: flex;
-        gap: 5px;
-        align-items: baseline;
-        min-width: 40%;
-      }
-    }
-  }
-}
-</style>
