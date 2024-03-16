9be5e35bcaaa336172070761d429305209766c13
Leon Dumestre <leon.dumestre@gmail.com>
2024-03-16 18:01:03 +0100

    chore: create new nuxt project #1688
    chore: rename web folders and run new web #1688
    
    chore: add auth #1688
    
    chore: ci & docker for new web #1688
    
    fix: pnpm-lock #1688
    
    fix: pnpm-lock again #1688
    
    fix: config #1688
    
    fix: format cmd & remove test packages #1688
    
    fix: eslint #1688
    
    fix: ci & eslint #1688
    
    fix: parser #1688
    
    fix: dockerfile #1688
    
    fix: add recursive #1688
    
    fix: remove component #1688
    
    fix: component name #1688
    
    fix: docker file #1688

---
+++ apps/new-web/plugins/vuetify.ts
@@ -0,0 +1,18 @@
+// plugins/vuetify.js
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import * as labsComponents from "vuetify/labs/components";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    ssr: true,
+    components: {
+      ...components,
+      ...labsComponents,
+    },
+    directives,
+  });
+
+  nuxtApp.vueApp.use(vuetify);
+});
