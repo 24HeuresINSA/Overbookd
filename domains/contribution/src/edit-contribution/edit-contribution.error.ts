cc580e29ff6c81e4bb145a91960249d7210388cd
Leon Dumestre <leon.dumestre@gmail.com>
2024-03-18 16:11:10 +0100

    fix: prune #1680

--- domains/contribution/src/edit-contribution/edit-contribution.error.ts
+++ domains/contribution/src/edit-contribution/edit-contribution.error.ts
@@ -1,9 +1,9 @@
 import { ContributionError } from "../contribution.error";
 
-export const NOT_FOUND_CONTRIBUTION_ERROR_MESSAGE =
+const NOT_FOUND_CONTRIBUTION_ERROR_MESSAGE =
   "La cotisation n'a pas été trouvée";
 
-export class EditAmountError extends ContributionError {}
+class EditAmountError extends ContributionError {}
 
 export class NotFoundContribution extends EditAmountError {
   constructor() {
