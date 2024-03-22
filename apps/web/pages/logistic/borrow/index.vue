251c9be7bbd7e5be86f75e583e3fa4cec08e77bd
LucVDH <luke.vandh@gmail.com>
2024-03-22 12:56:53 +0000

    prefeat(borrow-sheet): add a data table to the index of borrow sheets #1712

--- apps/web/pages/logistic/borrow/index.vue
+++ apps/web/pages/logistic/borrow/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <div></div>
+  <div class="borrows-page">
+    <h1>Fiches emprunts</h1>
+    <v-data-table :headers="headers" :items="borrowItems"></v-data-table>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "BorrowSheet",
+  name: "Borrows",
+  data() {
+    return {
+      headers: [
+        { text: "Nom", value: "name" },
+        { text: "Date de disponibilité", value: "availableOn" },
+        { text: "Date de retour", value: "unavailableOn" },
+      ],
+      borrowItems: [],
+    };
+  },
 });
 </script>
+
+<style lang="scss">
+.borrows-page {
+  margin-left: 20px;
+}
+</style>
