235f56eb68dc99350cd6cdb1808d55290ff6ed75
armynion <army.brunet@laposte.net>
2024-03-28 21:35:13 +0100

    prefeat(logistic-purchase): create table #1673

---
+++ apps/web/components/organisms/logistic/purchase/PurchaseTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-data-table :headers="headers" :items="purchases" class="purchase-list">
+    <template #no-data> Aucune fiche achat </template>
+  </v-data-table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Header } from "~/utils/models/data-table.model";
+
+type PurchaseTableData = {
+  headers: Header[];
+};
+
+export default defineComponent({
+  name: "PurchaseTable",
+  data: (): PurchaseTableData => ({
+    headers: [
+      { text: "Nom", value: "seller" },
+      { text: "Date de disponibilité", value: "availableOn" },
+      { text: "Supprimer", value: "remove", sortable: false, align: "center" },
+    ],
+  }),
+  computed: {
+    purchases() {
+      return [];
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.purchase-list {
+  cursor: pointer;
+}
+</style>
--- apps/web/pages/logistic/purchase/index.vue
+++ apps/web/pages/logistic/purchase/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <div></div>
+  <div class="purchases-page">
+    <h1>Fiches achats</h1>
+    <PurchaseTable />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import PurchaseTable from "~/components/organisms/logistic/purchase/PurchaseTable.vue";
 
 export default defineComponent({
-  name: "PurchaseSheet",
+  name: "Purchases",
+  components: { PurchaseTable },
 });
 </script>
