51e4ceb776edf6f1d3f54ab116001af84ef4f41a
Leon Dumestre <leon.dumestre@gmail.com>
2024-04-15 19:20:48 +0200

    chore: remove vuetify colors

--- apps/web/nuxt.config.js
+++ apps/web/nuxt.config.js
@@ -1,5 +1,4 @@
 import { ONE_DAY_IN_MS } from "@overbookd/period";
-import colors from "vuetify/es5/util/colors";
 
 export default {
   env: {
@@ -108,17 +107,6 @@ export default {
     customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
     },
   },
 
--- apps/web/plugins/vuetify.js
+++ apps/web/plugins/vuetify.js
@@ -6,13 +6,5 @@ Vue.use(Vuetify);
 export default new Vuetify({
   theme: {
     dark: true,
-    themes: {
-      jaune: {
-        primary: "#d1d420",
-        secondary: "#d1d420",
-        accent: "#d1d420",
-        error: "#d1d420",
-      },
-    },
   },
 });
