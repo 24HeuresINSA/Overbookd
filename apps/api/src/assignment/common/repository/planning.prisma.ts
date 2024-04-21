c7224d670017d7db35ff348799e67be14fb6a5fa
Léon Dumestre <leon.dumestre@gmail.com>
2024-04-21 18:58:08 +0000

    prefeat(assignment): display mobilizations in funnel planning #1778

--- apps/api/src/assignment/common/repository/planning.prisma.ts
+++ apps/api/src/assignment/common/repository/planning.prisma.ts
@@ -2,27 +2,54 @@ import { Planning, PlanningEvent } from "@overbookd/assignment";
 import { PrismaService } from "../../../prisma.service";
 import { IProvidePeriod } from "@overbookd/period";
 import { SELECT_PERIOD } from "./period.query";
+import { EXISTS_AND_NOT_READY_TO_ASSIGN } from "./task.query";
 
-type DatabasePlanningEvent = IProvidePeriod & {
+type DatabaseAssignment = IProvidePeriod & {
   festivalTask: { name: string };
+};
+
+type DatabaseMobilization = IProvidePeriod & {
+  ft: { name: string };
 };
 
 export class PrismaPlanning implements Planning {
   constructor(private readonly prisma: PrismaService) {}
 
   async for(volunteerId: number): Promise<PlanningEvent[]> {
-    const events = await this.prisma.assignment.findMany({
-      where: { assignees: { some: { userId: volunteerId } } },
-      select: {
-        ...SELECT_PERIOD,
-        festivalTask: { select: { name: true } },
-      },
-    });
-    return events.map(toPlanningEvent);
+    const isAssignedCondition = {
+      assignees: { some: { userId: volunteerId } },
+    };
+
+    const [dbAssignments, dbMobilizations] = await Promise.all([
+      this.prisma.assignment.findMany({
+        where: isAssignedCondition,
+        select: {
+          ...SELECT_PERIOD,
+          festivalTask: { select: { name: true } },
+        },
+      }),
+      this.prisma.festivalTaskMobilization.findMany({
+        where: { ...isAssignedCondition, ft: EXISTS_AND_NOT_READY_TO_ASSIGN },
+        select: { ...SELECT_PERIOD, ft: { select: { name: true } } },
+      }),
+    ]);
+
+    const assignments = dbAssignments.map(toPlanningEventFromAssignment);
+    const mobilizations = dbMobilizations.map(toPlanningEventFromMobilization);
+    return [...assignments, ...mobilizations];
   }
 }
 
-function toPlanningEvent(event: DatabasePlanningEvent): PlanningEvent {
+function toPlanningEventFromAssignment(
+  event: DatabaseAssignment,
+): PlanningEvent {
   const { start, end, festivalTask } = event;
   return { start, end, task: festivalTask.name };
+}
+
+function toPlanningEventFromMobilization(
+  event: DatabaseMobilization,
+): PlanningEvent {
+  const { start, end, ft } = event;
+  return { start, end, task: ft.name };
 }
---
+++ apps/api/src/assignment/common/repository/task.query.ts
@@ -0,0 +1,11 @@
+import { READY_TO_ASSIGN } from "@overbookd/festival-event-constants";
+import { IS_NOT_DELETED } from "./common.query";
+
+const NOT_READY_TO_ASSIGN = {
+  status: { not: READY_TO_ASSIGN },
+} as const;
+
+export const EXISTS_AND_NOT_READY_TO_ASSIGN = {
+  ...IS_NOT_DELETED,
+  ...NOT_READY_TO_ASSIGN,
+} as const;
