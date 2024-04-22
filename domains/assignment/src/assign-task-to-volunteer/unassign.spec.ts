95f657c21cdcb278cc6405487cfa8e2770a91bef
Leon Dumestre <leon.dumestre@gmail.com>
2024-04-22 12:00:01 +0200

    fix: tests #1787

--- domains/assignment/src/assign-task-to-volunteer/unassign.spec.ts
+++ domains/assignment/src/assign-task-to-volunteer/unassign.spec.ts
@@ -1,21 +1,17 @@
 import { describe, expect } from "vitest";
 import { InMemoryAssignments } from "./repositories/assignments.inmemory";
 import {
-  couperDesCarottes,
+  gererLaCaisse,
   lea,
   rendreKangoo,
 } from "./funnel/assign-volunteers-funnel.test-utils";
 import { Unassign } from "./unassign";
 
 describe("Unassign volunteer", () => {
-  const assignments = new InMemoryAssignments([
-    rendreKangoo,
-    couperDesCarottes,
-  ]);
+  const assignments = new InMemoryAssignments([rendreKangoo, gererLaCaisse]);
   const unassign = new Unassign(assignments);
   describe("when the volunteer is assigned to the task", async () => {
-    await unassign.volunteerFromTask(couperDesCarottes, lea.volunteer.id);
-
-    expect(assignments.all).toEqual([rendreKangoo]);
+    await unassign.volunteerFromTask(gererLaCaisse, lea.volunteer.id);
+    expect(assignments.all).toContainEqual({ ...gererLaCaisse, assignees: [] });
   });
 });
