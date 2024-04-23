73a1e621ea020178a8540348a470377fd4afb068
Léo Mouyna <mouynaleo@gmail.com>
2024-04-23 11:45:35 +0000

    fix(assignment): count assignees on each assignment instead of on mobilization. #1800

--- apps/api/src/assignment/task-to-volunteer/repository/task.query.ts
+++ apps/api/src/assignment/task-to-volunteer/repository/task.query.ts
@@ -8,19 +8,25 @@ export const IS_READY_AND_EXISTS = {
   status: READY_TO_ASSIGN,
 } as const;
 
+const SELECT_ASSIGNEE = { teamCode: true, userId: true };
+const SELECT_ASSIGNMENT = {
+  id: true,
+  start: true,
+  end: true,
+  assignees: { select: SELECT_ASSIGNEE },
+};
+const SELECT_MOBILIZATION = {
+  id: true,
+  teams: { select: { teamCode: true, count: true } },
+  assignments: { select: SELECT_ASSIGNMENT },
+};
+
 export const SELECT_TASK_WITH_ASSIGNMENTS = {
   id: true,
   name: true,
   topPriority: true,
   category: true,
-  mobilizations: {
-    select: {
-      id: true,
-      teams: { select: { teamCode: true, count: true } },
-      assignees: { select: { teamCode: true, userId: true } },
-      assignments: { select: { id: true, start: true, end: true } },
-    },
-  },
+  mobilizations: { select: SELECT_MOBILIZATION },
 };
 
 export type DatabaseTask = Categorize & {
@@ -29,7 +35,9 @@ export type DatabaseTask = Categorize & {
   mobilizations: {
     id: string;
     teams: { teamCode: string; count: number }[];
-    assignees: { teamCode: string; userId: number }[];
-    assignments: (IProvidePeriod & { id: string })[];
+    assignments: (IProvidePeriod & {
+      id: string;
+      assignees: { teamCode: string; userId: number }[];
+    })[];
   }[];
 };
