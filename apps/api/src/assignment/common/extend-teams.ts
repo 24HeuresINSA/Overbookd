732c43a70b25a995980800e95a12bf54ce414afc
Léon Dumestre <leon.dumestre@gmail.com>
2024-04-24 17:52:36 +0000

    fix(assignment): sort assignments in orga-task and fix confiance assign #1818

--- apps/api/src/assignment/common/extend-teams.ts
+++ apps/api/src/assignment/common/extend-teams.ts
@@ -2,6 +2,6 @@ import { CONFIANCE, HARD, VIEUX } from "@overbookd/assignment";
 
 export function extendOneOfTeams(oneOfTeams: string[]): string[] {
   return oneOfTeams.includes(CONFIANCE)
-    ? [...new Set(...oneOfTeams, VIEUX, HARD)]
+    ? [...oneOfTeams, VIEUX, HARD]
     : oneOfTeams;
 }
