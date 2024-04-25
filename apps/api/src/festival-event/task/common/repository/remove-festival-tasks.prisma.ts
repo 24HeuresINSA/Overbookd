65a1bbfc3ed8dc8b4b6a1e58035b195eb587f927
Leo Mouyna <mouynaleo@gmail.com>
2024-04-25 18:17:54 +0200

    fix(festival-task): don't allow delete ready to assign task. #1837

--- apps/api/src/festival-event/task/common/repository/remove-festival-tasks.prisma.ts
+++ apps/api/src/festival-event/task/common/repository/remove-festival-tasks.prisma.ts
@@ -2,13 +2,17 @@ import { FestivalActivity } from "@overbookd/festival-event";
 import { PrismaService } from "../../../../prisma.service";
 import { RemoveFestivalTasks } from "../festival-task-common.model";
 import { buildFestivalTaskCondition } from "./festival-task.query";
+import { READY_TO_ASSIGN } from "@overbookd/festival-event-constants";
 
 export class PrismaRemoveFestivalTasks implements RemoveFestivalTasks {
   constructor(private readonly prisma: PrismaService) {}
 
   async apply(id: FestivalActivity["id"]): Promise<void> {
     await this.prisma.festivalTask.update({
-      where: buildFestivalTaskCondition(id),
+      where: {
+        ...buildFestivalTaskCondition(id),
+        NOT: { status: READY_TO_ASSIGN },
+      },
       data: { isDeleted: true },
     });
   }
