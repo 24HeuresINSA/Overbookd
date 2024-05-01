f82d0d7a17e4ed2c0eba05f35c97a8ac4ca11e67
Leo Mouyna <mouynaleo@gmail.com>
2024-05-01 13:08:31 +0200

    prefeat(planning): allow download or sync planning. #1869

---
+++ apps/web/components/atoms/planning/DownloadPlanning.vue
@@ -0,0 +1,130 @@
+<template>
+  <div v-if="canDowloadPlanning">
+    <v-list dense>
+      <v-list-group prepend-icon="mdi-calendar">
+        <template #activator>
+          <v-list-item-title>
+            Récupérer
+            {{ isMe ? "mon" : "le" }} planning
+          </v-list-item-title>
+        </template>
+        <v-list-group v-if="isMe" no-action sub-group>
+          <template #activator>
+            <v-list-item-title>
+              <v-icon>mdi-sync</v-icon>
+              Synchroniser mon agenda
+            </v-list-item-title>
+          </template>
+          <v-list-item link @click="syncWithGoogle">
+            <v-list-item-icon>
+              <v-icon>mdi-google</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Avec Google Calendar</v-list-item-title>
+          </v-list-item>
+          <v-list-item link @click="syncWithMicrosoft">
+            <v-list-item-icon>
+              <v-icon>mdi-microsoft-outlook</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title> Avec Microsoft Outlook </v-list-item-title>
+          </v-list-item>
+          <v-list-item link @click="copySyncLinkToClipboard">
+            <v-list-item-icon>
+              <v-icon>mdi-link</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              Obtenir le lien de synchronisation
+            </v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+        <v-list-group no-action sub-group>
+          <template #activator>
+            <v-list-item-title>
+              <v-icon>mdi-download</v-icon>
+              Télécharger
+            </v-list-item-title>
+          </template>
+          <v-list-item link @click="downloadPdf">
+            <v-list-item-icon>
+              <v-icon>mdi-file-pdf-box</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Au format PDF</v-list-item-title>
+          </v-list-item>
+          <v-list-item link @click="downloadIcal">
+            <v-list-item-icon>
+              <v-icon>mdi-calendar-multiselect</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Au format ICal</v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+      </v-list-group>
+    </v-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { AFFECT_VOLUNTEER, DOWNLOAD_PLANNING } from "@overbookd/permission";
+import { User, UserPersonalData } from "@overbookd/user";
+import { downloadPlanning } from "~/utils/planning/download";
+import { Edition } from "@overbookd/contribution";
+
+export default defineComponent({
+  name: "DownloadPlanning",
+  computed: {
+    me(): User {
+      return this.$accessor.user.me;
+    },
+    user(): UserPersonalData {
+      return this.$accessor.user.selectedUser;
+    },
+    syncLink() {
+      return this.$accessor.planning.link;
+    },
+    isMe(): boolean {
+      return this.me.id === this.user.id;
+    },
+    canDowloadPlanning(): boolean {
+      if (!this.isMe) {
+        return this.$accessor.user.can(AFFECT_VOLUNTEER);
+      }
+      return this.$accessor.user.can(DOWNLOAD_PLANNING);
+    },
+  },
+  methods: {
+    syncWithGoogle() {
+      if (!this.syncLink) return;
+      const link = `https://calendar.google.com/calendar/u/0/r?cid=${this.syncLink}`;
+      window.open(link, "_blank");
+    },
+    syncWithMicrosoft() {
+      if (!this.syncLink) return;
+      const link = `https://outlook.live.com/calendar/addcalendar?name=24%20Heures%20de%20l%27INSA%20-%20${Edition.current}e&url=${this.syncLink}`;
+      window.open(link, "_blank");
+    },
+    async copySyncLinkToClipboard() {
+      if (!this.syncLink) return;
+      await navigator.clipboard.writeText(this.syncLink);
+      this.$accessor.notif.pushNotification({ message: "Lien copié ✅" });
+    },
+    async downloadPdf() {
+      if (this.isMe) {
+        await this.$accessor.planning.fetchMyPdfPlanning();
+        downloadPlanning(this.$accessor.planning.planningBase64Data, this.me);
+        return;
+      }
+      await this.$accessor.planning.fetchAllPdfPlannings([this.user]);
+      this.$accessor.planning.volunteerPlannings.map(
+        ({ volunteer, planningBase64Data }) =>
+          downloadPlanning(planningBase64Data, volunteer),
+      );
+    },
+    async downloadIcal() {
+      if (this.isMe) {
+        console.log("get My planning");
+        return;
+      }
+      console.log("get", this.user.id, "planning");
+    },
+  },
+});
+</script>
