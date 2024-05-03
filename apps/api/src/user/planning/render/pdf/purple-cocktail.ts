087ea4fb6eecdfda0c329745a0487312213596bd
LucVDH <luke.vandh@gmail.com>
2024-05-03 14:46:58 +0200

    fix(planning): give only one purple cocktail instruction list #1893

--- apps/api/src/user/planning/render/pdf/purple-cocktail.ts
+++ apps/api/src/user/planning/render/pdf/purple-cocktail.ts
@@ -1,80 +1,17 @@
 import { Content } from "pdfmake/interfaces";
 
 export class PurpleCocktail {
-  static generateBarmanWorkflow(): Content[] {
-    const headerText =
-      "PROCEDURE SIMPLIFIEE COCKTAIL PURPLE A DESTINATION DES BARMANS";
+  static generateWorkflow(): Content[] {
+    const headerText = "Procédure Cocktail Purple - 24h de l’INSA";
     const descriptionText =
-      "Cocktail Purple : Cocktail Purple est un dispositif mis en place par l'association Purple Effect en milieu festif. Une victime de VHSS (violences et harcèlement sexistes et sexuels) a la possibilité de commander au bar de l’événement un cocktail purple, qui est un nom de code permettant de la mettre en sécurité et de prévenir les équipes de sécurité et de prévention.";
+      "Définition : Cocktail Purple est un dispositif mis en place par l’association Purple Effect en milieu festif. Une victime de VHSS (violences et harcèlement sexistes et sexuels) a la possibilité de commander au bar de l’événement un cocktail purple, qui est un nom de code permettant de la mettre en sécurité et de prévenir les équipes de sécurité et de prévention.";
     const procedureSteps = [
       {
         text: [
           "La ",
           { text: "VICTIME", style: ["bold"] },
           " se présente au bar et commande un cocktail purple au ",
-          { text: "BARMAN A(=TOI).", style: ["bold"] },
-        ],
-      },
-      {
-        text: [
-          "Le ",
-          { text: "BARMAN A", style: ["bold"] },
-          " reste en contact avec la ",
-          { text: "VICTIME", style: ["bold"] },
-          " et prévient la personne la plus proche de lui ",
-          { text: "(BARMAN B(=UN AUTRE BÉNÉVOLE)).", style: ["bold"] },
-        ],
-      },
-      {
-        text: [
-          "Le ",
-          { text: "BARMAN B", style: ["bold"] },
-          " va chercher le ",
-          {
-            text: "RESP BARMAN (CASQUETTE NOIRE NINKASI)",
-            style: ["bold"],
-          },
-          " et lui explique qu'il y a une commande de cocktail purple.",
-        ],
-      },
-      {
-        text: [
-          "Le ",
-          { text: "BARMAN B", style: ["bold"] },
-          " et le ",
-          { text: "RESP BARMAN", style: ["bold"] },
-          " se rendent dans la fosse pour aller à la rencontre de la ",
-          { text: "VICTIME.", style: ["bold"] },
-        ],
-      },
-    ];
-    const safetyInstructionsText = [
-      "Si le BARMAN A ne se sent pas de rester seul avec la VICTIME, il peut demander à une autre personne de rester avec lui ou de le remplacer.",
-      "Si le RESP BARMAN ou le BARMAN B ne se sentent pas d'aller dans la fosse, ils peuvent prévenir le RESP BAR qui peut y aller à leur place.",
-      "Dans la fosse, si vous êtes en contact avec l'AGRESSEUR, ne pas chercher à le confronter ou à le surveiller. L'objectif est de mettre la VICTIME à l'abri, le reste sera à la charge de la SECURITE.",
-    ];
-    const header = PurpleCocktail.generateHeader(headerText);
-    const description = PurpleCocktail.generateDescription(descriptionText);
-    const steps: Content = PurpleCocktail.generateSteps(procedureSteps);
-    const safetyInstructions: Content =
-      PurpleCocktail.generateSafetyInstructions(safetyInstructionsText);
-
-    return [header, description, steps, safetyInstructions];
-  }
-
-  static generateLeaderWorkflow(): Content[] {
-    const headerText =
-      "PROCEDURE GENERALE COCKTAIL PURPLE A DESTINATION DES RESPS BARS, RESPS BARMANS";
-    const descriptionText =
-      "Cocktail Purple : Cocktail Purple est un dispositif mis en place par l'association Purple Effect en milieu festif. Une victime de VHSS (violences et harcèlement sexistes et sexuels) a la possibilité de commander au bar de l’événement un cocktail purple, qui est un nom de code permettant de la mettre en sécurité et de prévenir les équipes de sécurité et de prévention.";
-    const procedureSteps = [
-      {
-        text: [
-          "La ",
-          { text: "VICTIME", style: ["bold"] },
-          " se présente au bar et commande un cocktail purple au ",
-          { text: "BARMAN A", style: ["bold"] },
-          " un cocktail purple",
+          { text: "BARMAN A.", style: ["bold"] },
         ],
       },
       {
@@ -90,22 +27,22 @@ export class PurpleCocktail {
       {
         text: [
           "Le ",
-          { text: "RESP BARMAN", style: ["bold"] },
-          " appelle par radio la ",
-          { text: "SECURITE", style: ["bold"] },
-          ", informe qu'il y a une commande de cocktail purple et précise le nom du bar",
+          { text: "BARMAN B", style: ["bold"] },
+          " va chercher le ",
+          {
+            text: "RESP BARMAN",
+            style: ["bold"],
+          },
+          ", lui explique qu'il y a une commande de cocktail purple.",
         ],
       },
       {
         text: [
           "Le ",
-          { text: "BARMAN B", style: ["bold"] },
-          " va chercher le ",
-          {
-            text: "RESP BARMAN (CASQUETTE NOIRE NINKASI)",
-            style: ["bold"],
-          },
-          " et lui explique qu’il y a une commande de cocktail purple.",
+          { text: "RESP BARMAN", style: ["bold"] },
+          " appelle par radio la ",
+          { text: "SECURITE", style: ["bold"] },
+          ", informe qu'il y a une commande de cocktail purple et précise le nom du bar",
         ],
       },
       {
@@ -179,11 +116,18 @@ export class PurpleCocktail {
         ],
       },
     ];
+    const safetyInstructionsText = [
+      "Si le BARMAN A ne se sent pas de rester seul avec la VICTIME, il peut demander à une autre personne de rester avec lui ou de le remplacer.",
+      "Si le RESP BARMAN ou le BARMAN B ne se sentent pas d'aller dans la fosse, ils peuvent prévenir le RESP BAR qui peut y aller à leur place.",
+      "Dans la fosse, si vous êtes en contact avec l'AGRESSEUR, ne pas chercher à le confronter ou à le surveiller. L'objectif est de mettre la VICTIME à l'abri, le reste sera à la charge de la SECURITE.",
+    ];
     const header = PurpleCocktail.generateHeader(headerText);
     const description = PurpleCocktail.generateDescription(descriptionText);
     const steps: Content = PurpleCocktail.generateSteps(procedureSteps);
+    const safetyInstructions: Content =
+      PurpleCocktail.generateSafetyInstructions(safetyInstructionsText);
 
-    return [header, description, steps];
+    return [header, description, steps, safetyInstructions];
   }
 
   private static generateSafetyInstructions(
