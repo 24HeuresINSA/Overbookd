898fed5c31e297112fcea21f80670fd065d3e1ff
Leon Dumestre <leon.dumestre@gmail.com>
2024-05-30 15:43:20 +0200

    fix: add logo layout #1924

---
+++ apps/web/layouts/logo.vue
@@ -0,0 +1,58 @@
+<template>
+  <nuxt-link class="application" to="/">
+    <img class="logo" :src="`/img/logo/${logo}`" @click="increment" />
+    <span class="version">{{ versionString }}</span>
+  </nuxt-link>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed, watch } from "vue";
+
+const version = process.env.OVERBOOKD_VERSION;
+const TWENTY_FOUR = 24;
+const FIFTY_ONE = 51;
+
+const counter = ref(0);
+
+const versionString = computed(() => `v${version}` ?? "");
+
+const logo = computed(() => {
+  if (counter.value >= FIFTY_ONE) return "Pastis.png";
+  if (counter.value >= TWENTY_FOUR) return "Ricard.png";
+  if (window.innerWidth <= 960) return "logo_mobile.png";
+  return "logo_desktop.png";
+});
+
+const track = "audio/jaune.m4a";
+
+watch(counter, (newCounter) => {
+  if (newCounter === TWENTY_FOUR) {
+    const audio = new Audio(track);
+    audio.play();
+  }
+});
+
+function increment() {
+  counter.value++;
+}
+</script>
+
+<style lang="scss" scoped>
+.application {
+  display: flex;
+  gap: 3px;
+  align-items: center;
+
+  .logo {
+    max-width: 200px;
+    max-height: 51px;
+  }
+
+  .version {
+    font-weight: 600;
+    @media only screen and (max-width: $mobile-max-width) {
+      display: none;
+    }
+  }
+}
+</style>
