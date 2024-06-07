1cdb26c6c4b7fb928cb77513a628e4be624436fa
Léon Dumestre <leon.dumestre@gmail.com>
2024-06-07 13:53:14 +0000

    refactor(web): upgrade error page #1940

--- apps/web/error.vue
+++ apps/web/error.vue
@@ -1,74 +1,75 @@
 <template>
   <NuxtLayout>
-    <div class="container">
-      <div v-if="error?.statusCode === pageNotFoundErrorCode" class="content">
-        <v-img
-          class="lost-gif"
-          src="https://media.giphy.com/media/1EmBoG0IL50VIJLWTs/giphy.gif"
-        />
-        <h1>T'es perdu ?</h1>
-      </div>
-
-      <div v-else class="content">
-        <v-img
-          class="error-gif"
-          src="https://media1.giphy.com/media/EtB1yylKGGAUg/giphy.gif?cid=ecf05e47qaynhsxphvtrr3q7u3bskmwj15onszejof9z483h&rid=giphy.gif&ct=g"
-        />
-        <h1>La ComSI be like</h1>
-      </div>
-      <p class="error-message">{{ error?.message }}</p>
+    <div class="error">
+      <v-img class="error__gif" :src="error.gif" />
+      <h1>{{ error.comment }}</h1>
+      <p class="error__message">{{ error.message }}</p>
     </div>
   </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
-const error = useError();
+const nuxtError = useError();
 
 const pageNotFoundErrorCode = 404;
 const pageNotFound = "404 Not Found";
 const otherError = "An error occurred";
 
+const isPageNotFoundError = computed(
+  () => nuxtError.value?.statusCode === pageNotFoundErrorCode,
+);
+
+type PageError = {
+  title: string;
+  gif: string;
+  message: string;
+  comment: string;
+};
+
+const lostGif = "https://media.giphy.com/media/1EmBoG0IL50VIJLWTs/giphy.gif";
+const angryPandaGif =
+  "https://media1.giphy.com/media/EtB1yylKGGAUg/giphy.gif?cid=ecf05e47qaynhsxphvtrr3q7u3bskmwj15onszejof9z483h&rid=giphy.gif&ct=g";
+
+const lost = { title: pageNotFound, gif: lostGif, comment: "T'es perdu ?" };
+const other = {
+  title: otherError,
+  gif: angryPandaGif,
+  comment: "La ComSa be like",
+};
+
+const context = isPageNotFoundError.value ? lost : other;
+const message = nuxtError.value?.message || "";
+const error: PageError = { ...context, message };
+
 useHead(() => {
-  const title =
-    error.value?.statusCode === pageNotFoundErrorCode
-      ? pageNotFound
-      : otherError;
-  return { title };
+  return { title: error.title };
 });
 </script>
 
 <style lang="scss" scoped>
-.container {
+.error {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   text-align: center;
   padding: 20px;
-}
 
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 20px;
-}
+  h1 {
+    font-size: 24px;
+    font-weight: bold;
+    margin: 20px 0;
+  }
 
-h1 {
-  font-size: 24px;
-  font-weight: bold;
-  margin-top: 20px;
-}
+  &__message {
+    font-size: 18px;
+    color: #666;
+  }
 
-.error-message {
-  font-size: 18px;
-  color: #666;
-}
-
-.lost-gif,
-.error-gif {
-  width: 480px;
-  border-radius: 5px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  &__gif {
+    width: 480px;
+    border-radius: 5px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
 }
 </style>
