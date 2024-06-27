ac84ee0be9141785c19df45753803d433b60faf7
Léon Dumestre <leon.dumestre@gmail.com>
2024-06-27 16:50:45 +0000

    feat(web): add tiptap component #1987

---
+++ apps/web/components/atoms/field/TipTap.vue
@@ -0,0 +1,53 @@
+<template>
+  <VuetifyTiptap
+    v-model="content"
+    :label="props.title"
+    :extensions="extensions"
+    rounded
+  />
+</template>
+
+<script lang="ts" setup>
+import { VuetifyTiptap } from "vuetify-pro-tiptap";
+import "vuetify-pro-tiptap/style.css";
+import {
+  BaseKit,
+  Bold,
+  Italic,
+  Underline,
+  Strike,
+  Color,
+  Highlight,
+  Heading,
+  Table,
+  Fullscreen,
+  History,
+} from "vuetify-pro-tiptap";
+
+const extensions = [
+  BaseKit.configure({
+    placeholder: {
+      placeholder: "Ecris ton texte ici...",
+    },
+  }),
+  Bold,
+  Italic,
+  Underline,
+  Strike,
+  Color,
+  Highlight,
+  Heading,
+  Table,
+  Fullscreen,
+  History,
+];
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: undefined,
+  },
+});
+
+const content = ref("");
+</script>
