a31bea3b6562544e5201d7b29b2ea3cbdac3d784
Léon Dumestre <leon.dumestre@gmail.com>
2024-07-30 08:58:07 +0000

    fix(web): change vuetify language in fr #2083
    Approved-by: Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>

--- apps/web/plugins/vuetify.client.ts
+++ apps/web/plugins/vuetify.client.ts
@@ -1,6 +1,13 @@
 import { createVuetify } from "vuetify";
+import { fr } from "vuetify/locale";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify({ ssr: false });
+  const vuetify = createVuetify({
+    ssr: false,
+    locale: {
+      locale: "fr",
+      messages: { fr },
+    },
+  });
   nuxtApp.vueApp.use(vuetify);
 });
