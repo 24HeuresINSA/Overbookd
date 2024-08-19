5b712681e2ad3ea520cf072fc23361d66a296b70
Léon Dumestre <leon.dumestre@gmail.com>
2024-08-19 17:11:04 +0000

    fix(friends): throw custom error when already friends #2126
    Approved-by: Léo Mouyna <mouynaleo@gmail.com>

--- apps/api/src/friend/friend.service.ts
+++ apps/api/src/friend/friend.service.ts
@@ -1,88 +1,53 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "../../src/prisma.service";
 import { User } from "@overbookd/user";
-
-type FriendWithData = {
-  friend: User;
-};
+import { SELECT_USER_IDENTIFIER } from "../common/query/user.query";
+import { IS_NOT_DELETED } from "../common/query/not-deleted.query";
 
 @Injectable()
 export class FriendService {
   constructor(private prisma: PrismaService) {}
 
-  private readonly SELECT_FRIEND = {
-    id: true,
-    lastname: true,
-    firstname: true,
-    nickname: true,
-  };
-
   async findUserFriends(id: number): Promise<User[]> {
     const friends = await this.prisma.friend.findMany({
-      where: {
-        requestorId: id,
-      },
-      select: {
-        friend: {
-          select: this.SELECT_FRIEND,
-        },
-      },
+      where: { requestorId: id },
+      select: { friend: { select: SELECT_USER_IDENTIFIER } },
     });
-
-    return friends.map(retrieveFriend);
+    return friends.map(({ friend }) => friend);
   }
 
   async findFriends(): Promise<User[]> {
     const nonFriendableTeams = ["fen", "voiture", "camion"];
 
     return this.prisma.user.findMany({
-      select: this.SELECT_FRIEND,
+      select: SELECT_USER_IDENTIFIER,
       where: {
         teams: {
-          none: {
-            team: {
-              code: { in: nonFriendableTeams },
-            },
-          },
+          none: { team: { code: { in: nonFriendableTeams } } },
         },
-        isDeleted: false,
+        ...IS_NOT_DELETED,
       },
     });
   }
 
-  async create(requestorId: number, friendId: number): Promise<User | null> {
-    const { friend } = await this.prisma.friend.create({
-      data: {
-        requestorId,
-        friendId,
-      },
-      select: {
-        friend: {
-          select: this.SELECT_FRIEND,
-        },
-      },
+  async create(requestorId: number, friendId: number): Promise<User> {
+    const isAlreadyFriend = await this.prisma.friend.findFirst({
+      where: { requestorId, friendId },
     });
+    if (isAlreadyFriend) {
+      throw new ForbiddenException("Cette personne fait déjà partie des amis");
+    }
 
+    const { friend } = await this.prisma.friend.create({
+      data: { requestorId, friendId },
+      select: { friend: { select: SELECT_USER_IDENTIFIER } },
+    });
     return friend;
   }
 
   async delete(requestorId: number, friendId: number): Promise<void> {
     await this.prisma.friend.delete({
-      where: {
-        requestorId_friendId: {
-          requestorId,
-          friendId,
-        },
-      },
+      where: { requestorId_friendId: { requestorId, friendId } },
     });
   }
-}
-
-function retrieveFriend({ friend }: FriendWithData): User {
-  return {
-    id: friend.id,
-    firstname: friend.firstname,
-    lastname: friend.lastname,
-    nickname: friend.nickname,
-  };
 }
