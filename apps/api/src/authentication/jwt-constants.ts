5074b6db09f4e7f33cb74667febc7324511781cf
Luc VDH <luke.vandh@gmail.com>
2024-10-08 22:20:41 +0000

    fix(security): throw error if JWT is not dezfined in production environment #2176
    Approved-by: Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>

--- apps/api/src/authentication/jwt-constants.ts
+++ apps/api/src/authentication/jwt-constants.ts
@@ -1,3 +1,12 @@
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET || "secretKey",
+  secret: (() => {
+    const envSecret = process.env.JWT_SECRET;
+    const nodEnv = process.env.NODE_ENV || "development";
+
+    if (!envSecret && nodEnv === "production") {
+      throw new Error("JWT_SECRET is required in production environment");
+    }
+
+    return envSecret || "devSecretKey";
+  })(),
 };
