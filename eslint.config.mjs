f41f23545d1eac65b02f144539120e402d98ad5d
Titouan-Joseph CICORELLA <titouanjoseph@gmail.com>
2024-10-20 12:44:05 +0000

    chore: :rotating_light: setup new eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,89 @@
+import unusedImports from "eslint-plugin-unused-imports";
+import pluginVue from "eslint-plugin-vue";
+import pluginSecurity from "eslint-plugin-security";
+import pulginJs from "@eslint/js";
+import pluginTs from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import vueParser from "vue-eslint-parser";
+import typescriptParser from "@typescript-eslint/parser";
+
+export default pluginTs.config(
+  pulginJs.configs.recommended,
+  ...pluginTs.configs.recommended,
+  pluginSecurity.configs["recommended"],
+  eslintConfigPrettier,
+  ...pluginVue.configs["flat/recommended"],
+  {
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: typescriptParser,
+        sourceType: "module",
+      },
+    },
+  },
+  {
+    rules: {
+      "no-undef": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "vue/multi-word-component-names": "off",
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/html-self-closing": [
+        "error",
+        {
+          html: {
+            void: "always",
+            normal: "always",
+            component: "always",
+          },
+        },
+      ],
+      "vue/valid-v-slot": [
+        "error",
+        {
+          allowModifiers: true,
+        },
+      ],
+      "vue/max-attributes-per-line": [
+        "error",
+        {
+          singleline: {
+            max: 3,
+          },
+          multiline: {
+            max: 1,
+          },
+        },
+      ],
+    },
+    files: ["**/*.vue"],
+  },
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {
+    ignores: [
+      "**/build/",
+      "**/dist/",
+      "apps/old-web/",
+      "apps/web/.nuxt/",
+      "apps/web/.output/",
+      "**/*.spec.ts",
+    ],
+  },
+);
