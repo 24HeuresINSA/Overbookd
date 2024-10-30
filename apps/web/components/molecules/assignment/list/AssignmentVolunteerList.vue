aebaf1c62fd61db2f650495cef2b7b1cccc21f17
Léon Dumestre <leon.dumestre@gmail.com>
2024-10-30 08:34:36 +0000

    prefeat(assignment): add AssignmentVolunteerList & AssignmentVolunteerResume components #2226
    Approved-by: VANDERHAGHEN Luc <luke.vandh@gmail.com>
    Co-authored-by: 24-bot <dsi@24heures.org>

---
+++ apps/web/components/molecules/assignment/list/AssignmentVolunteerList.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-virtual-scroll :items="volunteers" item-height="80" class="virtual-scroll">
+    <template #default="{ item }">
+      <v-list-item
+        :key="item.id"
+        :value="item"
+        class="list"
+        @click="selectVolunteer(item)"
+      >
+        <AssignmentVolunteerResume
+          :volunteer="item"
+          :class="{ 'is-selected': isSelected(item.id) }"
+          class="list__volunteer"
+        />
+      </v-list-item>
+    </template>
+  </v-virtual-scroll>
+</template>
+
+<script lang="ts" setup>
+import type { AssignableVolunteer } from "@overbookd/assignment";
+
+const assignTaskToVolunteerStore = useAssignTaskToVolunteerStore();
+
+defineProps({
+  volunteers: {
+    type: Array as PropType<AssignableVolunteer[]>,
+    required: true,
+  },
+});
+
+const isSelected = (id: number): boolean => {
+  return assignTaskToVolunteerStore.selectedVolunteer?.id === id;
+};
+
+const emit = defineEmits(["select-volunteer"]);
+const selectVolunteer = (volunteer: AssignableVolunteer): void => {
+  if (!volunteer) return;
+  emit("select-volunteer", volunteer);
+};
+</script>
+
+<style lang="scss" scoped>
+.virtual-scroll {
+  height: 100%;
+  margin-top: 4px;
+}
+
+.list {
+  padding: 0;
+  &__volunteer {
+    padding: 0 16px;
+  }
+}
+
+.is-selected {
+  background: $list-selected-item-background-color;
+}
+</style>
