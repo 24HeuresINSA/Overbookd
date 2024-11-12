31879ef3e3bfb41db26ca902adb2764f5e128639
Leon Dumestre <leon.dumestre@gmail.com>
2024-11-12 16:10:25 +0100

    prefeat(availabilitites): add custom calendar components #2220

---
+++ apps/web/components/molecules/calendar/availabilities/AvailabilitiesDailyCalendarContent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="daily-content">
+    <AvailabilititesCalendarEvent
+      v-for="event in eventsInDisplayedDay"
+      :key="event.id"
+      :event="event"
+      :displayed-day="displayedDay"
+      @click="propagateEventClick"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Period } from "@overbookd/time";
+import type { CalendarEvent } from "~/utils/calendar/event";
+
+const props = defineProps({
+  events: {
+    type: Array as PropType<CalendarEvent[]>,
+    required: true,
+  },
+  displayedDay: {
+    type: Date,
+    required: true,
+  },
+});
+
+const eventsInDisplayedDay = computed<CalendarEvent[]>(() => {
+  return props.events.filter((event) =>
+    Period.init(event).isInDay(props.displayedDay),
+  );
+});
+
+const emit = defineEmits(["event-click"]);
+const propagateEventClick = (event: CalendarEvent) =>
+  emit("event-click", event);
+</script>
+
+<style scoped>
+.daily-content {
+  position: relative;
+}
+</style>
