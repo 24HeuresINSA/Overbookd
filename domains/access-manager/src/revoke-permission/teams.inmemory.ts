1fb997020be394a31d47d24c54edc126db76049a
Léo Mouyna <mouynaleo@gmail.com>
2024-11-18 19:40:46 +0000

    feat(access-manager): revoke permission. #2155
    Approved-by: Léon Dumestre <leon.dumestre@gmail.com>
    Co-authored-by: 24-bot <dsi@24heures.org>

---
+++ domains/access-manager/src/revoke-permission/teams.inmemory.ts
@@ -0,0 +1,30 @@
+import { Permission } from "@overbookd/permission";
+import { Teams, Team } from "./revoke-permission";
+
+export class InMemoryTeams implements Teams {
+  constructor(private teamPermissions: Map<Team, Permission[]> = new Map()) {}
+
+  as(team: Team): { revoke: (permission: Permission) => Promise<void> } {
+    return {
+      revoke: async (permission: Permission) => {
+        const permissions = this.teamPermissions.get(team) ?? [];
+        const withoutPermission = permissions.filter(
+          (current) => current !== permission,
+        );
+        this.teamPermissions.set(team, withoutPermission);
+      },
+    };
+  }
+
+  is(team: Team): { ableTo: (permission: Permission) => Promise<boolean> } {
+    return {
+      ableTo: async (permission: Permission) => {
+        return this.teamPermissions.get(team)?.includes(permission) ?? false;
+      },
+    };
+  }
+
+  permissionsOf(team: Team): Permission[] {
+    return this.teamPermissions.get(team) ?? [];
+  }
+}
