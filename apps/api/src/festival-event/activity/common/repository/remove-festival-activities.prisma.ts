af53c87a1847e1538ad9c8785a5a76d187ce7678
Leon Dumestre <leon.dumestre@gmail.com>
2024-12-25 23:46:49 +0100

    fix(fa): prevent FA removal if linked to a FT #1598

--- apps/api/src/festival-event/activity/common/repository/remove-festival-activities.prisma.ts
+++ apps/api/src/festival-event/activity/common/repository/remove-festival-activities.prisma.ts
@@ -2,6 +2,8 @@ import { FestivalActivity } from "@overbookd/festival-event";
 import { PrismaService } from "../../../../prisma.service";
 import { RemoveFestivalActivities } from "../festival-activity-common.model";
 import { buildFestivalActivityCondition } from "./festival-activity.query";
+import { IS_NOT_DELETED } from "../../../../common/query/not-deleted.query";
+import { ForbiddenException } from "@nestjs/common";
 
 export class PrismaRemoveFestivalActivities
   implements RemoveFestivalActivities
@@ -9,6 +11,17 @@ export class PrismaRemoveFestivalActivities
   constructor(private readonly prisma: PrismaService) {}
 
   async remove(id: FestivalActivity["id"]): Promise<void> {
+    const linkedTasks = await this.prisma.festivalTask.findMany({
+      where: { festivalActivityId: id, ...IS_NOT_DELETED },
+      select: { id: true },
+    });
+    if (linkedTasks.length > 0) {
+      const tasksTitles = linkedTasks.map(({ id }) => id).join(", ");
+      const startErrorMessage = "Impossible de supprimer la FA, elle est liée";
+      const endErrorMessage = `${linkedTasks.length === 1 ? "à la FT" : "aux FTs"} ${tasksTitles}`;
+      throw new ForbiddenException(`${startErrorMessage} ${endErrorMessage}`);
+    }
+
     await this.prisma.festivalActivity.update({
       where: buildFestivalActivityCondition(id),
       data: { isDeleted: true },
