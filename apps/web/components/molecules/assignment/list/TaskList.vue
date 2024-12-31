557e2600c6947934658408f65dbe647b54f574da
Léon Dumestre <leon.dumestre@gmail.com>
2024-12-31 12:39:01 +0000

    prefeat(assignment): add TaskList & TaskResume components #2233
    Approved-by: VANDERHAGHEN Luc <luke.vandh@gmail.com>

---
+++ apps/web/components/molecules/assignment/list/TaskList.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-virtual-scroll :items="tasks" item-height="64" class="virtual-scroll">
+    <template #default="{ item }">
+      <v-list-item
+        :key="item.id"
+        :value="item"
+        class="list"
+        @click="selectTask(item)"
+      >
+        <TaskResume
+          :task="item"
+          :class="{ 'is-selected': isSelected(item.id) }"
+          class="list__task"
+        />
+      </v-list-item>
+    </template>
+  </v-virtual-scroll>
+</template>
+
+<script lang="ts" setup>
+import type { MissingAssignmentTask } from "@overbookd/assignment";
+
+const assignTaskToVolunteerStore = useAssignTaskToVolunteerStore();
+
+defineProps({
+  tasks: {
+    type: Array as PropType<MissingAssignmentTask[]>,
+    required: true,
+  },
+});
+
+const selectTask = (task: MissingAssignmentTask) => {
+  assignTaskToVolunteerStore.selectTask(task.id);
+};
+
+const isSelected = (id: number): boolean => {
+  return assignTaskToVolunteerStore.selectedTask?.id === id;
+};
+</script>
+
+<style lang="scss" scoped>
+.virtual-scroll {
+  height: 100%;
+  margin-top: 4px;
+}
+
+.list {
+  padding: 0;
+  &__task {
+    padding: 0 16px;
+  }
+}
+
+.is-selected {
+  background: $list-selected-item-background-color;
+}
+</style>
---
+++ apps/web/components/molecules/assignment/resume/TaskResume.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="task-card">
+    <div class="task-card__data" @contextmenu.prevent="openFtNewTab(task.id)">
+      <div class="task-card__name">
+        <span>{{ task.id }} - {{ task.name }}</span>
+      </div>
+      <div class="task-card__teams">
+        <TeamChip
+          v-for="code of sortedVolunteerTeams"
+          :key="code"
+          :team="code"
+          show-hidden
+        />
+      </div>
+    </div>
+    <v-divider />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { MissingAssignmentTask } from "@overbookd/assignment";
+import { FT_URL } from "@overbookd/web-page";
+import { sortTeamsForAssignment } from "~/utils/sort/sort-teams.utils";
+
+const props = defineProps({
+  task: {
+    type: Object as PropType<MissingAssignmentTask>,
+    required: true,
+  },
+});
+
+const sortedVolunteerTeams = computed<string[]>(() =>
+  sortTeamsForAssignment(props.task.teams),
+);
+
+const openFtNewTab = (ftId: number) => {
+  window.open(`${FT_URL}/${ftId}`, "_blank");
+};
+</script>
+
+<style lang="scss" scoped>
+.task-card {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  &__data {
+    height: 64px;
+    overflow: hidden;
+    display: flex;
+  }
+  &__name {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    padding-left: 8px;
+  }
+  &__teams {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-right: 8px;
+    margin: 2px 0 2px 5px;
+    flex-wrap: wrap;
+    flex-basis: 100px;
+    gap: 2px;
+  }
+}
+</style>
