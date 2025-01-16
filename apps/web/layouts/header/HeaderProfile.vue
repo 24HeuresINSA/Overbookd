900f164bbe917d04dc70260f00a49eccca763c27
lucas-lhm <lucaslhomme01@gmail.com>
2025-01-16 18:43:12 +0000

    feat(web): Add dropdown menu for mobile #2332
    Approved-by: Léon Dumestre <leon.dumestre@gmail.com>
    Co-authored-by: Leon Dumestre <leon.dumestre@gmail.com>
    
    Closes #2332
    
    See merge request 24-heures-insa/overbookd-mono!3142

--- apps/web/layouts/header/HeaderProfile.vue
+++ apps/web/layouts/header/HeaderProfile.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="profile">
-    <div class="profile__header">
+    <div class="profile__header" @click="handleProfileClick">
       <div class="profile__data">
         <ProfilePicture
           v-if="loggedUser"
@@ -18,20 +18,47 @@
       <v-icon class="extend-icon">mdi-chevron-down</v-icon>
     </div>
 
-    <div class="dropdown-menu">
-      <a class="dropdown-menu__item" @click="toggleCurrentTheme">
-        <v-icon>{{ themeIcon }}</v-icon>
-        {{ themeTitle }}
-      </a>
-      <a class="dropdown-menu__item" @click="displayEULA">
-        <v-icon>mdi-book-open-variant-outline</v-icon>
-        Voir les CGU
-      </a>
-      <a class="dropdown-menu__item logout" @click="logout">
-        <v-icon>mdi-close-circle-outline</v-icon>
-        Deconnexion
-      </a>
+    <v-dialog
+      v-if="isMobile"
+      v-model="isMenuOpen"
+      max-width="300"
+      transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <div class="mobile_menu">
+          <div class="mobile_menu__item" @click="toggleCurrentTheme">
+            <v-icon>{{ themeIcon }}</v-icon>
+            <span>{{ themeTitle }}</span>
+          </div>
+          <div class="mobile_menu__item" @click="displayEULA">
+            <v-icon>mdi-book-open-variant-outline</v-icon>
+            <span>Voir les CGU</span>
+          </div>
+          <div class="mobile_menu__item logout" @click="logout">
+            <v-icon>mdi-close-circle-outline</v-icon>
+            <span>Déconnexion</span>
+          </div>
+        </div>
+      </v-card>
+    </v-dialog>
+
+    <div v-if="!isMobile" class="dropdown-container">
+      <div class="dropdown-menu">
+        <div class="dropdown-menu__item" @click="toggleCurrentTheme">
+          <v-icon>{{ themeIcon }}</v-icon>
+          <span>{{ themeTitle }}</span>
+        </div>
+        <div class="dropdown-menu__item" @click="displayEULA">
+          <v-icon>mdi-book-open-variant-outline</v-icon>
+          <span>Voir les CGU</span>
+        </div>
+        <div class="dropdown-menu__item logout" @click="logout">
+          <v-icon>mdi-close-circle-outline</v-icon>
+          <span>Déconnexion</span>
+        </div>
+      </div>
     </div>
+
     <v-dialog
       v-model="isEULADialogOpen"
       transition="dialog-bottom-transition"
@@ -46,23 +73,28 @@
 import { nicknameOrFirstName } from "@overbookd/user";
 import { HAVE_PERSONAL_ACCOUNT } from "@overbookd/permission";
 import { Money } from "@overbookd/money";
-import { LOGIN_URL } from "@overbookd/web-page";
 import { useTheme } from "vuetify";
 import { pickReverseTheme } from "~/utils/vuetify/theme/theme.utils";
 import { navigateTo } from "#app";
+import { LOGIN_URL } from "@overbookd/web-page";
 
 const theme = useTheme();
 const layoutStore = useLayoutStore();
 const authStore = useAuthStore();
 const userStore = useUserStore();
 
+const isMobile = computed<boolean>(() => layoutStore.isMobile);
+const isMenuOpen = ref<boolean>(false);
+
 const loggedUser = computed(() => userStore.loggedUser);
 const myName = computed<string>(() =>
   loggedUser.value ? nicknameOrFirstName(loggedUser.value) : "",
 );
+
 const haveBalance = computed<boolean>(() =>
   userStore.can(HAVE_PERSONAL_ACCOUNT),
 );
+
 const myBalance = computed(() => loggedUser.value?.balance ?? 0);
 const displayedBalance = computed<string>(() =>
   haveBalance.value ? Money.cents(myBalance.value).toString() : "",
@@ -74,6 +106,7 @@ const balanceClassColor = computed<string>(() => {
 });
 
 const logout = async () => {
+  isMenuOpen.value = false;
   authStore.logout();
   await navigateTo(LOGIN_URL);
   userStore.clearLoggedUser();
@@ -87,6 +120,7 @@ const themeIcon = computed<string>(() =>
   isDarkTheme.value ? "mdi-weather-sunny" : "mdi-weather-night",
 );
 const toggleCurrentTheme = () => {
+  isMenuOpen.value = false;
   layoutStore.toggleTheme();
 
   const currentTheme = theme.global.name.value;
@@ -94,8 +128,17 @@ const toggleCurrentTheme = () => {
 };
 
 const isEULADialogOpen = ref<boolean>(false);
-const displayEULA = () => (isEULADialogOpen.value = true);
+const displayEULA = () => {
+  isMenuOpen.value = false;
+  isEULADialogOpen.value = true;
+};
 const closeEULA = () => (isEULADialogOpen.value = false);
+
+const handleProfileClick = () => {
+  if (isMobile) {
+    isMenuOpen.value = !isMenuOpen.value;
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -109,10 +152,8 @@ $header-profile-max-width: 300px;
   &__header {
     display: flex;
     gap: 10px;
-    justify-content: space-between;
     align-items: center;
-    padding: 0 10px;
-    min-width: $header-profile-min-width;
+    margin-right: 20px;
     max-width: $header-profile-max-width;
     height: 100%;
     cursor: pointer;
@@ -149,52 +190,49 @@ $header-profile-max-width: 300px;
       display: none;
     }
   }
+
+  &:hover {
+    .dropdown-container {
+      display: block;
+
+      .dropdown-menu {
+        opacity: 1;
+        transform: translateY(0);
+        pointer-events: auto;
+      }
+    }
+  }
+
   &:hover .extend-icon {
     transform: rotate(180deg);
   }
 }
 
-@media only screen and (min-width: $mobile-max-width) {
-  .profile:hover .dropdown-menu {
-    display: flex;
-    animation: dropdown 0.3s ease forwards;
-  }
-}
-
-.dropdown-menu {
-  display: none;
-  position: fixed;
-  top: 100%;
-  right: 0;
-  min-width: $header-profile-min-width;
-  max-width: $header-profile-max-width;
-  padding: 2px 10px 10px 10px;
+.mobile_menu {
+  padding: 10px;
+  display: flex;
   flex-direction: column;
   gap: 5px;
-  background-color: rgb(var(--v-theme-surface));
-  border-radius: 0 0 10px 10px;
-  align-items: center;
-  opacity: 0;
-  transform: translateY(-10px);
-  transition:
-    opacity 0.3s ease,
-    transform 0.3s ease;
 
   &__item {
-    display: flex;
-    width: 80%;
-    gap: 10px;
-    align-items: center;
-    font-weight: 600;
-    font-size: 0.85rem;
-    cursor: pointer;
-    border-radius: 30px;
-    padding: 4px;
+    margin: 5px 0;
+    border-radius: 15px;
     background-color: rgb(var(--v-theme-background));
     transition: background-color 0.1s ease;
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    font-size: 1rem;
+    font-weight: 600;
+    padding: 15px 20px 15px 20px;
+
     &:hover {
       background-color: rgb(var(--v-theme-primary));
       color: rgb(var(--v-theme-on-primary));
+    }
+
+    span {
+      font-weight: 600;
     }
   }
 }
@@ -207,6 +245,49 @@ $header-profile-max-width: 300px;
   to {
     opacity: 1;
     transform: translateY(0);
+  }
+}
+
+.dropdown-container {
+  position: fixed;
+  top: 100%;
+  right: 0;
+  display: none;
+}
+
+.dropdown-menu {
+  min-width: $header-profile-min-width;
+  max-width: $header-profile-max-width;
+  padding: 2px 10px 10px 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  background-color: rgb(var(--v-theme-surface));
+  border-radius: 0 0 10px 10px;
+  align-items: center;
+  animation: dropdown 0.3s ease forwards;
+  opacity: 0;
+  transform: translateY(-10px);
+  transition: all 0.3s ease;
+  pointer-events: none;
+
+  &__item {
+    display: flex;
+    width: 80%;
+    gap: 10px;
+    align-items: center;
+    font-weight: 600;
+    font-size: 0.85rem;
+    cursor: pointer;
+    border-radius: 30px;
+    padding: 6.5px;
+    background-color: rgb(var(--v-theme-background));
+    transition: background-color 0.1s ease;
+
+    &:hover {
+      background-color: rgb(var(--v-theme-primary));
+      color: rgb(var(--v-theme-on-primary));
+    }
   }
 }
 
