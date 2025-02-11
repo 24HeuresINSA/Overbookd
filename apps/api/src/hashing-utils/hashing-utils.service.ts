b2c401020c4ff75166e5359ce209b02cd92bbba6
Leon Dumestre <leon.dumestre@gmail.com>
2025-02-11 16:51:37 +0100

    fix: bcrypt import

--- apps/api/src/hashing-utils/hashing-utils.service.ts
+++ apps/api/src/hashing-utils/hashing-utils.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@nestjs/common";
-import * as bcrypt from "bcrypt";
+import { hash as bcryptHash, compare as bcryptCompare } from "bcrypt";
 
 @Injectable()
 export class HashingUtilsService {
   async hash(password: string): Promise<string> {
-    const hash = await bcrypt.hash(password, 13);
+    const hash = await bcryptHash(password, 13);
     return hash;
   }
 
   async compare(password: string, hash: string): Promise<boolean> {
-    return bcrypt.compare(password, hash);
+    return bcryptCompare(password, hash);
   }
 }
