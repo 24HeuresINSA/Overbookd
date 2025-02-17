a50a2b8641595363137d449ef97284e4c9d52acc
Polo <polo.diemunsch@gmail.com>
2025-02-17 13:31:07 +0000

    fix(web): fix calendar events content display #2405 #2340

---
+++ apps/web/components/atoms/calendar/CalendarEventContent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="name">{{ name }}</div>
+  <div class="hour">{{ hour }}</div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  hour: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.name {
+  font-size: 0.85rem;
+  font-weight: bold;
+  line-height: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-bottom: 5px;
+}
+
+.hour {
+  font-size: 0.75rem;
+  line-height: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
