06476a92ac6b68fcd179c479fcedd1b1b541ed02
Leon Dumestre <leon.dumestre@gmail.com>
2025-03-11 10:22:39 +0100

    fix: build

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -26,7 +26,6 @@ export default defineNuxtConfig({
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
-          hid: "description",
           name: "description",
           content: "Overbookd - Organisation du festival des 24h de l’INSA",
         },
