7a0f5b87441f0e786915cf5ba3c2e26291de17df
lucas-science <12345678+username@users.noreply.github.com>
2025-03-27 14:00:07 +0100

    fix(web): the tooltip can be clicked from a cell phone #2463

--- apps/web/components/atoms/field/CommentField.vue
+++ apps/web/components/atoms/field/CommentField.vue
@@ -2,7 +2,12 @@
   <div>
     <div class="comment-title">
       <span>Commentaire</span>
-      <v-tooltip location="top">
+      <v-tooltip
+        location="top"
+        :activate-on-click="true"
+        open-on-click
+        close-on-back
+      >
         <template #activator="{ props }">
           <v-icon v-bind="props" icon="mdi-information" color="primary" />
         </template>
