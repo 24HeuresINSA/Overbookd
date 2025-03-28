54d31532d4defb018d0290cf7f19bcb161c617c1
Leon Dumestre <leon.dumestre@gmail.com>
2025-03-28 10:59:47 +0100

    fix: wait important request only #2456

--- apps/web/middleware/05.store-data.global.ts
+++ apps/web/middleware/05.store-data.global.ts
@@ -4,12 +4,11 @@ export default defineNuxtRouteMiddleware(async () => {
   const teamStore = useTeamStore();
   const configurationStore = useConfigurationStore();
 
-  const actions = [
-    teamStore.teams.length === 0 && teamStore.fetchTeams(),
-    teamStore.faReviewers.length === 0 && teamStore.fetchFaReviewers(),
-    teamStore.ftReviewers.length === 0 && teamStore.fetchFtReviewers(),
-    configurationStore.fetch(EVENT_DATE_KEY),
-  ].filter(Boolean);
+  teamStore.fetchFaReviewers();
+  teamStore.fetchFtReviewers();
 
-  await Promise.all(actions);
+  await Promise.all([
+    teamStore.fetchTeams(),
+    configurationStore.fetch(EVENT_DATE_KEY),
+  ]);
 });
