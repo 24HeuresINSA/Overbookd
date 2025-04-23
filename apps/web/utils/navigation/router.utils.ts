22561d1dd18b551c9a58b66145c39647eaa0c83a
VANDERHAGHEN Luc <luke.vandh@gmail.com>
2025-04-23 18:11:59 +0200

    fix(web): handle crtl click when changing page if necssary #2498
    Approved-by: Léon Dumestre <leon.dumestre@gmail.com>
    Co-authored-by: Leon Dumestre <leon.dumestre@gmail.com>
    
    Closes #2498
    
    See merge request 24-heures-insa/overbookd-mono!3395

--- apps/web/utils/navigation/router.utils.ts
+++ apps/web/utils/navigation/router.utils.ts
@@ -1,10 +1,21 @@
 export function openPageWithIdInNewTab(path: string, id: number) {
   const router = useRouter();
-  const taskRoute = router.resolve(`${path}/${id}`);
-  window.open(taskRoute.href);
+  const pageRoute = router.resolve(`${path}/${id}`);
+  window.open(pageRoute.href);
 }
 
 export function openPageWithId(event: PointerEvent, path: string, id: number) {
   if (event.ctrlKey) return openPageWithIdInNewTab(path, id);
   navigateTo(`${path}/${id}`);
+}
+
+export function openPage(event: PointerEvent, path: string) {
+  if (event.ctrlKey) return openPageInNewTab(path);
+  navigateTo(path);
+}
+
+export function openPageInNewTab(path: string) {
+  const router = useRouter();
+  const pageRoute = router.resolve(path);
+  window.open(pageRoute.href);
 }
