9c731c5a3ca80dc8d75d075c8481c31d4e69092f
Leo Mouyna <mouynaleo@gmail.com>
2025-04-23 16:13:30 +0200

    fix(signage): use builder for festival activity export. #2495

--- apps/api/src/festival-event/activity/preview/signa-preview.ts
+++ apps/api/src/festival-event/activity/preview/signa-preview.ts
@@ -1,5 +1,6 @@
 import { BaseSignage, FestivalActivity } from "@overbookd/festival-event";
 import { statusLabels } from "@overbookd/festival-event-constants";
+import { CSVBuilder } from "@overbookd/csv";
 
 export type PreviewForSigna = {
   id: FestivalActivity["id"];
@@ -12,41 +13,42 @@ export type PreviewForSigna = {
 
 export class SignaPreview {
   static toCsv(previews: PreviewForSigna[]): string {
-    const csvHeader = [
-      "Id FA",
-      "Nom FA",
-      "Statut FA",
-      "Equipe",
-      "Lieu",
-      "Nom catalogue",
-      "Type",
-      "Quantité",
-      "Texte",
-      "Taille",
-      "Commentaire",
-    ].join(";");
-
-    const csvContent = previews.flatMap((preview) => {
+    const data = previews.flatMap((preview) => {
       return preview.signages.flatMap((signage) => {
-        return [
-          preview.id,
-          preview.name,
-          statusLabels.get(preview.status) ?? "",
-          preview.team,
-          preview.locationName ?? "",
-          signage.catalogName ?? "",
-          signage.type,
-          signage.quantity,
-          signage.text,
-          signage.size,
-          signage?.comment ?? "",
-        ].join(";");
+        const { id, name, status: rawStatus, team, locationName } = preview;
+        const status = statusLabels.get(rawStatus);
+        const activity = { id, name, status, team, locationName };
+        return { signage, activity };
       });
     });
 
-    const csv = [csvHeader, ...csvContent].join("\n");
-    const regex = new RegExp(/undefined/i, "g");
-
-    return csv.replace(regex, "");
+    return CSVBuilder.from(data)
+      .select([
+        "activity.id",
+        "activity.name",
+        "activity.status",
+        "activity.team",
+        "activity.locationName",
+        "signage.catalogName",
+        "signage.type",
+        "signage.quantity",
+        "signage.text",
+        "signage.size",
+        "signage.comment",
+      ])
+      .translate([
+        ["activity.id", "Id FA"],
+        ["activity.name", "Nom FA"],
+        ["activity.status", "Statut FA"],
+        ["activity.team", "Equipe"],
+        ["activity.locationName", "Lieu"],
+        ["signage.catalogName", "Nom catalogue"],
+        ["signage.type", "Type"],
+        ["signage.quantity", "Quantité"],
+        ["signage.text", "Texte"],
+        ["signage.size", "Taille"],
+        ["signage.comment", "Commentaire"],
+      ])
+      .build();
   }
 }
