6ebb7ce4da5a33409779e5c7d4b98bd7430dba29
Léo Mouyna <mouynaleo@gmail.com>
2025-04-23 11:43:21 +0000

    feat(csv): handle nested object on file generation. #2496
    Approved-by: VANDERHAGHEN Luc <luke.vandh@gmail.com>
    
    Closes #2496
    
    See merge request 24-heures-insa/overbookd-mono!3393

--- libraries/csv/src/csv.builder.ts
+++ libraries/csv/src/csv.builder.ts
@@ -2,12 +2,19 @@ const DEFAULT_DELIMITER = ",";
 const SPECIAL_CHARACTERS = /["\n\r,\\]/;
 const ALL_QUOTES = /"/g;
 
-type CsvData = string | number | boolean | null | undefined;
-type CsvObject = Record<string, CsvData>;
+type CsvObject = Record<string, unknown>;
+
+type DeepPath<T, Key extends keyof T = keyof T> = T extends never
+  ? never
+  : Key extends string
+    ? T[Key] extends CsvObject
+      ? `${Key}.${DeepPath<T[Key]>}`
+      : `${Key}`
+    : never;
 
 export class CSVBuilder<
   Item extends CsvObject,
-  Header extends keyof Item = keyof Item,
+  Header extends DeepPath<Item> = DeepPath<Item>,
 > {
   private constructor(
     private readonly items: Item[],
@@ -16,9 +23,13 @@ export class CSVBuilder<
     private readonly translations: Map<Header, string> = new Map(),
   ) {}
 
-  static from<T extends CsvObject>(items: T[]): CSVBuilder<T, keyof T> {
-    const headers = Object.keys(items.at(0) ?? {});
-    return new CSVBuilder(items, headers);
+  static from<T extends CsvObject>(items: T[]): CSVBuilder<T, DeepPath<T>> {
+    const headers = items.reduce((headers, item) => {
+      const keys = extractKeys(item).filter((key) => isPath(item, key));
+      return new Set<DeepPath<T>>([...headers, ...keys]);
+    }, new Set<DeepPath<T>>());
+
+    return new CSVBuilder(items, [...headers]);
   }
 
   select(headers: Header[]): CSVBuilder<Item, Header> {
@@ -55,14 +66,14 @@ export class CSVBuilder<
 
     const lines = this.items.map((item) =>
       this.headers
-        .map((key) => this.escapeWhenNeeded(item[key.toString()]))
+        .map((key) => this.escapeWhenNeeded(get(item, key)))
         .join(this.delimiter),
     );
 
     return [headers, ...lines].join("\n");
   }
 
-  private escapeWhenNeeded(value: CsvData) {
+  private escapeWhenNeeded(value: unknown) {
     if (typeof value !== "string") return value;
     const hasSpecialCharacters = SPECIAL_CHARACTERS.test(value);
     const hasDelimiter = value.includes(this.delimiter);
@@ -71,6 +82,43 @@ export class CSVBuilder<
   }
 }
 
+function isObject(value: unknown): value is object {
+  return typeof value === "object" && value !== null;
+}
+
 function escape(value: string): string {
   return `"${value.replace(ALL_QUOTES, '""')}"`;
+}
+
+function isPath<T extends CsvObject>(
+  item: T,
+  path: string,
+): path is DeepPath<T> {
+  return pathExists(item, path);
+}
+
+function extractKeys(value: object): string[] {
+  return Object.entries(value).flatMap(([key, value]) => {
+    if (!isObject(value)) return key;
+    return extractKeys(value).map((nested) => `${key}.${nested}`);
+  });
+}
+
+function get<T extends CsvObject>(item: T, path: DeepPath<T>) {
+  return path.split(".").reduce<unknown>((deepObject, key) => {
+    if (!hasProperty(deepObject, key)) return undefined;
+    return deepObject[`${key}`];
+  }, item);
+}
+
+function hasProperty(item: unknown, key: string): item is { [key]: unknown } {
+  return isObject(item) && key in item;
+}
+
+function pathExists(item: unknown, path: string): boolean {
+  const [key, ...remaining] = path.split(".");
+  if (key === undefined || key === "") return true;
+  if (!isObject(item)) return false;
+  const nextPath = remaining.join(".");
+  return hasProperty(item, key) && pathExists(item[`${key}`], nextPath);
 }
