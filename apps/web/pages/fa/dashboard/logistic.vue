0ca1639d65e64da18de60244a0614a7ee8b153de
Leon Dumestre <leon.dumestre@gmail.com>
2025-04-29 22:45:20 +0200

    fix: add filters #2519

--- apps/web/pages/fa/dashboard/logistic.vue
+++ apps/web/pages/fa/dashboard/logistic.vue
@@ -1,74 +1,90 @@
 <template>
   <DesktopPageTitle />
-  <v-card>
-    <v-card-text>
-      <v-data-table
-        :headers="activityHeaders"
-        :items="filteredActivities"
-        no-data-text="Aucune activité avec une demande de matériel"
-        :loading="loading"
-        loading-text="Chargement des fiches activités..."
-        :hover="filteredActivities.length > 0"
-        :mobile="isMobile"
-        density="comfortable"
-        class="fa"
-        show-expand
-        expand-on-click
-      >
-        <template #top>
-          <div class="filters">
-            <v-text-field
-              v-model="search"
-              label="Rechercher une activité"
-              hide-details
-            />
-            <v-btn
-              prepend-icon="mdi-export"
-              text="Exporter les FA affichées"
-              color="secondary"
-              @click="exportCsv"
-            />
-          </div>
-        </template>
 
-        <template #item.id="{ item }">
-          <div class="status">
-            <v-chip :class="item.status.toLowerCase()">
-              {{ item.id }}
-            </v-chip>
-          </div>
-        </template>
-
-        <template #item.team="{ item }">
-          <TeamChip v-if="item.team" :team="item.team" with-name />
-        </template>
-
-        <template #item.link="{ item }">
-          <v-btn
-            icon="mdi-open-in-new"
-            variant="flat"
-            density="comfortable"
-            @click.stop="openPageWithId($event, FA_URL, item.id)"
+  <div class="dashboard">
+    <v-card>
+      <v-card-text class="dashboard__filters">
+        <GearFilter
+          v-model:search="filters.search"
+          v-model:category="filters.category"
+          v-model:team="filters.team"
+        />
+        <div class="dashboard__filters-additional">
+          <v-autocomplete
+            :items="[]"
+            label="Lieu de stockage"
+            hide-details
+            clearable
+            hide-selected
+            no-data-text="Aucun lieu correspondant"
           />
-        </template>
+          <v-text-field
+            v-model="search"
+            label="Rechercher une activité"
+            hide-details
+          />
+          <v-btn
+            prepend-icon="mdi-export"
+            text="Exporter les FA affichées"
+            color="secondary"
+          />
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card>
+      <v-card-text>
+        <v-data-table
+          :headers="activityHeaders"
+          :items="filteredActivities"
+          no-data-text="Aucune activité avec une demande de matériel"
+          :loading="loading"
+          loading-text="Chargement des fiches activités..."
+          :hover="filteredActivities.length > 0"
+          :mobile="isMobile"
+          density="comfortable"
+          class="fa"
+          show-expand
+          expand-on-click
+        >
+          <template #item.id="{ item }">
+            <div class="status">
+              <v-chip :class="item.status.toLowerCase()">
+                {{ item.id }}
+              </v-chip>
+            </div>
+          </template>
 
-        <template #expanded-row="{ item }">
-          <td :colspan="activityHeaders.length + 1">
-            <v-data-table
-              :headers="gearHeaders"
-              :items="item.inquiries"
-              no-data-text="Aucun matos demandé"
-              :mobile="isMobile"
-              density="compact"
-              items-per-page="-1"
-              hide-default-footer
+          <template #item.team="{ item }">
+            <TeamChip v-if="item.team" :team="item.team" with-name />
+          </template>
+
+          <template #item.link="{ item }">
+            <v-btn
+              icon="mdi-open-in-new"
+              variant="flat"
+              density="comfortable"
+              @click.stop="openPageWithId($event, FA_URL, item.id)"
             />
-            <v-divider />
-          </td>
-        </template>
-      </v-data-table>
-    </v-card-text>
-  </v-card>
+          </template>
+
+          <template #expanded-row="{ item }">
+            <td :colspan="activityHeaders.length + 1">
+              <v-data-table
+                :headers="gearHeaders"
+                :items="item.inquiries"
+                no-data-text="Aucun matos demandé"
+                :mobile="isMobile"
+                density="compact"
+                items-per-page="-1"
+                hide-default-footer
+              />
+              <v-divider />
+            </td>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -77,6 +93,7 @@ import type { PreviewForLogistic } from "@overbookd/http";
 import { SlugifyService } from "@overbookd/slugify";
 import { FA_URL } from "@overbookd/web-page";
 import { downloadCsv } from "~/utils/file/download.utils";
+import type { FilterGear } from "~/utils/logistic/filter-gear";
 import { openPageWithId } from "~/utils/navigation/router.utils";
 import {
   matchingSearchItems,
@@ -103,6 +120,12 @@ const gearHeaders: TableHeaders = [
   { title: "Lieu de stockage", value: "drive", sortable: true },
 ];
 const isMobile = computed<boolean>(() => layoutStore.isMobile);
+
+const filters = reactive<FilterGear>({
+  search: "",
+  category: undefined,
+  team: undefined,
+});
 
 const activities = computed<PreviewForLogistic[]>(
   () => faStore.activities.forLogistic,
@@ -187,10 +210,21 @@ const exportCsv = async () => {
 };
 </script>
 
-<style scoped>
-.filters {
+<style lang="scss" scoped>
+.dashboard {
   display: flex;
-  gap: 10px;
-  align-items: center;
+  flex-direction: column;
+  gap: $card-gap;
+
+  &__filters {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    &-additional {
+      display: flex;
+      gap: 10px;
+      align-items: center;
+    }
+  }
 }
 </style>
