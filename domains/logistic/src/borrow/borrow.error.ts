2fb360af0d8a9793aa9b02f3eb050e4eec845bc4
Leon Dumestre <leon.dumestre@gmail.com>
2025-06-10 16:22:06 +0200

    fix: remove useless export #2415

--- domains/logistic/src/borrow/borrow.error.ts
+++ domains/logistic/src/borrow/borrow.error.ts
@@ -1,7 +1,7 @@
 import { LogisticError } from "../logistic.error.js";
 import { Borrow } from "./borrow.js";
 
-export class BorrowError extends LogisticError {}
+class BorrowError extends LogisticError {}
 
 export class BorrowNotFound extends BorrowError {
   constructor(id: Borrow["id"]) {
