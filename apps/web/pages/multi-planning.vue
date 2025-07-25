8ba3cde788f6ee612a044564bcfac761ce765ccd
Polo <polo.diemunsch@gmail.com>
2025-07-25 13:00:37 +0000

    feat(planning): save multi planning params in url #2567

--- apps/web/pages/multi-planning.vue
+++ apps/web/pages/multi-planning.vue
@@ -2,7 +2,11 @@
   <DesktopPageTitle />
 
   <div class="multi-planning">
-    <MultiPlanningFilterCard @apply="onApplyFilters" />
+    <MultiPlanningFilterCard
+      v-model:volunteers="volunteers"
+      :loading="loading"
+      @apply="onApplyFilters"
+    />
 
     <OverMultiCalendar
       v-model="day"
@@ -21,14 +25,22 @@
 </template>
 
 <script lang="ts" setup>
+import { formatLocalDateTime } from "@overbookd/time";
 import type { User } from "@overbookd/user";
 import { createCalendarEvent } from "~/utils/calendar/event";
 import type { VolunteerForCalendar } from "~/utils/calendar/volunteer";
+import { updateQueryParams } from "~/utils/http/url-params.utils";
+import { MultiPlanningParamsBuilder } from "~/utils/multi-planning/multi-planning.filter";
 
 useHead({ title: "Multi Planning" });
 
+const DEFAULT_PAGE = 0;
+const DEFAULT_ITEMS_PER_PAGE = 10;
+
 const multiPlanningStore = useMultiPlanningStore();
 const configurationStore = useConfigurationStore();
+
+const volunteers = ref<User[]>([]);
 
 const volunteersForCalendar = computed<VolunteerForCalendar[]>(() =>
   multiPlanningStore.volunteers.map((volunteer) => ({
@@ -37,13 +49,54 @@ const volunteersForCalendar = computed<VolunteerForCalendar[]>(() =>
   })),
 );
 
-const onApplyFilters = (volunteers: User[]) => {
-  multiPlanningStore.fetchVolunteers(volunteers.map(({ id }) => id));
+const loading = ref<boolean>(false);
+
+const onApplyFilters = async () => {
+  loading.value = true;
+  const volunteerIds = volunteers.value.map(({ id }) => id);
+  await Promise.all([
+    multiPlanningStore.fetchVolunteers(volunteerIds),
+    updateQueryParams("volunteerIds", volunteerIds.map(String)),
+  ]);
+  loading.value = false;
 };
 
-const day = ref<Date>(configurationStore.eventStartDate);
-const page = ref<number>(0);
-const itemsPerPage = ref<number>(10);
+const defaultDay = computed<Date>(() => configurationStore.eventStartDate);
+
+const day = ref<Date>(defaultDay.value);
+const page = ref<number>(DEFAULT_PAGE);
+const itemsPerPage = ref<number>(DEFAULT_ITEMS_PER_PAGE);
+
+const route = useRoute();
+onMounted(async () => {
+  await useUserStore().fetchVolunteers();
+  const params = MultiPlanningParamsBuilder.getFromRouteQuery(route.query);
+  if (params.volunteers) {
+    volunteers.value = params.volunteers;
+    onApplyFilters();
+  }
+  if (params.day) day.value = params.day;
+  if (params.page) page.value = params.page;
+  if (params.itemsPerPage) itemsPerPage.value = params.itemsPerPage;
+});
+
+watch(day, (d) =>
+  updateQueryParams(
+    "day",
+    d.getTime() !== defaultDay.value.getTime()
+      ? formatLocalDateTime(d)
+      : undefined,
+  ),
+);
+watch(page, (p) =>
+  updateQueryParams("page", p !== DEFAULT_PAGE ? p : undefined),
+);
+watch(itemsPerPage, (ipp) =>
+  updateQueryParams(
+    "itemsPerPage",
+    ipp !== DEFAULT_ITEMS_PER_PAGE ? ipp : undefined,
+  ),
+);
 </script>
 
 <style lang="scss" scoped>
