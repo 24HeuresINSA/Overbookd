a4a9d5fdac2adc104092d5b9c633796f83ee100d
Leon Dumestre <leon.dumestre@gmail.com>
2025-08-12 16:58:43 +0200

    fix: tests #2569

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -18,6 +18,6 @@ describe("AppController (e2e)", () => {
   it("/ (GET)", async () => {
     const res = await request(app.getHttpServer()).get("/");
     expect(res.status).toBe(200);
-    expect(res.text).toContain("Hello from overbookd backend");
+    expect(res.text).toContain("Hello from overbookd api");
   });
 });
