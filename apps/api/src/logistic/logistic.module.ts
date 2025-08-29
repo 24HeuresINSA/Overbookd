2defe9cf400791679b544c15fdafbb5a59ad926f
Leon Dumestre <leon.dumestre@gmail.com>
2025-08-29 13:51:14 +0200

    fix: remove borrows for tuto

--- apps/api/src/logistic/logistic.module.ts
+++ apps/api/src/logistic/logistic.module.ts
@@ -2,16 +2,9 @@ import { Module } from "@nestjs/common";
 import { DashboardModule } from "./dashboard/dashboard.module";
 import { CatalogModule } from "./catalog/catalog.module";
 import { InventoryModule } from "./inventory/inventory.module";
-import { BorrowModule } from "./borrow/borrow.module";
 import { PurchaseModule } from "./purchase/purchase.module";
 
 @Module({
-  imports: [
-    DashboardModule,
-    CatalogModule,
-    InventoryModule,
-    BorrowModule,
-    PurchaseModule,
-  ],
+  imports: [DashboardModule, CatalogModule, InventoryModule, PurchaseModule],
 })
 export class LogisticModule {}
