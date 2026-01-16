c2c7c7ef865e16039eece2fdc3dc5bbe71e4272f
LucVDH <luke.vandh@gmail.com>
2026-01-16 18:26:16 +0100

    refactor(mail): replace nestjs/mailer by nodmailer for security #2605

--- apps/api/src/mail/mail.module.ts
+++ apps/api/src/mail/mail.module.ts
@@ -1,30 +1,20 @@
 import { Module } from "@nestjs/common";
 import { MailService } from "./mail.service";
-import { DomainEventModule } from "../domain-event/domain-event.module";
-import { MailerService } from "@nestjs-modules/mailer";
-import { DomainEventService } from "../domain-event/domain-event.service";
 import { PrismaMembers } from "./repository/members.prisma";
 import { PrismaService } from "../prisma.service";
 import { PrismaModule } from "../prisma.module";
+import { DomainEventModule } from "../domain-event/domain-event.module";
 
 @Module({
+  imports: [PrismaModule, DomainEventModule],
   providers: [
     {
       provide: PrismaMembers,
       useFactory: (prisma: PrismaService) => new PrismaMembers(prisma),
       inject: [PrismaService],
     },
-    {
-      provide: MailService,
-      useFactory: (
-        mailer: MailerService,
-        eventStore: DomainEventService,
-        members: PrismaMembers,
-      ) => new MailService(mailer, eventStore, members),
-      inject: [MailerService, DomainEventService, PrismaMembers],
-    },
+    MailService,
   ],
-  exports: [MailService],
-  imports: [DomainEventModule, PrismaModule],
+  exports: [MailService, PrismaMembers],
 })
 export class MailModule {}
