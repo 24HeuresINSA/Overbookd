a1df6e55fda44c550629665cc92c2ad45be6a7d3
Ejerym <quentin.jeremy79@gmail.com>
2026-02-19 00:53:03 +0100

    feat: Dispos etendues petite corrections

--- apps/web/middleware/05.store-data.global.ts
+++ apps/web/middleware/05.store-data.global.ts
@@ -1,4 +1,4 @@
-import { EVENT_DATE_KEY } from "@overbookd/configuration";
+import { EVENT_DATE_KEY, ORGA_WEEK_DATE_KEY } from "@overbookd/configuration";
 
 export default defineNuxtRouteMiddleware(async () => {
   const teamStore = useTeamStore();
@@ -6,10 +6,14 @@ export default defineNuxtRouteMiddleware(async () => {
 
   const hasNoTeam = teamStore.teams.length === 0;
   const hasNoEventDate = !configurationStore.get(EVENT_DATE_KEY);
+  const hasNoOrgaWeekDate = !configurationStore.get(ORGA_WEEK_DATE_KEY);
 
   const actions = [
     hasNoTeam ? teamStore.fetchTeams() : undefined,
     hasNoEventDate ? configurationStore.fetch(EVENT_DATE_KEY) : undefined,
+    hasNoOrgaWeekDate
+      ? configurationStore.fetch(ORGA_WEEK_DATE_KEY)
+      : undefined,
   ].filter(Boolean);
   await Promise.all(actions);
 });
