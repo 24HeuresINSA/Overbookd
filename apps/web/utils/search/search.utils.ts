a90edb9f388d182dfa80bacac3f3e69a9e1f93cf
Polo Diemunsch <polo.diemunsch@gmail.com>
2026-04-29 09:08:08 +0000

    fix(web): improve custom slugified filter for search fields #2590
    Approved-by: Léon Dumestre <leon.dumestre@gmail.com>
    
    Closes #2590
    
    See merge request 24-heures-insa/overbookd-mono!3698

--- apps/web/utils/search/search.utils.ts
+++ apps/web/utils/search/search.utils.ts
@@ -1,4 +1,5 @@
 import { SlugifyService } from "@overbookd/slugify";
+import type { FilterMatch } from "vuetify";
 
 export type Searchable<T> = T & { searchable: string };
 
@@ -19,7 +20,31 @@ export function keepMatchingSearchCriteria<T>(
   return ({ searchable }) => searchable.includes(slugifiedSearch);
 }
 
-export function slugifiedFilter(itemFilterKey: string, typedSearch: string) {
-  const search = SlugifyService.apply(typedSearch);
-  return SlugifyService.apply(itemFilterKey).indexOf(search);
+export function slugifiedFilter(
+  itemFilterKey: string,
+  typedSearch: string,
+): FilterMatch {
+  const slugifiedKey = SlugifyService.apply(itemFilterKey);
+  const searchWords = typedSearch
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "")
+    .map((word) => SlugifyService.apply(word));
+
+  const matches = searchWords.reduce<[number, number][]>((matches, word) => {
+    const lastMatch = matches.at(-1);
+    const startIndex = lastMatch ? lastMatch[1] : 0;
+    const nextWordIndex =
+      startIndex === 0 ? 0 : itemFilterKey.indexOf(" ", startIndex);
+    const start = slugifiedKey.indexOf(word, nextWordIndex);
+    matches.push([start, start + word.length]);
+    return matches;
+  }, []);
+
+  const uniqueMatches = new Map(matches.map((match) => [match[0], match]));
+  if (uniqueMatches.size !== searchWords.length || uniqueMatches.has(-1))
+    return -1;
+
+  const sortedMatches = [...uniqueMatches.values()].sort((a, b) => a[0] - b[0]);
+  return sortedMatches;
 }
