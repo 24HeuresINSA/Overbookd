ac6ec3beae74104525fda5a506577fb409dfb610
Leon Dumestre <leon.dumestre@gmail.com>
2026-09-19 15:03:01 +0200

    fix: remove line break #2747

--- apps/web/layouts/unauthenticated.vue
+++ apps/web/layouts/unauthenticated.vue
@@ -18,7 +18,6 @@ import { useFavicon } from "@vueuse/core";
 import { isCetaitMieuxAvant, isPreProd } from "~/utils/navigation/url.constant";
 
 const favicon = useFavicon();
-
 if (isPreProd()) favicon.value = "/favicon-preprod.ico";
 if (isCetaitMieuxAvant()) favicon.value = "/favicon-ctma.ico";
 </script>
